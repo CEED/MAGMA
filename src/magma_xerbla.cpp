07a50514156d163aa2b403b62f54435c699f0fa4
rnath1 <rnath1@mycompany.com>
2009-11-08 20:22:17 +0000

    exit added
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@56 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/magma_xerbla.cpp
+++ src/magma_xerbla.cpp
@@ -1,4 +1,34 @@
 #include <stdio.h>
-void magma_xerbla(char *name , int *info){
-printf("MAGMA Error: On Routine %s argument number %d had an illegal value\n",name , *info);
+/*
+   -- MAGMA (version 0.1) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       August 2009
+
+ Purpose
+ =======
+
+ XERBLA  is an error handler for the LAPACK routines.
+ It is called by an LAPACK routine if an input parameter has an
+ invalid value.  A message is printed and execution stops.
+
+ Installers may consider modifying the STOP statement in order to
+ call system-specific exception-handling facilities.
+
+ Arguments
+ =========
+
+ SRNAME  (input) CHARACTER*(*)
+         The name of the routine which called XERBLA.
+
+ INFO    (input) INTEGER
+         The position of the invalid parameter in the parameter list
+         of the calling routine.
+
+=====================================================================
+*/
+void magma_xerbla(char *srname , int *info){
+printf("MAGMA Error: On Routine %s argument number %d had an illegal value\n",srname , *info);
+exit(1);
 }
