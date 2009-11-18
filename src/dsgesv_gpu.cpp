aeed51290757dd6bcb420bd8b3a493633a340abe
Stanimire Tomov <tomov@icl.utk.edu>
2009-11-18 17:17:51 +0000

    clearing up stuff
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@101 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/dsgesv_gpu.cpp
+++ src/dsgesv_gpu.cpp
@@ -7,21 +7,8 @@
 
 #define cublasDtrsm magmablas_dtrsm
 #define cublasDgemm magmablasDgemm
-
-#define cublasStrsm magmablas_strsm
-#define cublasSgemm magmablasSgemm
-
-
-#define MAX(a,b)       (((a)>(b))?(a):(b))
-
 #define BWDMAX 1.0
 #define ITERMAX 30
-
-extern "C" double dlamch_(char *);
-extern "C" float slamch_(char *);
-
-void magma_dgetrs_v2( char *TRANS , int N , int NRHS, double *A , int LDA , int *IPIV , double *B, int LDB, int *INFO, double *BB1 );
-void magma_sgetrs_v2( char *TRANS , int N , int NRHS, float *A , int LDA , int *IPIV , float *B, int LDB, int *INFO, float *BB1 );
 
 int
 magma_dsgesv_gpu(int N, int NRHS, double *A, int LDA, int *IPIV, double *B, 
@@ -29,127 +16,131 @@ magma_dsgesv_gpu(int N, int NRHS, double *A, int LDA, int *IPIV, double *B,
 		 int *ITER, int *INFO, float *H_SWORK, double *H_WORK,
 		 int *DIPIV)
 {
-/*
-  Purpose
-  =======
-
-  DSGESV computes the solution to a real system of linear equations
-     A * X = B,
-  where A is an N-by-N matrix and X and B are N-by-NRHS matrices.
-
-  DSGESV first attempts to factorize the matrix in SINGLE PRECISION
-  and use this factorization within an iterative refinement procedure
-  to produce a solution with DOUBLE PRECISION normwise backward error
-  quality (see below). If the approach fails the method switches to a
-  DOUBLE PRECISION factorization and solve.
-
-  The iterative refinement is not going to be a winning strategy if
-  the ratio SINGLE PRECISION performance over DOUBLE PRECISION
-  performance is too small. A reasonable strategy should take the
-  number of right-hand sides and the size of the matrix into account.
-  This might be done with a call to ILAENV in the future. Up to now, we
-  always try iterative refinement.
-  The iterative refinement process is stopped if
-      ITER > ITERMAX
-  or for all the RHS we have:
-      RNRM < SQRT(N)*XNRM*ANRM*EPS*BWDMAX
-  where
-      o ITER is the number of the current iteration in the iterative
-        refinement process
-      o RNRM is the infinity-norm of the residual
-      o XNRM is the infinity-norm of the solution
-      o ANRM is the infinity-operator-norm of the matrix A
-      o EPS is the machine epsilon returned by DLAMCH('Epsilon')
-  The value ITERMAX and BWDMAX are fixed to 30 and 1.0D+00
-  respectively.
-
-  Arguments
-  =========
-
-  N       (input) INTEGER
-          The number of linear equations, i.e., the order of the
-          matrix A.  N >= 0.
-
-  NRHS    (input) INTEGER
-          The number of right hand sides, i.e., the number of columns
-          of the matrix B.  NRHS >= 0.
-
-  A       (input or input/ouptut) DOUBLE PRECISION array,
-          dimension (LDA,N)
-          On entry, the N-by-N coefficient matrix A.
-          On exit, if iterative refinement has been successfully used
-          (INFO.EQ.0 and ITER.GE.0, see description below), then A is
-          unchanged, if double precision factorization has been used
-          (INFO.EQ.0 and ITER.LT.0, see description below), then the
-          array A contains the factors L and U from the factorization
-          A = P*L*U; the unit diagonal elements of L are not stored.
-
-  LDA     (input) INTEGER
-          The leading dimension of the array A.  LDA >= max(1,N).
-
-  IPIV    (output) INTEGER array, dimension (N)
-          The pivot indices that define the permutation matrix P;
-          row i of the matrix was interchanged with row IPIV(i).
-          Corresponds either to the single precision factorization
-          (if INFO.EQ.0 and ITER.GE.0) or the double precision
-          factorization (if INFO.EQ.0 and ITER.LT.0).
-
-  B       (input) DOUBLE PRECISION array, dimension (LDB,NRHS)
-          The N-by-NRHS right hand side matrix B.
-
-  LDB     (input) INTEGER
-          The leading dimension of the array B.  LDB >= max(1,N).
-
-  X       (output) DOUBLE PRECISION array, dimension (LDX,NRHS)
-          If INFO = 0, the N-by-NRHS solution matrix X.
-
-  LDX     (input) INTEGER
-          The leading dimension of the array X.  LDX >= max(1,N).
-
-  WORK    (workspace) DOUBLE PRECISION array, dimension (N*NRHS)
-          This array is used to hold the residual vectors.
-
-  SWORK   (workspace) REAL array, dimension (N*(N+NRHS))
-          This array is used to use the single precision matrix and the
-          right-hand sides or solutions in single precision.
-
-  ITER    (output) INTEGER
-          < 0: iterative refinement has failed, double precision
-               factorization has been performed
-               -1 : the routine fell back to full precision for
-                    implementation- or machine-specific reasons
-               -2 : narrowing the precision induced an overflow,
-                    the routine fell back to full precision
-               -3 : failure of SGETRF
-               -31: stop the iterative refinement after the 30th
-                    iterations
-          > 0: iterative refinement has been sucessfully used.
-               Returns the number of iterations
+/*  -- MAGMA (version 0.2) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2009
 
 
-  INFO    (output) INTEGER
-          = 0:  successful exit
-          < 0:  if INFO = -i, the i-th argument had an illegal value
-          > 0:  if INFO = i, U(i,i) computed in DOUBLE PRECISION is
-                exactly zero.  The factorization has been completed,
-                but the factor U is exactly singular, so the solution
-                could not be computed.
+    Purpose
+    =======
 
-  H_SWORK    (workspace) REAL array, dimension at least (nb, nb)
-          where nb can be obtained through magma_get_spotrf_nb(*n)
-          Work array allocated with cudaMallocHost.
+    DSGESV computes the solution to a real system of linear equations
+       A * X = B,
+    where A is an N-by-N matrix and X and B are N-by-NRHS matrices.
 
-  H_WORK   (workspace) DOUBLE array, dimension at least (nb, nb)
-           where nb can be obtained through magma_get_dpotrf_nb(*n)
-           Work array allocated with cudaMallocHost.
+    DSGESV first attempts to factorize the matrix in SINGLE PRECISION
+    and use this factorization within an iterative refinement procedure
+    to produce a solution with DOUBLE PRECISION normwise backward error
+    quality (see below). If the approach fails the method switches to a
+    DOUBLE PRECISION factorization and solve.
 
-  DIPIV   (output) INTEGER array on the GPU, dimension (min(M,N))
+    The iterative refinement is not going to be a winning strategy if
+    the ratio SINGLE PRECISION performance over DOUBLE PRECISION
+    performance is too small. A reasonable strategy should take the
+    number of right-hand sides and the size of the matrix into account.
+    This might be done with a call to ILAENV in the future. Up to now, we
+    always try iterative refinement.
+    The iterative refinement process is stopped if
+        ITER > ITERMAX
+    or for all the RHS we have:
+        RNRM < SQRT(N)*XNRM*ANRM*EPS*BWDMAX
+    where
+        o ITER is the number of the current iteration in the iterative
+          refinement process
+        o RNRM is the infinity-norm of the residual
+        o XNRM is the infinity-norm of the solution
+        o ANRM is the infinity-operator-norm of the matrix A
+        o EPS is the machine epsilon returned by DLAMCH('Epsilon')
+    The value ITERMAX and BWDMAX are fixed to 30 and 1.0D+00 respectively.
+
+    Arguments
+    =========
+
+    N       (input) INTEGER
+            The number of linear equations, i.e., the order of the
+            matrix A.  N >= 0.
+
+    NRHS    (input) INTEGER
+            The number of right hand sides, i.e., the number of columns
+            of the matrix B.  NRHS >= 0.
+
+    A       (input or input/ouptut) DOUBLE PRECISION array,
+            dimension (LDA,N)
+            On entry, the N-by-N coefficient matrix A.
+            On exit, if iterative refinement has been successfully used
+            (INFO.EQ.0 and ITER.GE.0, see description below), then A is
+            unchanged, if double precision factorization has been used
+            (INFO.EQ.0 and ITER.LT.0, see description below), then the
+            array A contains the factors L and U from the factorization
+            A = P*L*U; the unit diagonal elements of L are not stored.
+
+    LDA     (input) INTEGER
+            The leading dimension of the array A.  LDA >= max(1,N).
+
+    IPIV    (output) INTEGER array, dimension (N)
+            The pivot indices that define the permutation matrix P;
+            row i of the matrix was interchanged with row IPIV(i).
+            Corresponds either to the single precision factorization
+            (if INFO.EQ.0 and ITER.GE.0) or the double precision
+            factorization (if INFO.EQ.0 and ITER.LT.0).
+
+    B       (input) DOUBLE PRECISION array, dimension (LDB,NRHS)
+            The N-by-NRHS right hand side matrix B.
+
+    LDB     (input) INTEGER
+            The leading dimension of the array B.  LDB >= max(1,N).
+
+    X       (output) DOUBLE PRECISION array, dimension (LDX,NRHS)
+            If INFO = 0, the N-by-NRHS solution matrix X.
+
+    LDX     (input) INTEGER
+            The leading dimension of the array X.  LDX >= max(1,N).
+
+    WORK    (workspace) DOUBLE PRECISION array, dimension (N*NRHS)
+            This array is used to hold the residual vectors.
+
+    SWORK   (workspace) REAL array, dimension (N*(N+NRHS))
+            This array is used to use the single precision matrix and the
+            right-hand sides or solutions in single precision.
+
+    ITER    (output) INTEGER
+            < 0: iterative refinement has failed, double precision
+                 factorization has been performed
+                 -1 : the routine fell back to full precision for
+                      implementation- or machine-specific reasons
+                 -2 : narrowing the precision induced an overflow,
+                      the routine fell back to full precision
+                 -3 : failure of SGETRF
+                 -31: stop the iterative refinement after the 30th
+                      iterations
+            > 0: iterative refinement has been sucessfully used.
+                 Returns the number of iterations
+ 
+    INFO    (output) INTEGER
+            = 0:  successful exit
+            < 0:  if INFO = -i, the i-th argument had an illegal value
+            > 0:  if INFO = i, U(i,i) computed in DOUBLE PRECISION is
+                  exactly zero.  The factorization has been completed,
+                  but the factor U is exactly singular, so the solution
+                  could not be computed.
+
+    H_SWORK (workspace) REAL array, dimension at least (nb, nb)
+            where nb can be obtained through magma_get_spotrf_nb(*n)
+            Work array allocated with cudaMallocHost.
+
+    H_WORK  (workspace) DOUBLE array, dimension at least (nb, nb)
+            where nb can be obtained through magma_get_dpotrf_nb(*n)
+            Work array allocated with cudaMallocHost.
+
+    DIPIV   (output) INTEGER array on the GPU, dimension (min(M,N))
             The pivot indices; for 1 <= i <= min(M,N), row i of the
             matrix was moved to row IPIV(i).
 
-  =========
-*/
+    =====================================================================    */
 
+  #define MAX(a,b)       (((a)>(b))?(a):(b))
 
   /*
     Check The Parameters. 
@@ -302,32 +293,8 @@ unnecessary may be*/
 
   magma_dgetrf_gpu(&N, &N, A, &LDA, IPIV, H_WORK, INFO);
   magma_dlacpy(N, NRHS, B , LDB, X, N);
-  magma_dgetrs_v2("N",N ,NRHS, A ,LDA,IPIV, X,N,INFO,H_WORK);
+  magma_dgetrs_gpu("N",N ,NRHS, A ,LDA,IPIV, X,N,INFO,H_WORK);
   return 0;
-}
-
-void magma_dgetrs_v2(char *TRANS , int N , int NRHS, double *A , int LDA , 
-		     int *IPIV , double *B, int LDB, int *INFO, double *BB1 ){
-  cublasGetMatrix( N, NRHS, sizeof(double), B,N , BB1 , N ) ;
-  int k1 = 1 ;
-  int k2 = N;
-  int k3 = 1 ;
-  dlaswp_(&NRHS,BB1,&LDB , &k1, &k2, IPIV ,&k3) ;
-  cublasSetMatrix( N, NRHS, sizeof(double), BB1, N , B , N ) ;
-  magmablas_dtrsm('L','L','N','U', N , NRHS, 1.0, A , LDA , B , LDB );
-  magmablas_dtrsm('L','U','N','N', N , NRHS, 1.0, A , LDA , B , LDB );
-}
-
-void magma_sgetrs_v2(char *TRANS , int N , int NRHS, float *A , int LDA , 
-		     int *IPIV , float *B, int LDB, int *INFO, float *BB1 ){
-  cublasGetMatrix( N, NRHS, sizeof(float), B,N , BB1 , N ) ;
-  int k1 = 1 ;
-  int k2 = N;
-  int k3 = 1 ;
-  slaswp_(&NRHS,BB1,&LDB , &k1, &k2, IPIV ,&k3);
-  cublasSetMatrix( N, NRHS, sizeof(float), BB1, N , B , N ) ;
-  cublasStrsm('L','L','N','U', N , NRHS, 1.0, A , LDA , B , LDB );
-  cublasStrsm('L','U','N','N', N , NRHS, 1.0, A , LDA , B , LDB );
 }
 
 #undef MAX
