29cf8c3213e0ac83be8b27fad2a401a10feb8017
Mathieu Faverge <mathieu.faverge@inria.fr>
2010-10-21 01:50:36 +0000

    Clean magma_z.h
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@194 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- include/magma.h
+++ include/magma.h
@@ -20,7 +20,6 @@ typedef int magma_int_t;
 #include "magma_d.h"
 #include "magma_s.h"
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -28,95 +27,7 @@ extern "C" {
 /* ////////////////////////////////////////////////////////////////////////////
    -- MAGMA function definitions
 */
-
-
-int magma_slarfb(char, char, int, int, int *, float *, int *, float *,
-                 int *, float *, int *, float *, int *);
-int magma_sgeqrf(int *, int *, float *, int  *,  float  *,
-		 float *, int *, float *, int *);
-int magma_sgeqrf2(int *, int *, float *, int  *,  float  *,
-		  float *, int *, int *);
-int magma_sgeqrf_gpu(int *, int *, float *, int  *,  float  *,
-		     float *, int *, float *, int *);
-int magma_sgeqrf_gpu2(int *, int *, float *, int  *,  float  *,
-		      float *, int *, float *, int *);
-int magma_sgeqrf_gpu3(int *, int *, float *, int  *,  float  *,
-		      float *, int *, float *, int *);
-int magma_sgeqrs_gpu(int *, int *, int *, float *, int *, float *, float *,
-                     int *, float *, int *, float *, int *);
-int magma_sgeqlf(int *, int *, float *, int  *,  float  *,
-                 float *, int *, float *, int *);
-int magma_sgelqf(int *, int *, float *, int  *,  float  *,
-                 float *, int *, float *, int *);
-int magma_sgelqf2(int *, int *, float *, int  *,  float  *,
-		  float *, int *, float *, int *);
-
-int magma_sgetrs_gpu(char *, int, int, float *, int,
-		     int *, float *, int, int *, float *);
-int magma_sgehrd(int *, int *, int *, float *, int *, float *, float *,
-                 int *, float *, int *);
-int magma_slahr2(int *, int *, int *, float *, float *, float *, int *,
-                 float *, float *, int *, float *, int *);
-int magma_slahru(int, int, int,  float *, int,
-                 float *, float *, float *, float *, float *);
-int magma_ssytrd(char *, int *, float *, int *, float *, float *, 
-		 float *, float *, int *, float *, int *);
-int magma_sgebrd(int *, int *, float *, int *, float *, float *, float *, 
-		 float *, float *, int *, float *, int *);
-int magma_slabrd(int *, int *, int *, float *, int *, float *, float *, 
-		 float *, float *, float *, int *, float *, int *,
-		 float *, int *, float *, int *, float *, int *);
-
-
-int magma_dlarfb(char, char, int, int, int *, double *, int *, double *,
-                 int *, double *, int *, double *, int *);
-int magma_dgeqrf(int *, int *, double *, int  *,  double  *,
-		 double *, int *, double *, int *);
-int magma_dgeqrf_gpu(int *, int *, double *, int  *, double  *,
-		     double *, int *, double *, int *);
-int magma_dgeqrf_gpu2(int *, int *, double *, int  *, double  *,
-                      double *, int *, double *, int *);
-int magma_dgeqrs_gpu(int *, int *, int *, double *, int *, double *, double *,
-                     int *, double *, int *, double *, int *);
-int magma_dgeqlf(int *, int *, double *, int  *, double  *,
-		 double *, int *, double *, int *);
-int magma_dgelqf(int *, int *, double *, int  *, double *,
-		 double *, int *, double *, int *);
-int magma_dgelqf2(int *, int *, double *, int  *, double *,
-		  double *, int *, double *, int *);
-int magma_dgetrs_gpu(char *, int, int, double *, int,
-                     int *, double *, int, int *, double *);
-int magma_dgehrd(int *, int *, int *, double *, int *, double *, double *,
-		 int *, double *, int *);
-int magma_dlahr2(int *, int *, int *, double *, double *, double *, int *,
-                 double *, double *, int *, double *, int *);
-int magma_dlahru(int, int, int,  double *, int,
-		 double *, double *, double *, double *, double *);
-int magma_dsytrd(char *, int *, double *, int *, double *, double *,
-		 double *, double *, int *, double *, int *);
-
-int magma_clarfb(char, char, int, int, int *, float2 *, int *, float2 *,
-                 int *, float2 *, int *, float2 *, int *);
-int magma_cgeqrf(int *, int *, float2 *, int  *,  float2  *,
-                 float2 *, int *, float2 *, int *);
-int magma_cgeqrf_gpu(int *, int *, float2 *, int  *,  float2  *,
-		     float2 *, int *, float2 *, int *);
-
-int magma_sdgetrs_gpu(int *n, int *nrhs, float *a, int *lda,
-                  int *ipiv, float *x, double *b, int *ldb, int *info);
-int magma_dsgesv_gpu(int, int, double *, int, int *, double *, int, double *,
-		     int, double *, float *, int *, int *, float *, double *,
-		     int *);
-int magma_spotrs_gpu(char *, int, int, float *, int, float *, int, int *);
-int magma_dpotrs_gpu(char *, int, int, double *, int, double *, int, int *);
-int magma_dsposv_gpu(char, int, int, double *, int, double *, int, double *,
-		     int, double *, float *, int *, int *, float *, double *);
-
-void magma_xerbla(char *name , int *info);
-
-int magma_dsgeqrsv_gpu(int, int, int, double *, int, double *, int, double *,
-		       int, double *, float *, int *, int *, float *, int, 
-		       float *, float *, double *, int, double *, double *);
+void magma_xerbla(char *name , magma_int_t *info);
 
 #ifdef __cplusplus
 }
--- include/magma_z.h
+++ include/magma_z.h
@@ -6,11 +6,8 @@
        November 2010
 */
 
-#ifndef _MAGMA_
-#define _MAGMA_
-
-#include "auxiliary.h"
-#include "magmablas.h"
+#ifndef _MAGMA_Z_H_
+#define _MAGMA_Z_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,40 +16,54 @@ extern "C" {
 /* ////////////////////////////////////////////////////////////////////////////
    -- MAGMA function definitions / Data on CPU
 */
-magma_int_t magma_zpotrf( char *, magma_int_t, double2 *, magma_int_t, double2 *, magma_int_t*);
-magma_int_t magma_zpotrf2(char *, magma_int_t, double2 *, magma_int_t,            magma_int_t*);
-magma_int_t magma_zpotrf3(char *, magma_int_t, double2 *, magma_int_t, double2 *, magma_int_t*);
+magma_int_t magma_zgebrd( magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, double2 *d, double2 *e, double2 *tauq,  double2 *taup, double2 *work, magma_int_t lwork, double2 *da, magma_int_t *info);
+magma_int_t magma_zgehrd( magma_int_t n, magma_int_t ilo, magma_int_t ihi, double2 *A, magma_int_t lda, double2 *tau, double2 *work, magma_int_t lwork, double2 *da, magma_int_t *info);
+magma_int_t magma_zgelqf( magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, double2 *tau, double2 *work, magma_int_t lwork, double2 *da, magma_int_t *info);
+magma_int_t magma_zgelqf2(magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, double2 *tau, double2 *work, magma_int_t lwork, double2 *da, magma_int_t *info);
+magma_int_t magma_zgeqlf( magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, double2 *tau, double2 *work, magma_int_t lwork, double2 *da, magma_int_t *info);
+magma_int_t magma_zgeqrf( magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, double2 *tau, double2 *work, magma_int_t lwork, double2 *da, magma_int_t *info);
+magma_int_t magma_zgeqrf2(magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, double2 *tau, double2 *work, magma_int_t lwork,              magma_int_t *info);
+magma_int_t magma_zgetrf( magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, magma_int_t *ipiv, double2 *work, double2 *da, magma_int_t *info);
+magma_int_t magma_zgetrf2(magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, magma_int_t *ipiv, double2 *work,              magma_int_t *info);
+magma_int_t magma_zlabrd( magma_int_t m, magma_int_t n, magma_int_t nb, 
+			  double2 *a,  magma_int_t lda,  double2 *d,  double2 * e, 
+			  double2 *tauq, double2 *taup,  double2 *x,  magma_int_t ldx,  
+			  double2 *y,  magma_int_t ldy,  double2 *da, magma_int_t ldda, 
+			  double2 *dx, magma_int_t lddx, double2 *dy, magma_int_t *lddy);
+magma_int_t magma_zlahr2( magma_int_t m, magma_int_t n, magma_int_t nb, 
+			  double2 *da, double2 *dv, double2 *a, magma_int_t lda, double2 *tau, 
+			  double2 *t, magma_int_t ldt, double2 *y, magma_int_t ldy);
+magma_int_t magma_zlahru( magma_int_t m, magma_int_t n, magma_int_t nb, double2 *a, magma_int_t lda, 
+			  double2 *da, double2 *y, double2 *v, double2 *t, double2 *dwork);
+magma_int_t magma_zlarfb( char direct, char storev, magma_int_t m, magma_int_t n, magma_int_t k, 
+			  double2 *dv, magma_int_t ldv, double2 *dt,    magma_int_t ldt, 
+			  double2 *dc, magma_int_t ldc, double2 *dowrk, magma_int_t ldwork);
+magma_int_t magma_zpotrf( char uplo, magma_int_t n, double2 *A, magma_int_t lda, double2 *work, magma_int_t *info);
+magma_int_t magma_zpotrf2(char uplo, magma_int_t n, double2 *A, magma_int_t lda,                magma_int_t *info);
+magma_int_t magma_zpotrf3(char uplo, magma_int_t n, double2 *A, magma_int_t lda, double2 *work, magma_int_t *info);
+magma_int_t magma_zsytrd( char uplo, magma_int_t n, double2 *A, magma_int_t lda, 
+			  double2 *d, double2 *e, double2 *tau, 
+			  double2 *work, magma_int_t lwork, double2 *da, magma_int_t *info);
 
-magma_int_t magma_zgetrf( magma_int_t, magma_int_t, double2 *, magma_int_t, 
-			  magma_int_t *, double2 *, double2 *, magma_int_t*);
-magma_int_t magma_zgetrf2(magma_int_t, magma_int_t, double2 *, magma_int_t, 
-			  magma_int_t *, double2 *, magma_int_t*);
-
-
-
-magma_int_t magma_zlarfb(char, char, magma_int_t, magma_int_t, magma_int_t *, double2 *, magma_int_t *, double2 *,
-			 magma_int_t *, double2 *, magma_int_t *, double2 *, magma_int_t *);
-magma_int_t magma_zgeqrf(magma_int_t *, magma_int_t *, double2 *, magma_int_t  *,  double2  *,
-                 double2 *, magma_int_t *, double2 *, magma_int_t *);
-
-
-/* ////////////////////////////////////////////////////////////////////////////
-   -- MAGMA function definitions / Data on GPU
+/* //////////////////////////////////////////////////////////////////////////// 
+ -- MAGMA function definitions / Data on GPU
 */
-magma_int_t magma_zpotrf_gpu(char *, magma_int_t, double2 *, magma_int_t, double2 *, magma_int_t*);
+magma_int_t magma_zgeqrf_gpu( magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, double2 *tau, double2 *work, magma_int_t lwork, double2 *dwork, magma_int_t *info);
+magma_int_t magma_zgeqrf_gpu2(magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, double2 *tau, double2 *work, magma_int_t lwork, double2 *dwork, magma_int_t *info);
+magma_int_t magma_zgeqrf_gpu3(magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, double2 *tau, double2 *work, magma_int_t lwork, double2 *dwork, magma_int_t *info);
+magma_int_t magma_zgeqrs_gpu( magma_int_t m, magma_int_t n, magma_int_t nrhs, double2 *A, magma_int_t lda, double2 *tau, double2 *c, magma_int_t ldc, double2 *work, magma_int_t lwork, double2 *td, magma_int_t *info);
+magma_int_t magma_zgetrf_gpu( magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, magma_int_t *ipiv,                     double2 *work, magma_int_t *info);
+magma_int_t magma_zgetrf_gpu2(magma_int_t m, magma_int_t n, double2 *A, magma_int_t lda, magma_int_t *ipiv, magma_int_t *dipiv, double2 *work, magma_int_t *info);
+magma_int_t magma_zgetrs_gpu( char trans, magma_int_t n, magma_int_t nrhs, double2 *A, magma_int_t lda, magma_int_t *ipiv, double2 *b, magma_int_t ldb, magma_int_t *info, double2 *hwork);
+magma_int_t magma_zpotrf_gpu( char uplo,  magma_int_t n, double2 *A, magma_int_t lda, double2 *work, magma_int_t *info);
+magma_int_t magma_zpotrs_gpu( char uplo,  magma_int_t n, magma_int_t nrhs, double2 *A, magma_int_t lda, double2 *b, magma_int_t ldb, magma_int_t *info);
 
-magma_int_t magma_zgetrf_gpu( magma_int_t, magma_int_t, double2 *, magma_int_t, 
-			      magma_int_t *, double2 *, magma_int_t *);
-magma_int_t magma_zgetrf_gpu2(magma_int_t, magma_int_t, double2 *, magma_int_t, 
-			      magma_int_t *, double2 *, double2 *, magma_int_t*);
-
-
-magma_int_t magma_zgeqrf_gpu(magma_int_t *, magma_int_t *, double2 *, magma_int_t  *, double2  *,
-		     double2 *, magma_int_t *, double2 *, magma_int_t *);
+  /* Mixed precision */
+magma_int_t magma_czgetrs_gpu(magma_int_t *n, magma_int_t *nrhs, float2 *a, magma_int_t lda, magma_int_t *ipiv, float2 *x, double2 *b, magma_int_t ldb, magma_int_t *info);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* _MAGMA_Z_H_ */
 
