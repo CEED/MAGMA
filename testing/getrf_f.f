d492cf38ce386d311e930f2390bc05f119c81be9
Stanimire Tomov <tomov@icl.utk.edu>
2010-10-31 19:09:59 +0000

    I figured our how to call magma from fortran but now everything is a mess. I can not figure out how to fix the genrators ... in testing. These two files work in single precision. I compile them with
    gcc -g3 -I/usr/local/cuda-2.3/src/ -I/usr/local/cuda-2.3/include -c fortran2.cpp -o fortran2.o
    
    gfortran -fPIC getrf_f.o -o getrf_f lin/liblapacktest.a -L../lib -lcuda -lmagma -lmagmablas -L/home/tomov/intel/mkl/10.0.1.014/lib/em64t -L/usr/local/cuda-2.3/lib64 -lmkl_em64t -lguide -lpthread -lcublas -lcudart -lm get_nb.cpp fortran2.o
    
    I changed the name from testing_... to try to trick the scripts not to go
    through these files but now nothing works - even make clean.
    
    To be done:
    1) fix the scripts
    2) figure out in what file to put
       subroutine cublasAlloc
       subroutine cudaMallocHost
       ...
       and the module magma
    3) The way is now cublasAlloc and cudaMallocHost and others
       (if we code them similarly) depend on the type. For example
       these handle float. How to make them general?
       e.g., pointers like
       real*8, pointer, dimension(:)::ptr
       would have to be different. I am not sure, are pointers in
       fortran some kind of integers. Can we change the definition with
       something else
    4) Start writing the example.
       I am going to say that we do it in the following way
    
    !--- use the magma module
       use magma
    
    !--- declare memory the ususal fortran way
       integer, allocatable :: ipiv(:)
    
    !--- declare GPU pointers and pinned memory pointers (as needed ...)
       real*8 pointer :: h_A(:), d_A(:)
    
    !--- Allocate pinned memory (as needed ...)
       call cublasMallocHost(h_A, LDA*N*sizeof_real)
    
    !--- Allocate GPU memory (as needed ...)
       call cublasAlloc(LDA*N, sizeof_real, d_A)
    
    !--- Allocate on the CPU
       allocate(ipiv(min(M,N)))
    
     ...
    
    !--- Call magma
       call magma_sgetrf(M, N, h_A, lda, ipiv, info)
    
     ...
    
    
    
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@258 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ testing/getrf_f.f
@@ -0,0 +1,121 @@
+!
+!   -- MAGMA (version 1.0) --
+!      Univ. of Tennessee, Knoxville
+!      Univ. of California, Berkeley
+!      Univ. of Colorado, Denver
+!      November 2010
+!
+!  @generated s
+!
+
+c==================================================================
+
+      subroutine cudaMallocHost(ptr, n)
+
+      integer, intent(in)        :: n
+      real*4,pointer,dimension(:):: ptr
+
+      integer :: sizeof_real = 4
+      integer*8 off
+      real*4, TARGET :: phony(1)
+
+      call cublasMallochost_f( phony(1), n, off)
+      ptr => phony(off/sizeof_real+1:)
+
+      end subroutine
+
+c==================================================================
+
+      subroutine cublasAlloc(n, size, ptr)
+
+      integer, intent(in)        :: n
+      integer, intent(in)        :: size
+      real*4,pointer,dimension(:):: ptr
+
+      integer*8 off
+      real*4, TARGET :: phony(1)
+
+      call cublasAlloc_f(n, size, phony(1), off)
+      ptr => phony(off+1:)
+
+      end subroutine
+
+c==================================================================
+
+      module magma
+      interface magmaAlloc
+         subroutine cublasAlloc(n, size, ptr)
+           integer, intent(in)        :: n
+           integer, intent(in)        :: size
+           real*4,pointer,dimension(:):: ptr
+         end subroutine
+
+         subroutine cudaMallocHost(ptr, n)
+           integer, intent(in)        :: n
+           real*4,pointer,dimension(:):: ptr
+         end subroutine
+      end interface
+      end module
+
+c==================================================================
+
+      program testing_sgetrf_f
+
+      use magma
+
+      real*4 rnumber(2), matnorm
+      real*4, allocatable,dimension(:) :: h_A(:)
+      integer,allocatable,dimension(:) :: ipiv
+
+      real*4, pointer :: h_R(:)
+
+      integer i, n, nb, info, lda
+      integer :: sizeof_real = 4
+
+      n = 2048
+      nb = 128
+      lda = n
+
+!------ Initialize CUBLAS --------------------------
+      call cublas_init( )
+
+!------ Allocate CPU memory ------------------------
+      allocate(h_A(n*n), ipiv(n))
+
+!------ Allocate CPU pinned memory -----------------
+      call cudaMallocHost(h_R, n*n*sizeof_real)
+
+!------ Initialize the matrix ----------------------
+      do i=1,n*n
+        call random_number(rnumber)
+        h_A(i) = rnumber(1)
+        h_R(i) = rnumber(1)
+      end do
+
+!------ Call magma ---------------------------------
+      call magma_sgetrf(N, N, h_R, lda, ipiv, info)
+
+!------ Call LAPACK --------------------------------
+      call sgetrf(N, N, h_A, lda, ipiv, info)
+
+!------ Compare the two results --------------------
+      do i=1,n*n
+         h_R(i) = h_R(i) - h_A(i)
+      end do
+
+      matnorm = 0
+      do i=1,n*n
+         if (matnorm < abs(h_R(i))) then
+            matnorm = h_R(i)
+         end if
+      end do
+
+      write(*,105) 'error = ', matnorm
+ 105  format((a10,es10.3))
+
+      call cublas_free(h_A)
+      deallocate(h_A, ipiv)
+
+      call cublas_shutdown()
+
+      end
