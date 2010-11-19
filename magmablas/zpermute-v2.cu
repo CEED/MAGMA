878b6a7717e788684e23cdfac0c6bb878afb2630
Mathieu Faverge <mathieu.faverge@inria.fr>
2010-11-19 06:01:33 +0000

    Fix zswap
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@383 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- magmablas/zpermute-v2.cu
+++ magmablas/zpermute-v2.cu
@@ -29,18 +29,18 @@ typedef struct {
 typedef struct {
   cuDoubleComplex *A1;
   cuDoubleComplex *A2;
-  int n; /*, lda1, lda2;*/
+  int n, lda1, lda2;
 } swap_params_t;
 
 __global__ void myzswap( swap_params_t params )
 {
-  unsigned int tid = threadIdx.x + __mul24(blockDim.x, blockIdx.x);
-  if( tid < params.n )
+  unsigned int x = threadIdx.x + blockDim.x*blockIdx.x;
+  unsigned int offset1 = __mul24( x, params.lda1);
+  unsigned int offset2 = __mul24( x, params.lda2);
+  if( x < params.n )
     {
-/*       int lda1 = params.lda1; */
-/*       int lda2 = params.lda2; */
-      cuDoubleComplex *A1 = params.A1 + tid;
-      cuDoubleComplex *A2 = params.A2 + tid;
+      cuDoubleComplex *A1 = params.A1 + offset1;
+      cuDoubleComplex *A2 = params.A2 + offset2;
       cuDoubleComplex temp = *A1;
       *A1 = *A2;
       *A2 = temp;
@@ -129,10 +129,11 @@ extern "C" void
 magmablas_zswap( int n, cuDoubleComplex *dA1T, int lda1, 
                  cuDoubleComplex *dA2T, int lda2)
 {
-  swap_params_t params = { dA1T, dA2T, n }; /*, lda1, lda2 };*/
-  int  blocksize = 64;
-  dim3 blocks = (params.n+blocksize-1) / blocksize;
-  myzswap<<< blocks, blocksize >>>( params );
+    swap_params_t params = { dA1T, dA2T, n, lda1, lda2 };
+    int  blocksize = 64;
+    int  blocks = (params.n+blocksize-1) / blocksize;
+    
+    myzswap<<< blocks, blocksize >>>( params );
 }
 
 #undef BLOCK_SIZE
