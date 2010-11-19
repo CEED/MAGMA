66b0090e8fae338f9bbb13fb20c1dbcab578014f
horton <horton@mycompany.com>
2010-11-19 19:13:39 +0000

    QR for multicore + GPU.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@385 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ src/zgeqrf-v3.cpp
@@ -0,0 +1,172 @@
+/*
+    -- MAGMA (version 1.0) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2010
+
+       @precisions normal z -> s d c
+
+*/
+
+#include <stdio.h>
+#include <cuda_runtime_api.h>
+#include <cublas.h>
+#include "magma.h"
+
+#define min(a,b)  (((a)<(b))?(a):(b))
+#define max(a,b)  (((a)>(b))?(a):(b))
+
+#include <pthread.h>
+
+typedef struct {
+  int nthreads;
+  int nb;
+  int np_gpu;
+  int m;
+  int n;
+  int lda;
+  cuDoubleComplex *a;
+  cuDoubleComplex *t;
+  pthread_t *thread;
+  cuDoubleComplex **p;
+} MAGMA_GLOBALS;
+
+
+
+extern "C" MAGMA_GLOBALS MG;
+
+extern "C" magma_int_t
+magma_zgeqrf3(magma_int_t m, magma_int_t n, 
+             cuDoubleComplex *a,    magma_int_t lda, cuDoubleComplex *tau, 
+             cuDoubleComplex *work, magma_int_t lwork,
+             magma_int_t *info )
+{
+/*  -- MAGMA (version 1.0) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2010
+
+    Purpose   
+    =======   
+
+    SGEQRF computes a QR factorization of a real M-by-N matrix A:   
+    A = Q * R.   
+
+    Arguments   
+    =========   
+
+    M       (input) INTEGER   
+            The number of rows of the matrix A.  M >= 0.   
+
+    N       (input) INTEGER   
+            The number of columns of the matrix A.  N >= 0.   
+
+    A       (input/output) REAL array, dimension (LDA,N)   
+            On entry, the M-by-N matrix A.   
+            On exit, the elements on and above the diagonal of the array   
+            contain the min(M,N)-by-N upper trapezoidal matrix R (R is   
+            upper triangular if m >= n); the elements below the diagonal,   
+            with the array TAU, represent the orthogonal matrix Q as a   
+            product of min(m,n) elementary reflectors (see Further   
+            Details).   
+
+    LDA     (input) INTEGER   
+            The leading dimension of the array A.  LDA >= max(1,M).   
+
+    TAU     (output) REAL array, dimension (min(M,N))   
+            The scalar factors of the elementary reflectors (see Further   
+            Details).   
+
+    WORK    (workspace/output) REAL array, dimension (MAX(1,LWORK))   
+            On exit, if INFO = 0, WORK(1) returns the optimal LWORK.   
+
+    LWORK   (input) INTEGER   
+            The dimension of the array WORK.  LWORK >= N*NB. 
+
+            If LWORK = -1, then a workspace query is assumed; the routine   
+            only calculates the optimal size of the WORK array, returns   
+            this value as the first entry of the WORK array, and no error   
+            message related to LWORK is issued.
+
+    INFO    (output) INTEGER   
+            = 0:  successful exit   
+            < 0:  if INFO = -i, the i-th argument had an illegal value   
+
+    Further Details   
+    ===============   
+
+    The matrix Q is represented as a product of elementary reflectors   
+
+       Q = H(1) H(2) . . . H(k), where k = min(m,n).   
+
+    Each H(i) has the form   
+
+       H(i) = I - tau * v * v'   
+
+    where tau is a real scalar, and v is a real vector with   
+    v(1:i-1) = 0 and v(i) = 1; v(i+1:m) is stored on exit in A(i+1:m,i),   
+    and tau in TAU(i).   
+
+    ====================================================================    */
+
+/*
+
+  int i,j,l;
+
+  int ii=-1,jj=-1,ll=-1;
+
+  char sgeqrt_dag_label[1000];
+  char slarfb_dag_label[1000];
+
+  *info = 0;
+
+  int K;
+
+  work[0] = (float) (*n)*MG.nb;
+  long int lquery = *lwork == -1;
+
+  // check arguments
+  if (*m < 0) {
+    *info = -1;
+  } else if (*n < 0) {
+    *info = -2;
+  } else if (*lda < max(1,*m)) {
+    *info = -4;
+  } else if (*lwork < max(1,*n) && ! lquery) {
+    *info = -7;
+  }
+  if (*info != 0)
+    return 0;
+  else if (lquery)
+    return 0;
+
+  int k = min(*m,*n);
+  if (k == 0) {
+    work[0] = 1.f;
+    return 0;
+  }
+
+*/
+
+//fprintf(stderr,"fjfj\n");
+
+int k;
+
+int M=MG.nthreads*MG.nb;
+int N=MG.nthreads*MG.nb;
+
+magma_zgeqrf2(m,n-MG.nthreads*MG.nb, a, m, tau, work, lwork, info);
+
+for (k = 0; k < MG.nthreads; k++){
+  pthread_join(MG.thread[k], NULL);
+}
+
+  magma_zgeqrf2(M, N, a+(n-MG.nthreads*MG.nb)*m+(n-MG.nthreads*MG.nb), m, 
+               &tau[n-MG.nthreads*MG.nb], work, lwork, info);
+
+}
+
+#undef min
+#undef max
