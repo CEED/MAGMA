0bf0f8b3d5d7ef141af9755b788d34b2d9a628e7
Mathieu Faverge <mathieu.faverge@inria.fr>
2010-11-20 23:33:54 +0000

    Split the different swap in different files and apparently it fixes the bug on the tile LU
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@398 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ magmablas/zswap.cu
@@ -0,0 +1,64 @@
+/*
+    -- MAGMA (version 1.0) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2010
+
+       @precisions normal z -> s d c
+
+*/
+
+#include <stdio.h>
+#include <cublas.h>
+
+#define BLOCK_SIZE 64
+
+/*********************************************************
+ *
+ * SWAP BLAS: permute to set of N elements
+ *
+ ********************************************************/
+/*
+ *  First version: line per line
+ */
+typedef struct {
+    cuDoubleComplex *A1;
+    cuDoubleComplex *A2;
+    int n; /*, lda1, lda2;*/
+} zswap_params_t;
+
+__global__ void myzswap( zswap_params_t params )
+{
+    unsigned int x = threadIdx.x + blockDim.x*blockIdx.x;
+    /* unsigned int offset1 = __mul24( x, params.lda1); */
+    /* unsigned int offset2 = __mul24( x, params.lda2); */
+    if( x < params.n )
+    {
+        cuDoubleComplex *A1  = params.A1 + x; /*offset1;*/
+        cuDoubleComplex *A2  = params.A2 + x; /*offset2;*/
+        cuDoubleComplex temp = *A1;
+        *A1 = *A2;
+        *A2 = temp;
+    }
+}
+
+extern "C" void zswap( zswap_params_t &params )
+{
+    int blocksize = 64;
+    dim3 blocks = (params.n+blocksize-1) / blocksize;
+    myzswap<<< blocks, blocksize >>>( params );
+}
+
+
+extern "C" void 
+magmablas_zswap( int n, cuDoubleComplex *dA1T, int lda1, 
+                 cuDoubleComplex *dA2T, int lda2)
+{
+    zswap_params_t params = { dA1T, dA2T, n }; /*, lda1, lda2 };*/
+/*     int  blocksize = 64; */
+/*     int  blocks = (params.n+blocksize-1) / blocksize; */
+    
+    zswap( params );
+}
+
---
+++ magmablas/zswapblk.cu
@@ -0,0 +1,80 @@
+/*
+    -- MAGMA (version 1.0) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2010
+
+       @precisions normal z -> s d c
+
+*/
+
+#include <stdio.h>
+#include <cublas.h>
+
+#define BLOCK_SIZE 64
+
+/*********************************************************/
+/*
+*  Blocked version: swap several pair of line
+ */
+typedef struct {
+    cuDoubleComplex *A1;
+    cuDoubleComplex *A2;
+    int n, ldx1, ldx2, ldy1, ldy2, npivots;
+    short ipiv[BLOCK_SIZE];
+} zswapblk_params_t;
+
+__global__ void myzswapblk( zswapblk_params_t params )
+{
+    unsigned int y = threadIdx.x + blockDim.x*blockIdx.x;
+    /* unsigned int offset1 = __mul24( y, params.ldy1); */
+    /* unsigned int offset2 = __mul24( y, params.ldy2); */
+    if( y < params.n )
+    {
+        cuDoubleComplex *A1 = params.A1 + y - params.ldx1; /*offset1*/
+        cuDoubleComplex *A2 = params.A2 + y;               /*offset2*/
+      
+        for( int i = 0; i < params.npivots; i++ )
+        {
+            A1 += params.ldx1;
+            if ( params.ipiv[i] == -1 )
+                continue;
+            cuDoubleComplex tmp1  = *A1;
+            cuDoubleComplex *tmp2 = A2 + params.ipiv[i]*params.ldx2;
+            *A1   = *tmp2;
+            *tmp2 = tmp1;
+        }
+    }
+}
+
+extern "C" void zswapblk( zswapblk_params_t &params )
+{
+    int blocksize = 64;
+    dim3 blocks = (params.n+blocksize-1) / blocksize;
+    myzswapblk<<< blocks, blocksize >>>( params );
+}
+
+
+extern "C" void 
+magmablas_zswapblk( int n, cuDoubleComplex *dA1T, int ldx1, int ldy1, 
+                    cuDoubleComplex *dA2T, int ldx2, int ldy2,
+                    int i1, int i2, int *ipiv, int inci, int offset )
+{
+    int  k, im;
+    for( k=(i1-1); k<i2; k+=BLOCK_SIZE )
+    {
+        int sb = min(BLOCK_SIZE, i2-k);
+        zswapblk_params_t params = { dA1T+k*ldx1, dA2T, n, ldx1, ldx2, ldy1, ldy2, sb };
+        for( int j = 0; j < sb; j++ )
+        {
+            im = ipiv[(k+j)*inci] - 1;
+            if ( (k+j) == im)
+                params.ipiv[j] = -1;
+            else
+                params.ipiv[j] = im - offset;
+        }
+        zswapblk ( params );
+    }
+}
+
