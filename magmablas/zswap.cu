b4bfa480ce79dd143e5aaf0304b04348d33a5e79
Mathieu Faverge <mathieu.faverge@inria.fr>
2010-11-21 00:18:42 +0000

    Now that it is in a separate file, it seems that it works. !!!
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@399 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- magmablas/zswap.cu
+++ magmablas/zswap.cu
@@ -25,18 +25,18 @@
 typedef struct {
     cuDoubleComplex *A1;
     cuDoubleComplex *A2;
-    int n; /*, lda1, lda2;*/
+    int n, lda1, lda2;
 } zswap_params_t;
 
 __global__ void myzswap( zswap_params_t params )
 {
-    unsigned int x = threadIdx.x + blockDim.x*blockIdx.x;
-    /* unsigned int offset1 = __mul24( x, params.lda1); */
-    /* unsigned int offset2 = __mul24( x, params.lda2); */
+    unsigned int x = threadIdx.x + __mul24(blockDim.x, blockIdx.x);
+    unsigned int offset1 = __mul24( x, params.lda1);
+    unsigned int offset2 = __mul24( x, params.lda2);
     if( x < params.n )
     {
-        cuDoubleComplex *A1  = params.A1 + x; /*offset1;*/
-        cuDoubleComplex *A2  = params.A2 + x; /*offset2;*/
+        cuDoubleComplex *A1  = params.A1 + offset1;
+        cuDoubleComplex *A2  = params.A2 + offset2;
         cuDoubleComplex temp = *A1;
         *A1 = *A2;
         *A2 = temp;
@@ -55,7 +55,7 @@ extern "C" void
 magmablas_zswap( int n, cuDoubleComplex *dA1T, int lda1, 
                  cuDoubleComplex *dA2T, int lda2)
 {
-    zswap_params_t params = { dA1T, dA2T, n }; /*, lda1, lda2 };*/
+    zswap_params_t params = { dA1T, dA2T, n, lda1, lda2 };
 /*     int  blocksize = 64; */
 /*     int  blocks = (params.n+blocksize-1) / blocksize; */
     
