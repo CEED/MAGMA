9111d2a8ea299907674be414219f9ae3b0f5cd25
Mathieu Faverge <mathieu.faverge@inria.fr>
2010-11-24 06:14:39 +0000

    Clean header of zlag2c/clag2z
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@447 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- include/magmablas_zc.h
+++ include/magmablas_zc.h
@@ -21,8 +21,8 @@ extern "C" {
   /* Mixed precision */
 void magmablas_zcaxpycp(float2 *, double2 *, magma_int_t, magma_int_t, magma_int_t, double2 *, double2 *);
 void magmablas_zclaswp(magma_int_t, double2 *, magma_int_t, float2 *, magma_int_t, magma_int_t *);
-void magmablas_zlag2c(magma_int_t, magma_int_t, const double2 *, magma_int_t, float2 *, magma_int_t, float2);
-void magmablas_clag2z(magma_int_t, magma_int_t, float2 *, magma_int_t, double2 *, magma_int_t, magma_int_t *);
+void magmablas_zlag2c(magma_int_t M, magma_int_t N, const double2 *A, magma_int_t lda,  float2 *SA, magma_int_t ldsa, magma_int_t *info);
+void magmablas_clag2z(magma_int_t M, magma_int_t N, const float2 *SA, magma_int_t ldsa, double2 *A, magma_int_t lda,  magma_int_t *info);
 void magmablas_zlat2c(char, magma_int_t, double2 *, magma_int_t, float2 *, magma_int_t, magma_int_t *);
 
 #ifdef __cplusplus
