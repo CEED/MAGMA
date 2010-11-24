db0b28029e17d62814dc71f7ab74b25b3622aad1
Mathieu Faverge <mathieu.faverge@inria.fr>
2010-11-24 05:31:51 +0000

    Add the missing file zcaxpycp
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@444 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ magmablas/zcaxpycp.cu
@@ -0,0 +1,59 @@
+/*
+  -- MAGMA (version 1.0) --
+  Univ. of Tennessee, Knoxville
+  Univ. of California, Berkeley
+  Univ. of Colorado, Denver
+  November 2010
+
+  @precisions mixed zc -> ds
+
+*/
+#include <cublas.h>
+#include "magma.h"
+
+extern "C" __global__ void
+zcaxpycp_special(cuFloatComplex *R, cuDoubleComplex *X, int M, int ldr,int lda, cuDoubleComplex *B,cuDoubleComplex *W )
+{
+    const int ibx = blockIdx.x * 64;
+    const int idt = threadIdx.x;
+    X += ibx+idt;
+    R += ibx+idt;
+    B += ibx+idt;
+    W += ibx+idt;
+    X[0] = MAGMA_Z_ADD( X[0], cuComplexFloatToDouble(R[0]) );
+    W[0] = B[0];
+}
+
+extern "C" __global__ void
+zcaxpycp_generic(cuFloatComplex *R, cuDoubleComplex *X, int M, int ldr,int lda, cuDoubleComplex *B,cuDoubleComplex *W )
+{
+    const int ibx = blockIdx.x * 64;
+    const int idt = threadIdx.x;
+    if( ( ibx + idt ) < M ) {
+        X += ibx+idt;
+        R += ibx+idt;
+        B += ibx+idt;
+        W += ibx+idt;
+    }
+    else{
+        X +=(M-1);
+        R +=(M-1);
+        B +=(M-1);
+        W +=(M-1);
+    }
+    X[0] = MAGMA_Z_ADD( X[0], cuComplexFloatToDouble( R[0] ) );
+    W[0] = B[0];
+}
+
+extern "C" void
+magmablas_zcaxpycp(cuFloatComplex *R, cuDoubleComplex *X, int M, int ldr, int lda, cuDoubleComplex *B, cuDoubleComplex *W)
+{
+    dim3 threads( 64, 1 );
+    dim3 grid(M/64+(M%64!=0),1);
+    if( M %64 == 0 ) {
+        zcaxpycp_special <<< grid, threads >>> ( R, X, M, ldr,lda, B, W) ;
+    }
+    else{
+        zcaxpycp_generic <<< grid, threads >>> ( R, X, M, ldr,lda, B, W) ;
+    }
+}
