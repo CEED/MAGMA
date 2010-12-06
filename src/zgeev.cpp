cd30a1f988efdb705016f10c1c25a9c3134e1ef6
Stanimire Tomov <tomov@icl.utk.edu>
2010-12-06 20:48:25 +0000

    Now the general eigenvalue problem compiles.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@575 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zgeev.cpp
+++ src/zgeev.cpp
@@ -17,9 +17,12 @@
 
 extern "C" int
 magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n, 
-	    cuDoubleComplex *a, magma_int_t *lda, cuDoubleComplex *w, cuDoubleComplex *vl, 
-	    magma_int_t *ldvl, cuDoubleComplex *vr, magma_int_t *ldvr, cuDoubleComplex *work, 
-	    magma_int_t *lwork, double *rwork, magma_int_t *info)
+	    cuDoubleComplex *a, magma_int_t *lda, 
+	    cuDoubleComplex *w, 
+	    cuDoubleComplex *vl, magma_int_t *ldvl, 
+	    cuDoubleComplex *vr, magma_int_t *ldvr, 
+	    cuDoubleComplex *work, magma_int_t *lwork, 
+	    double *rwork, magma_int_t *info)
 {
 /*  -- MAGMA (version 1.0) --
        Univ. of Tennessee, Knoxville
@@ -110,18 +113,21 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
                   converged.   
     =====================================================================    */
 
-    static magma_int_t c__1 = 1;
-    static magma_int_t c__0 = 0;
-    static magma_int_t c_n1 = -1;
+    #define min(a,b)       (((a)<(b))?(a):(b))
+    #define max(a,b)       (((a)>(b))?(a):(b))
+
+    magma_int_t c__1 = 1;
+    magma_int_t c__0 = 0;
+    magma_int_t c_n1 = -1;
     
     magma_int_t a_dim1, a_offset, vl_dim1, vl_offset, vr_dim1, vr_offset, i__1, 
 	    i__2, i__3;
     double d__1, d__2;
     cuDoubleComplex z__1, z__2;
-    /* Builtin functions */
+
     double sqrt(double), d_imag(cuDoubleComplex *);
     void d_cnjg(cuDoubleComplex *, cuDoubleComplex *);
-    /* Local variables */
+
     static magma_int_t i__, k, ihi;
     static double scl;
     static magma_int_t ilo;
@@ -131,12 +137,11 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
     static char side[1];
     static double anrm;
     static magma_int_t ierr, itau, iwrk, nout;
-    extern logical lsame_(char *, char *);
     extern int 
       zscal_(magma_int_t *, cuDoubleComplex *, cuDoubleComplex *, magma_int_t *), 
       dlabad_(double *, double *);
     extern double dznrm2_(magma_int_t *, cuDoubleComplex *, magma_int_t *);
-    static logical scalea;
+    magma_int_t scalea;
     extern double dlamch_(char *);
     static double cscale;
     extern int 
@@ -147,33 +152,27 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
     extern magma_int_t idamax_(magma_int_t *, double *, magma_int_t *);
     extern int xerbla_(char *, magma_int_t *);
     extern magma_int_t ilaenv_(magma_int_t *, char *, char *, magma_int_t *, magma_int_t *, 
-			       magma_int_t *, magma_int_t *, ftnlen, ftnlen);
-    static logical select[1];
+			       magma_int_t *, magma_int_t *, magma_int_t, magma_int_t);
+    magma_int_t select[1];
     extern int zdscal_(magma_int_t *, double *, 
 		       cuDoubleComplex *, magma_int_t *);
     static double bignum;
-    extern double zlange_(char *, magma_int_t *, magma_int_t *, cuDoubleComplex *, 
-			      magma_int_t *, double *);
     extern int 
-      zgehrd_(magma_int_t *, magma_int_t *, magma_int_t *, 
-	      cuDoubleComplex *, magma_int_t *, cuDoubleComplex *, cuDoubleComplex *, 
-	      magma_int_t *, magma_int_t *), 
       zlascl_(char *, magma_int_t *, magma_int_t *, double *, double *, magma_int_t *, 
-	      magma_int_t *, cuDoubleComplex *, magma_int_t *, magma_int_t *),
-      zlacpy_(char *, magma_int_t *, magma_int_t *, cuDoubleComplex *, magma_int_t *,
-	      cuDoubleComplex *, magma_int_t *);
+	      magma_int_t *, cuDoubleComplex *, magma_int_t *, magma_int_t *);
     static magma_int_t minwrk, maxwrk;
-    static logical wantvl;
+    magma_int_t wantvl;
     static double smlnum;
     static magma_int_t hswork, irwork;
     extern int 
       zhseqr_(char *, char *, magma_int_t *, magma_int_t *, magma_int_t *,
 	      cuDoubleComplex *, magma_int_t *, cuDoubleComplex *, cuDoubleComplex *,
 	      magma_int_t *, cuDoubleComplex *, magma_int_t *, magma_int_t *),
-      ztrevc_(char *, char *, logical *, magma_int_t *, cuDoubleComplex *, magma_int_t *,
-	      cuDoubleComplex *, magma_int_t *, cuDoubleComplex *, magma_int_t *, magma_int_t *,
+      ztrevc_(char *, char *, magma_int_t *, magma_int_t *, cuDoubleComplex *,
+	      magma_int_t *, cuDoubleComplex *, magma_int_t *, 
+	      cuDoubleComplex *, magma_int_t *, magma_int_t *,
 	      magma_int_t *, cuDoubleComplex *, double *, magma_int_t *);
-    static logical lquery, wantvr;
+    magma_int_t lquery, wantvr;
     extern int zunghr_(magma_int_t *, magma_int_t *, magma_int_t *, 
 		       cuDoubleComplex *, magma_int_t *, cuDoubleComplex *,
 		       cuDoubleComplex *, magma_int_t *, magma_int_t *);
@@ -191,14 +190,13 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
     --work;
     --rwork;
 
-    /* Function Body */
     *info = 0;
     lquery = *lwork == -1;
-    wantvl = lsame_(jobvl, "V");
-    wantvr = lsame_(jobvr, "V");
-    if (! wantvl && ! lsame_(jobvl, "N")) {
+    wantvl = lapackf77_lsame(jobvl, "V");
+    wantvr = lapackf77_lsame(jobvr, "V");
+    if (! wantvl && ! lapackf77_lsame(jobvl, "N")) {
 	*info = -1;
-    } else if (! wantvr && ! lsame_(jobvr, "N")) {
+    } else if (! wantvr && ! lapackf77_lsame(jobvr, "N")) {
 	*info = -2;
     } else if (*n < 0) {
 	*info = -3;
@@ -226,19 +224,19 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
 	    maxwrk = 1;
 	} else {
 	    maxwrk = *n + *n * ilaenv_(&c__1, "ZGEHRD", " ", n, &c__1, n, &
-				       c__0, (ftnlen)6, (ftnlen)1);
+				       c__0, 6, 1);
 	    minwrk = *n << 1;
 	    if (wantvl) {
 	      /* Computing MAX */
 		i__1 = maxwrk, i__2 = *n + (*n - 1) * ilaenv_(&c__1, "ZUNGHR",
-			 " ", n, &c__1, n, &c_n1, (ftnlen)6, (ftnlen)1);
+			 " ", n, &c__1, n, &c_n1, 6, 1);
 		maxwrk = max(i__1,i__2);
 		zhseqr_("S", "V", n, &c__1, n, &a[a_offset], lda, &w[1], &vl[
 			vl_offset], ldvl, &work[1], &c_n1, info);
 	    } else if (wantvr) {
 	      /* Computing MAX */
 		i__1 = maxwrk, i__2 = *n + (*n - 1) * ilaenv_(&c__1, "ZUNGHR",
-			 " ", n, &c__1, n, &c_n1, (ftnlen)6, (ftnlen)1);
+			 " ", n, &c__1, n, &c_n1, 6, 1);
 		maxwrk = max(i__1,i__2);
 		zhseqr_("S", "V", n, &c__1, n, &a[a_offset], lda, &w[1], &vr[
 			vr_offset], ldvr, &work[1], &c_n1, info);
@@ -246,12 +244,12 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
 		zhseqr_("E", "N", n, &c__1, n, &a[a_offset], lda, &w[1], &vr[
 			vr_offset], ldvr, &work[1], &c_n1, info);
 	    }
-	    hswork = (magma_int_t) work[1].r;
+	    hswork = (magma_int_t) MAGMA_Z_REAL(work[1]);
 	    /* Computing MAX */
 	    i__1 = max(maxwrk,hswork);
 	    maxwrk = max(i__1,minwrk);
 	}
-	work[1].r = (double) maxwrk, work[1].i = 0.;
+	MAGMA_Z_SET2REAL(work[1], (double) maxwrk);
 
 	if (*lwork < minwrk && ! lquery) {
 	    *info = -12;
@@ -280,13 +278,13 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
     bignum = 1. / smlnum;
 
     /* Scale A if max element outside range [SMLNUM,BIGNUM] */
-    anrm = zlange_("M", n, n, &a[a_offset], lda, dum);
-    scalea = FALSE_;
+    anrm = lapackf77_zlange("M", n, n, &a[a_offset], lda, dum);
+    scalea = 0;
     if (anrm > 0. && anrm < smlnum) {
-	scalea = TRUE_;
+	scalea = 1;
 	cscale = smlnum;
     } else if (anrm > bignum) {
-	scalea = TRUE_;
+	scalea = 1;
 	cscale = bignum;
     }
     if (scalea) {
@@ -308,14 +306,14 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
     itau = 1;
     iwrk = itau + *n;
     i__1 = *lwork - iwrk + 1;
-    zgehrd_(n, &ilo, &ihi, &a[a_offset], lda, &work[itau], &work[iwrk], &i__1,
-	     &ierr);
+    lapackf77_zgehrd(n, &ilo, &ihi, &a[a_offset], lda, &work[itau], &work[iwrk], &i__1,
+		     &ierr);
 
     if (wantvl) {
       /*        Want left eigenvectors   
 		Copy Householder vectors to VL */
 	*(unsigned char *)side = 'L';
-	zlacpy_("L", n, n, &a[a_offset], lda, &vl[vl_offset], ldvl);
+	lapackf77_zlacpy("L", n, n, &a[a_offset], lda, &vl[vl_offset], ldvl);
 
 	/* Generate unitary matrix in VL   
            (CWorkspace: need 2*N-1, prefer N+(N-1)*NB)   
@@ -336,14 +334,14 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
 	  /* Want left and right eigenvectors   
              Copy Schur vectors to VR */
 	    *(unsigned char *)side = 'B';
-	    zlacpy_("F", n, n, &vl[vl_offset], ldvl, &vr[vr_offset], ldvr);
+	    lapackf77_zlacpy("F", n, n, &vl[vl_offset], ldvl, &vr[vr_offset], ldvr);
 	}
 
     } else if (wantvr) {
       /*  Want right eigenvectors   
           Copy Householder vectors to VR */
 	*(unsigned char *)side = 'R';
-	zlacpy_("L", n, n, &a[a_offset], lda, &vr[vr_offset], ldvr);
+	lapackf77_zlacpy("L", n, n, &a[a_offset], lda, &vr[vr_offset], ldvr);
 	/* Generate unitary matrix in VR   
            (CWorkspace: need 2*N-1, prefer N+(N-1)*NB)   
            (RWorkspace: none) */
@@ -401,7 +399,7 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
 	      {
 		i__3 = k + i__ * vl_dim1;
 		/* Computing 2nd power */
-		d__1 = vl[i__3].r;
+		d__1 = MAGMA_Z_REAL(vl[i__3]);
 		/* Computing 2nd power */
 		d__2 = d_imag(&vl[k + i__ * vl_dim1]);
 		rwork[irwork + k - 1] = d__1 * d__1 + d__2 * d__2;
@@ -409,14 +407,14 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
 	    k = idamax_(n, &rwork[irwork], &c__1);
 	    d_cnjg(&z__2, &vl[k + i__ * vl_dim1]);
 	    d__1 = sqrt(rwork[irwork + k - 1]);
-	    z__1.r = z__2.r / d__1, z__1.i = z__2.i / d__1;
-	    tmp.r = z__1.r, tmp.i = z__1.i;
+	    MAGMA_Z_DSCALE(z__1, z__2, d__1);
+	    MAGMA_Z_ASSIGN(tmp, z__1);
 	    zscal_(n, &tmp, &vl[i__ * vl_dim1 + 1], &c__1);
 	    i__2 = k + i__ * vl_dim1;
 	    i__3 = k + i__ * vl_dim1;
-	    d__1 = vl[i__3].r;
-	    z__1.r = d__1, z__1.i = 0.;
-	    vl[i__2].r = z__1.r, vl[i__2].i = z__1.i;
+	    d__1 = MAGMA_Z_REAL(vl[i__3]);
+	    MAGMA_Z_SET2REAL(z__1, d__1);
+	    MAGMA_Z_ASSIGN(vl[i__2], z__1);
 	}
     }
 
@@ -436,7 +434,7 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
 	    for (k = 1; k <= i__2; ++k) {
 		i__3 = k + i__ * vr_dim1;
 		/* Computing 2nd power */
-		d__1 = vr[i__3].r;
+		d__1 = MAGMA_Z_REAL(vr[i__3]);
 		/* Computing 2nd power */
 		d__2 = d_imag(&vr[k + i__ * vr_dim1]);
 		rwork[irwork + k - 1] = d__1 * d__1 + d__2 * d__2;
@@ -444,14 +442,14 @@ magma_zgeev(char *jobvl, char *jobvr, magma_int_t *n,
 	    k = idamax_(n, &rwork[irwork], &c__1);
 	    d_cnjg(&z__2, &vr[k + i__ * vr_dim1]);
 	    d__1 = sqrt(rwork[irwork + k - 1]);
-	    z__1.r = z__2.r / d__1, z__1.i = z__2.i / d__1;
-	    tmp.r = z__1.r, tmp.i = z__1.i;
+	    MAGMA_Z_DSCALE(z__1, z__2, d__1);
+	    MAGMA_Z_ASSIGN(tmp, z__1);
 	    zscal_(n, &tmp, &vr[i__ * vr_dim1 + 1], &c__1);
 	    i__2 = k + i__ * vr_dim1;
 	    i__3 = k + i__ * vr_dim1;
-	    d__1 = vr[i__3].r;
-	    z__1.r = d__1, z__1.i = 0.;
-	    vr[i__2].r = z__1.r, vr[i__2].i = z__1.i;
+	    d__1 = MAGMA_Z_REAL(vr[i__3]);
+	    MAGMA_Z_SET2REAL(z__1, d__1);
+	    MAGMA_Z_ASSIGN(vr[i__2], z__1);
 	}
     }
 
@@ -471,7 +469,7 @@ L50:
 	}
     }
 
-    work[1].r = (double) maxwrk, work[1].i = 0.;
+    MAGMA_Z_SET2REAL(work[1], (double) maxwrk);
     return 0;
 
     /* End of MAGMA_ZGEEV */
