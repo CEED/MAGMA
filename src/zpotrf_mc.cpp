442b5d44de224440ecb964b29ba09cf86e555522
horton <horton@mycompany.com>
2010-12-07 21:33:50 +0000

    This file and it's driver (testing_zpotrf_mc.cpp) are release worthy.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@589 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zpotrf_mc.cpp
+++ src/zpotrf_mc.cpp
@@ -15,267 +15,138 @@
 #include <cublas.h>
 #include <magma.h>
 #include <quark.h>
-//#include <cblas.h>
-
-#include <event_trace.h>
-
-/*
-
-// used by trace 
-int    event_num        [MAX_THREADS];
-double event_start_time [MAX_THREADS];
-double event_end_time   [MAX_THREADS];
-double event_log        [MAX_THREADS][MAX_EVENTS];
-char *event_label       [MAX_THREADS][MAX_EVENTS];
-int log_events = 1;
-char tasklabel[200];
-char taskcolor[200];
-
-double get_current_timee(void) {
-  struct timeval tp;
-  gettimeofday( &tp, NULL );
-  return tp.tv_sec + 1e-6 * tp.tv_usec;
-}
-
-// dump trace
-void dump_trace(int cores_num) {
-  char trace_file_name[32];
-  FILE *trace_file;
-  double end;
-  int event, core;
-  double scale = 30000.0;
-
-  sprintf(trace_file_name, "trace.svg");
-  trace_file = fopen(trace_file_name, "w");
-
-  int x_end_max = -1;
-
-  for (core = 0; core < cores_num; core++) {
-    event = event_num[core]-4;
-    end = event_log[core][event+2] - event_log[0][1];
-    x_end_max = max(x_end_max, (int)(end * scale) + 20);
-  }
-
-fprintf(trace_file,
-"<?xml version=\"1.0\" standalone=\"no\"?>"
-"<svg version=\"1.1\" baseProfile=\"full\" xmlns=\"http://www.w3.org/2000/svg\" "
-"xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:ev=\"http://www.w3.org/2001/xml-events\"  "
-">\n"
-"  <g font-size=\"20\">\n");
-
-for (core = 0; core < cores_num; core++)
-
-for (event = 0; event < event_num[core]; event += 4) {
-
-int    tag   = (int)event_log[core][event+0];
-double start =      event_log[core][event+1];
-double end   =      event_log[core][event+2];
-int    color = (int)event_log[core][event+3];
-char   *label = event_label[core][event+0];
-
-start -= event_log[0][1];
-end   -= event_log[0][1];
-
-fprintf(trace_file,
-"    "
-"<rect x=\"%.2lf\" y=\"%.0lf\" width=\"%.2lf\" height=\"%.0lf\" "
-"fill=\"#%06x\" stroke=\"#000000\" stroke-width=\"1\"/>\n",
-start * scale,  // x
-core * 100.0,   // y
-(end - start) * scale, // width
-90.0,           // height
-color);
-
-  fprintf(trace_file,
-  "    "
-  "<text x=\"%.2lf\" y=\"%.0lf\" font-size=\"20\" fill=\"black\">"
-  "%s"
-  "</text>\n",
-  start * scale + 10, // x
-  core * 100.0 + 20, // y
-  label);
-
-}
-//}
-
-fprintf(trace_file,
-"  </g>\n"
-"</svg>\n");
-
-fclose(trace_file);
-}
-
-*/
-
 
 #define  A(m,n) (a+(n)*(*lda)+(m))
 
 extern "C" int EN_BEE;
 
-extern "C" int TRACE;
-
 extern "C" Quark *quark;
 
-void SCHED_sgemm(Quark* quark)
+// task execution code
+void SCHED_zgemm(Quark* quark)
 {
-int UPPER;
-int M;
-int N;
-int K;
-cuDoubleComplex *A1;
-int LDA;
-cuDoubleComplex *A2;
-cuDoubleComplex *A3;
+  int UPPER;
+  int M;
+  int N;
+  int K;
+  cuDoubleComplex *A1;
+  int LDA;
+  cuDoubleComplex *A2;
+  cuDoubleComplex *A3;
 
-cuDoubleComplex mone = MAGMA_Z_NEG_ONE;
-cuDoubleComplex one = MAGMA_Z_ONE;
+  cuDoubleComplex mone = MAGMA_Z_NEG_ONE;
+  cuDoubleComplex one = MAGMA_Z_ONE;
 
-quark_unpack_args_8(quark, UPPER, M, N, K, A1, LDA, A2, A3);
+  quark_unpack_args_8(quark, UPPER, M, N, K, A1, LDA, A2, A3);
 
-//if (TRACE == 1)
-  //core_event_start(QUARK_Thread_Rank(quark));
+  if (UPPER) {
 
-if (UPPER) {
+    blasf77_zgemm(MagmaConjTransStr, MagmaNoTransStr, 
+      &M, &N, &K, &mone, A1, &LDA, A2, &LDA, &one, A3, &LDA);
 
-blasf77_zgemm(MagmaConjTransStr, MagmaNoTransStr, 
-  &M, &N, &K, &mone, A1, &LDA, A2, &LDA, &one, A3, &LDA);
+  } else {
 
-} else {
+    blasf77_zgemm(MagmaNoTransStr, MagmaConjTransStr, 
+      &M, &N, &K, &mone, A1, &LDA, A2, &LDA, &one, A3, &LDA);
 
-blasf77_zgemm(MagmaNoTransStr, MagmaConjTransStr, 
-  &M, &N, &K, &mone, A1, &LDA, A2, &LDA, &one, A3, &LDA);
-
-}
-
-
-//if (TRACE == 1) {
-  //core_event_end(QUARK_Thread_Rank(quark));
-  //core_log_event_label(0x0000ff,QUARK_Thread_Rank(quark),0,"");
-//}
+  }
 
 }
 
-void SCHED_ssyrk(Quark* quark)
+// task execution code
+void SCHED_zsyrk(Quark* quark)
 {
-int UPPER;
-int N;
-cuDoubleComplex *A1;
-int LDA;
-int K;
-cuDoubleComplex *A2;
+  int UPPER;
+  int N;
+  cuDoubleComplex *A1;
+  int LDA;
+  int K;
+  cuDoubleComplex *A2;
 
-//cuDoubleComplex mone = MAGMA_Z_NEG_ONE;
-//cuDoubleComplex one = MAGMA_Z_ONE;
+  double mone = -1.0;
+  double one = 1.0;
 
-double mone = -1.0;
-double one = 1.0;
+  quark_unpack_args_6(quark, UPPER, N, K, A1, LDA, A2);
 
-quark_unpack_args_6(quark, UPPER, N, K, A1, LDA, A2);
+  if (UPPER) {
 
-//if (TRACE == 1)
-  //core_event_start(QUARK_Thread_Rank(quark));
+    blasf77_zherk(MagmaUpperStr, MagmaConjTransStr, &N, &K, &mone, A1, &LDA, &one, 
+      A2, &LDA);
 
-if (UPPER) {
+  } else {
 
-//blasf77_zsyrk(MagmaUpperStr, MagmaTransStr, &N, &K, &mone, A1, &LDA, &one, 
-blasf77_zherk(MagmaUpperStr, MagmaConjTransStr, &N, &K, &mone, A1, &LDA, &one, 
-  A2, &LDA);
+    blasf77_zherk(MagmaLowerStr, MagmaNoTransStr, &N, &K, &mone, A1, &LDA, &one, 
+      A2, &LDA);
 
-} else {
-
-blasf77_zherk(MagmaLowerStr, MagmaNoTransStr, &N, &K, &mone, A1, &LDA, &one, 
-  A2, &LDA);
+  }
 
 }
 
-//if (TRACE == 1) {
-  //core_event_end(QUARK_Thread_Rank(quark));
-  //core_log_event_label(0x00ff00,QUARK_Thread_Rank(quark),0,"");
-//}
-
-}
-
-void SCHED_spotrf(Quark* quark)
+// task execution code
+void SCHED_zpotrf(Quark* quark)
 {
-int UPPER;
-int N;
-cuDoubleComplex *A;
-int LDA;
+  int UPPER;
+  int N;
+  cuDoubleComplex *A;
+  int LDA;
 
-int *iinfo;
+  int *iinfo;
 
-int info;
+  int info;
 
-quark_unpack_args_5(quark, UPPER, N, A, LDA, iinfo);
+  quark_unpack_args_5(quark, UPPER, N, A, LDA, iinfo);
 
-//if (TRACE == 1)
-  //core_event_start(QUARK_Thread_Rank(quark));
+  if (UPPER) {
 
-if (UPPER) {
+    lapackf77_zpotrf(MagmaUpperStr, &N, A, &LDA, &info);
 
-lapackf77_zpotrf(MagmaUpperStr, &N, A, &LDA, &info);
+  } else {
 
-} else {
+    lapackf77_zpotrf(MagmaLowerStr, &N, A, &LDA, &info);
 
-lapackf77_zpotrf(MagmaLowerStr, &N, A, &LDA, &info);
+  }
 
-}
-
-if (info > 0) {
-  iinfo[1] = iinfo[0] + info;
-}
-
-
-//if (TRACE == 1) {
-  //core_event_end(QUARK_Thread_Rank(quark));
-  //core_log_event_label(0x00ffff,QUARK_Thread_Rank(quark),0,"");
-//}
+  if (info > 0) {
+    iinfo[1] = iinfo[0] + info;
+  }
 
 }
 
-void SCHED_strsm(Quark* quark)
+// task execution code
+void SCHED_ztrsm(Quark* quark)
 {
-int UPPER;
-int M;
-int N;
-cuDoubleComplex *A1;
-int LDA;
-cuDoubleComplex *A2;
+  int UPPER;
+  int M;
+  int N;
+  cuDoubleComplex *A1;
+  int LDA;
+  cuDoubleComplex *A2;
 
-cuDoubleComplex one = MAGMA_Z_ONE;
+  cuDoubleComplex one = MAGMA_Z_ONE;
 
-quark_unpack_args_6(quark, UPPER, M, N, A1, LDA, A2);
+  quark_unpack_args_6(quark, UPPER, M, N, A1, LDA, A2);
 
-//if (TRACE == 1)
-  //core_event_start(QUARK_Thread_Rank(quark));
+  if (UPPER) {
 
-if (UPPER) {
+    blasf77_ztrsm(MagmaLeftStr, MagmaUpperStr, MagmaConjTransStr, MagmaNonUnitStr,
+      &M, &N, &one, A1, &LDA, A2, &LDA);
 
-blasf77_ztrsm(MagmaLeftStr, MagmaUpperStr, MagmaConjTransStr, MagmaNonUnitStr,
-  &M, &N, &one, A1, &LDA, A2, &LDA);
+  } else {
 
-} else {
+    blasf77_ztrsm(MagmaRightStr, MagmaLowerStr, MagmaConjTransStr, MagmaNonUnitStr,
+      &M, &N, &one, A1, &LDA, A2, &LDA);
 
-blasf77_ztrsm(MagmaRightStr, MagmaLowerStr, MagmaConjTransStr, MagmaNonUnitStr,
-  &M, &N, &one, A1, &LDA, A2, &LDA);
-
-}
-
-//if (TRACE == 1) {
-  //core_event_end(QUARK_Thread_Rank(quark));
-  ////core_log_event_label(0xff0000,QUARK_Thread_Rank(quark),0,QUARK_Get_Task_Label(quark));
-  //core_log_event_label(0xff0000,QUARK_Thread_Rank(quark),0,"");
-//}
+  }
 
 }
 
 extern "C" int 
 magma_zpotrf_mc(
-char *uplo,
-int *n,
-cuDoubleComplex *a,
-int *lda,
-int *info)
+  char *uplo,
+  int *n,
+  cuDoubleComplex *a,
+  int *lda,
+  int *info)
 {
 /*  -- MAGMA (version 1.0) --
        Univ. of Tennessee, Knoxville
@@ -355,9 +226,6 @@ int *info)
   int iinfo[2];
   iinfo[1] = 0;
 
-  // start quark with 4 threads
-  //Quark *quark = QUARK_New(0);
-
   ii = -1;
 
   // traverse diagonal blocks
@@ -377,7 +245,7 @@ int *info)
 
         if (upper) {
 
-          QUARK_Insert_Task(quark, SCHED_ssyrk, 0,
+          QUARK_Insert_Task(quark, SCHED_zsyrk, 0,
             sizeof(int),             &upper,    VALUE,
             sizeof(int),             &temp2,    VALUE,
             sizeof(int),             &i,        VALUE,
@@ -391,7 +259,7 @@ int *info)
 
         } else {
 
-          QUARK_Insert_Task(quark, SCHED_ssyrk, 0,
+          QUARK_Insert_Task(quark, SCHED_zsyrk, 0,
             sizeof(int),             &upper,    VALUE,
             sizeof(int),             &temp2,    VALUE,
             sizeof(int),             &i,        VALUE,
@@ -418,7 +286,7 @@ int *info)
 
           if (upper) {
 
-            QUARK_Insert_Task(quark, SCHED_ssyrk, 0,
+            QUARK_Insert_Task(quark, SCHED_zsyrk, 0,
               sizeof(int),             &upper,    VALUE,
               sizeof(int),             &temp2,    VALUE,
               sizeof(int),             &nb,       VALUE,
@@ -431,7 +299,7 @@ int *info)
 
           } else {
 
-            QUARK_Insert_Task(quark, SCHED_ssyrk, 0,
+            QUARK_Insert_Task(quark, SCHED_zsyrk, 0,
               sizeof(int),             &upper,    VALUE,
               sizeof(int),             &temp2,    VALUE,
               sizeof(int),             &nb,       VALUE,
@@ -470,7 +338,7 @@ int *info)
 
             if (upper) {
 
-              QUARK_Insert_Task(quark, SCHED_sgemm, 0,
+              QUARK_Insert_Task(quark, SCHED_zgemm, 0,
                 sizeof(int),             &upper,    VALUE,
                 sizeof(int),             &nb,       VALUE,
                 sizeof(int),             &temp,     VALUE,
@@ -485,7 +353,7 @@ int *info)
 
             } else {
 
-              QUARK_Insert_Task(quark, SCHED_sgemm, 0,
+              QUARK_Insert_Task(quark, SCHED_zgemm, 0,
                 sizeof(int),             &upper,    VALUE,
                 sizeof(int),             &temp,     VALUE,
                 sizeof(int),             &nb,       VALUE,
@@ -512,7 +380,7 @@ int *info)
 
     sprintf(label, "POTRF %d", ii);
 
-    QUARK_Insert_Task(quark, SCHED_spotrf, 0,
+    QUARK_Insert_Task(quark, SCHED_zpotrf, 0,
       sizeof(int),             &upper,    VALUE,
       sizeof(int),             &temp2,    VALUE,
       sizeof(cuDoubleComplex)*(*n)*(*n), A(i,i),    INOUT,
@@ -534,7 +402,7 @@ int *info)
 
         if (upper) {
 
-          QUARK_Insert_Task(quark, SCHED_strsm, 0,
+          QUARK_Insert_Task(quark, SCHED_ztrsm, 0,
             sizeof(int),             &upper,    VALUE,
             sizeof(int),             &nb,       VALUE,
             sizeof(int),             &temp,     VALUE,
@@ -547,7 +415,7 @@ int *info)
 
         } else {
 
-          QUARK_Insert_Task(quark, SCHED_strsm, 0,
+          QUARK_Insert_Task(quark, SCHED_ztrsm, 0,
             sizeof(int),             &upper,    VALUE,
             sizeof(int),             &temp,     VALUE,
             sizeof(int),             &nb,       VALUE,
@@ -567,12 +435,6 @@ int *info)
   }
 
   QUARK_Barrier(quark);
-
-  //QUARK_Delete(quark);
-
-  if (TRACE == 1) {
-    //dump_trace(4);
-  }
 
 }
 
