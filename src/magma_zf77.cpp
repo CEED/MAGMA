7f70083aa2093f173b019cd946df7acae70e5774
Stanimire Tomov <tomov@icl.utk.edu>
2010-12-12 06:17:17 +0000

    account for one reduced argument
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@648 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/magma_zf77.cpp
+++ src/magma_zf77.cpp
@@ -95,10 +95,9 @@ extern "C" {
 /* ////////////////////////////////////////////////////////////////////////////
    -- MAGMA function definitions / Data on CPU
 */
-void MAGMA_ZGEBRD( magma_int_t *m, magma_int_t *n, double2 *A, magma_int_t *lda, double *d, double *e, double2 *tauq,  double2 *taup, double2 *work, magma_int_t *lwork, devptr_t *da, magma_int_t *info)
+void MAGMA_ZGEBRD( magma_int_t *m, magma_int_t *n, double2 *A, magma_int_t *lda, double *d, double *e, double2 *tauq,  double2 *taup, double2 *work, magma_int_t *lwork, magma_int_t *info)
 { 
-    cuDoubleComplex *d_a = (cuDoubleComplex *)(*da);
-    magma_zgebrd( *m, *n, A, *lda, d, e, tauq,  taup, work, *lwork, d_a, info); 
+    magma_zgebrd( *m, *n, A, *lda, d, e, tauq,  taup, work, *lwork, info); 
 }
 
 void MAGMA_ZGEHRD( magma_int_t *n, magma_int_t *ilo, magma_int_t *ihi, double2 *A, magma_int_t *lda, double2 *tau, double2 *work, magma_int_t *lwork, magma_int_t *info)
