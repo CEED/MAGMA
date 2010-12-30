c21fcc00f4b724885cb6f73f5b11b3046572c7c0
Stanimire Tomov <tomov@icl.utk.edu>
2010-12-30 19:35:25 +0000

    There was supposed to be a switch for the work-space required when nrhs becomes larger than nb. This fixes the bug we saw (actually matworks sent a bug report) with the least squares QR.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@791 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zgels_gpu.cpp
+++ src/zgels_gpu.cpp
@@ -30,7 +30,6 @@ magma_zgels_gpu( char trans, magma_int_t m, magma_int_t n, magma_int_t nrhs,
 
     Purpose
     =======
-
     Solves the least squares problem
            min || A*X - C ||
     using the QR factorization A = Q*R computed by ZGEQRF_GPU2.
@@ -38,7 +37,6 @@ magma_zgels_gpu( char trans, magma_int_t m, magma_int_t n, magma_int_t nrhs,
 
     Arguments
     =========
-
     M       (input) INTEGER
             The number of rows of the matrix A. M >= 0.
 
@@ -55,10 +53,6 @@ magma_zgels_gpu( char trans, magma_int_t m, magma_int_t n, magma_int_t nrhs,
 
     LDDA     (input) INTEGER
             The leading dimension of the array A, LDDA >= M.
-
-    TAU     (input) COMPLEX_16 array, dimension (N)
-            TAU(i) must contain the scalar factor of the elementary
-            reflector H(i), as returned by MAGMA_ZGEQRF_GPU.
 
     DB       (input/output) COMPLEX_16 array on the GPU, dimension (LDDB,NRHS)
             On entry, the M-by-NRHS matrix C.
@@ -79,13 +73,9 @@ magma_zgels_gpu( char trans, magma_int_t m, magma_int_t n, magma_int_t nrhs,
             only calculates the optimal size of the HWORK array, returns
             this value as the first entry of the WORK array.
 
-    DT      (input) COMPLEX_16 array that is the output (the 9th argument)
-            of magma_zgeqrf_gpu.
-
     INFO    (output) INTEGER
             = 0:  successful exit
             < 0:  if INFO = -i, the i-th argument had an illegal value
-
     =====================================================================    */
 
    #define a_ref(a_1,a_2) (dA+(a_2)*(ldda) + (a_1))
@@ -134,7 +124,10 @@ magma_zgels_gpu( char trans, magma_int_t m, magma_int_t n, magma_int_t nrhs,
     /*
      * Allocate temporary buffers
      */
-    if( CUBLAS_STATUS_SUCCESS != cublasAlloc(( 2*k + ((n+31)/32)*32 )*nb, sizeof(cuDoubleComplex), (void**)&dT) ) {
+    int ldtwork = ( 2*k + ((n+31)/32)*32 )*nb;
+    if (nb < nrhs)
+      ldtwork = ( 2*k + ((n+31)/32)*32 )*nrhs;
+    if( CUBLAS_STATUS_SUCCESS != cublasAlloc(ldtwork, sizeof(cuDoubleComplex), (void**)&dT) ) {
         magma_xerbla("magma_zgels_gpu", info);
 	return MAGMA_ERR_CUBLASALLOC;
     }
