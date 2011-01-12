8ccdea8e0171f2fdd9d3ce17acd6f5a3900ebf5a
Stanimire Tomov <tomov@icl.utk.edu>
2011-01-12 22:27:54 +0000

    This fixes the fortran example. Now this works in all precisions and uses the magma module.
    A few issues here:
    * in the new FORTRAN interface to magma, how are the arrays to the data passed?
    * I used for example 'integer pntrA(4)' - this is inportant because the users
      would be required to do it; we have also to include it in the module
      - this doesn't look natural though; is there something else in fortran
        that can be used;
      - just integer did not work; less than 4 did not work for all precision;
        we use it just for something like a pointer, so I assume we just have to
        give a big enough number so that the interfaces do not overwrite it???
      - can we use some automatic generation for the different precisions?
        maybe we have to have 4 modules for the different precisions and one
        magma module that contains the 4 precision modules?
    
    The module generation for now is in testing but after finalizing it we
    have to put magma.mod in include
    
    
    
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@824 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- testing/testing_zgetrf_gpu_f.f
+++ testing/testing_zgetrf_gpu_f.f
@@ -10,33 +10,45 @@
 
       program testing_zgetrf_gpu_f
 
+      use magma
+
       external cublas_init, cublas_set_matrix, cublas_get_matrix
       external cublas_shutdown, cublas_alloc
-      external zaxpy, zlange, zgetrf, magma_zgetrf_gpu
+      external zlange, zgemm, zgesv
 
       double precision zlange
       integer cublas_alloc
 
-      double precision rnumber(2), matnorm, lapnorm, magnorm, work
-      complex*16, allocatable         :: h_A(:), h_R(:)
-      integer                         :: devptrA
+      double precision rnumber(2), Anorm, Bnorm, Rnorm 
+      double precision, allocatable   :: work(:)
+      complex*16, allocatable         :: h_A(:), h_B(:), h_X(:)
+      complex*16, allocatable         :: h_A2(:)
+      integer                            devptrA(4), devptrB(4)
       integer,    allocatable         :: ipiv(:)
 
-      integer i, n, nb, info, lda, size_of_elt
+      complex*16 zone, mzone
+      integer i, n, info, stat, lda, size_of_elt, nrhs
+
+      PARAMETER          ( nrhs = 1, zone = 1, mzone = -1 )
 
       call cublas_init()
 
       n   = 2048
-      nb  = 128
       lda = n
-      size_of_elt = kind(complex*16)
-      print *,size_of_elt
-
+      size_of_elt = sizeof_complex_16
+ 
 !------ Allocate CPU memory
-      allocate(h_A(n*n), h_R(n*n), ipiv(n))
+      allocate(h_A(n*n), h_B(n*nrhs), h_X(n*nrhs), work(n), ipiv(n))
+      allocate(h_A2(n*n))
 
 !------ Allocate GPU memory
       stat = cublas_alloc(n*n, size_of_elt, devPtrA)
+      if (stat .ne. 0) then
+         write(*,*) "device memory allocation failed"
+         stop
+      endif
+
+      stat = cublas_alloc(n*nrhs, size_of_elt, devPtrB)
       if (stat .ne. 0) then
          write(*,*) "device memory allocation failed"
          stop
@@ -46,41 +58,60 @@
       do i=1,n*n
         call random_number(rnumber)
         h_A(i) = rnumber(1)
-        h_R(i) = rnumber(1)
+        h_A2(i) = h_A(i)
+      end do
+
+      do i=1,n*nrhs
+        call random_number(rnumber)
+        h_B(i) = rnumber(1)
+        h_X(i) = h_B(i)
       end do
 
 !---- devPtrA = h_A
-      call cublas_set_matrix (n, n, size_of_elt, h_A, lda, devptrA, lda)
+      call cublas_set_matrix(n, n, size_of_elt, h_A, lda, devptrA, lda)
 
-!---- Call magma -------------------
+!---- devPtrB = h_B
+      call cublas_set_matrix(n, nrhs, size_of_elt, h_B, lda, devptrB, 
+     $                       lda)
+
+!---- Call magma LU ----------------
       call magma_zgetrf_gpu(n, n, devptrA, lda, ipiv, info)
-      write(*,*) 'exit magma LU'
 
-!---- Call LAPACK ------------------
-      call zgetrf(N, N, h_A, lda, ipiv, info)
-      write(*,*) 'exit lapack LU'
+!---- Call magma solve -------------
+      call magma_zgetrs_gpu('n', n, nrhs, devptrA, lda, ipiv, devptrB, 
+     $                      lda, info)
 
-!---- h_R = devptrA
-      call cublas_get_matrix (n, n, size_of_elt, devptrA, lda, h_R, lda)
+!---- h_X = devptrB
+      call cublas_get_matrix (n, nrhs, size_of_elt, devptrB, lda, h_X, 
+     $                        lda)
+
+!---- Solve using LAPACK ----------------------------------------------
+c      call zgesv(n, nrhs, h_A2, lda, ipiv, h_X, lda, info)
 
 !---- Compare the two results ------
-      lapnorm = zlange('f', n, n, h_A, lda, work)
-      magnorm = zlange('f', n, n, h_R, lda, work)
+      Anorm = zlange('I', n, n, h_A, lda, work)
+      Bnorm = zlange('I', n, nrhs, h_B, lda, work)
 
-      call zaxpy(n*n, -1., h_A, 1, h_R, 1)
-      matnorm = zlange('M', n, n, h_R, lda, work)
+      call zgemm('n', 'n', n,  nrhs, n, zone, h_A, lda, h_X, lda,
+     $           mzone, h_B, lda)
+      Rnorm = zlange('I', n, nrhs, h_B, lda, work)
 
-      write(*,105) 'Norm Lapack = ', lapnorm
-      write(*,105) 'Norm MAGMA  = ', magnorm
-      write(*,105) 'error = ', matnorm
+      write(*,*)
+      write(*,*  ) 'Solving A x = b using LU factorization:'
+      write(*,105) '  || A || = ', Anorm
+      write(*,105) '  || b || = ', Bnorm
+      write(*,105) '  || b - A x || / (||A|| ||b||) = ', 
+     $                Rnorm/(Anorm*Bnorm)
+      write(*,*)
 
 !---- Free CPU memory
-      deallocate(h_A, h_R, ipiv)
+      deallocate(h_A, h_A2, h_X, h_B, work, ipiv)
 
 !---- Free GPU memory
       call cublas_free(devPtrA)
-      call cublas_shutdown
+      call cublas_free(devPtrB)
+      call cublas_shutdown()
 
- 105  format((a10,es10.3))
+ 105  format((a35,es10.3))
 
       end
