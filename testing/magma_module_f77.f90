7509008a211d758fab2ecfa7cb9aacfdcd15e409
Stanimire Tomov <tomov@icl.utk.edu>
2011-01-18 21:07:28 +0000

    Fortran magma module now goes through generation using just a z-version
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@835 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- testing/magma_module_f77.f90
+++ testing/magma_module_f77.f90
@@ -7,6 +7,11 @@
 !
 
 module magma
+
+  use magma_zf77
+  use magma_df77
+  use magma_cf77
+  use magma_sf77
   
   implicit none
 
@@ -15,52 +20,4 @@ module magma
   integer, parameter :: sizeof_double     = 8
   integer, parameter :: sizeof_real       = 4
   
-  !---- Fortran interfaces to MAGMA subroutines ----
-  interface
-     
-     subroutine magma_sgetrf_gpu(m, n, A, lda, ipiv, info)
-       integer   m, n, lda, ipiv(*), info
-       integer(kind=16) A
-     end subroutine magma_sgetrf_gpu
-     
-     subroutine magma_sgetrs_gpu(trans, n, nrhs, dA, ldda, ipiv, dB, lddb, info)
-       character trans
-       integer n, nrhs, ldda, ipiv(*), lddb, info
-       integer(kind=16) dA, dB
-     end subroutine magma_sgetrs_gpu
-     
-     subroutine magma_dgetrf_gpu(m, n, A, lda, ipiv, info)
-       integer   m, n, lda, ipiv(*), info
-       integer(kind=16) A
-     end subroutine magma_dgetrf_gpu
-     
-     subroutine magma_dgetrs_gpu(trans, n, nrhs, dA, ldda, ipiv, dB, lddb, info)
-       character trans
-       integer n, nrhs, ldda, ipiv(*), lddb, info
-       integer(kind=16) dA, dB
-     end subroutine magma_dgetrs_gpu
-     
-     subroutine magma_cgetrf_gpu(m, n, A, lda, ipiv, info)
-       integer   m, n, lda, ipiv(*), info
-       integer(kind=16) A
-     end subroutine magma_cgetrf_gpu
-     
-     subroutine magma_cgetrs_gpu(trans, n, nrhs, dA, ldda, ipiv, dB, lddb, info)
-       character trans
-       integer n, nrhs, ldda, ipiv(*), lddb, info
-       integer(kind=16) dA, dB
-     end subroutine magma_cgetrs_gpu
-     
-     subroutine magma_zgetrf_gpu(m, n, A, lda, ipiv, info)
-       integer   m, n, lda, ipiv(*), info
-       integer(kind=16) A
-     end subroutine magma_zgetrf_gpu
-     
-      subroutine magma_zgetrs_gpu(trans, n, nrhs, dA, ldda, ipiv, dB, lddb, info)
-        character trans
-        integer n, nrhs, ldda, ipiv(*), lddb, info
-        integer(kind=16) dA, dB
-      end subroutine magma_zgetrs_gpu
-     
-  End interface
 end module magma
---
+++ testing/magma_zf77.f90
@@ -0,0 +1,30 @@
+!
+!   -- MAGMA (version 1.0) --
+!      Univ. of Tennessee, Knoxville
+!      Univ. of California, Berkeley
+!      Univ. of Colorado, Denver
+!      November 2010
+!
+!   @precisions normal z -> c d s
+!
+
+module magma_zf77
+  
+  implicit none
+  
+  !---- Fortran interfaces to MAGMA subroutines ----
+  interface
+     
+     subroutine magma_zgetrf_gpu(m, n, A, lda, ipiv, info)
+       integer   m, n, lda, ipiv(*), info
+       integer(kind=16) A
+     end subroutine magma_zgetrf_gpu
+     
+      subroutine magma_zgetrs_gpu(trans, n, nrhs, dA, ldda, ipiv, dB, lddb, info)
+        character trans
+        integer n, nrhs, ldda, ipiv(*), lddb, info
+        integer(kind=16) dA, dB
+      end subroutine magma_zgetrs_gpu
+     
+  End interface
+end module magma_zf77
