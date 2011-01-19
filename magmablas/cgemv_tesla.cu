32d6f136445f7f6e1f8f9a2b1423e133fdeb4e1f
Mathieu Faverge <mathieu.faverge@inria.fr>
2011-01-19 23:25:57 +0000

    Add false [cz]gemv for tesla
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@847 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ magmablas/cgemv_tesla.cu
@@ -0,0 +1,20 @@
+/*
+    -- MAGMA (version 1.0) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2010
+
+*/
+
+#include "stdio.h"
+#include "cublas.h"
+#include "magma.h"
+
+#define magmablas_cgemv_tesla magmablas_cgemv
+
+extern "C" void
+magmablas_cgemv_tesla(char trans, int m, int n, float2 alpha, float2 *A, int lda, float2 *x, int incx, float2 beta, float2 *y, int incy) 
+{
+  cublasCgemv(trans, m, n, alpha, A, lda, x, incx, beta, y, incy);
+}
