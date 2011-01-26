aa5947d3b40a5125794a4c6ce48a2095d6534918
Stanimire Tomov <tomov@icl.utk.edu>
2011-01-26 21:51:43 +0000

    one more file as part of the last change
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@871 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/magma_zfortran.f90
+++ src/magma_zfortran.f90
@@ -16,17 +16,17 @@ module magma_zfortran
   interface
      
      subroutine magma_zgetrf_gpu(m, n, A, lda, ipiv, info)
-       integer,          intent(in)    :: m, n, lda
-       integer,          intent(out)   :: ipiv(*), info
-       integer(kind=16), intent(inout) :: A
+       integer,          intent(in)     :: m, n, lda
+       integer,          intent(out)    :: ipiv(*), info
+       real, dimension(4), intent(inout):: A
      end subroutine magma_zgetrf_gpu
      
      subroutine magma_zgetrs_gpu(trans, n, nrhs, dA, ldda, ipiv, dB, lddb, info)
-       character,        intent(in)    :: trans
-       integer,          intent(in)    :: n, nrhs, ldda, ipiv(*), lddb
-       integer,          intent(out)   :: info
-       integer(kind=16), intent(in)    :: dA
-       integer(kind=16), intent(inout) :: dB
+       character,        intent(in)     :: trans
+       integer,          intent(in)     :: n, nrhs, ldda, ipiv(*), lddb
+       integer,          intent(out)    :: info
+       real, dimension(4), intent(in)   :: dA
+       real, dimension(4), intent(inout):: dB
      end subroutine magma_zgetrs_gpu
      
   end interface
