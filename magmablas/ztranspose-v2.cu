2d4428e1a12b5dc40c4633a4e314c35148540ec2
Mathieu Faverge <mathieu.faverge@inria.fr>
2011-03-29 22:11:58 +0000

    First part og the patch from Evan Lezar
    git-svn-id: https://icl.cs.utk.edu/svn/magma/branches/sc_release@989 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- magmablas/ztranspose-v2.cu
+++ magmablas/ztranspose-v2.cu
@@ -180,10 +180,14 @@ magmablas_ztranspose2(cuDoubleComplex *odata, int ldo,
                       cuDoubleComplex *idata, int ldi, 
                       int m, int n )
 {
-	dim3 threads( ZSIZE_1SHARED, 8, 1 );
-	dim3 grid( (m+31)/32, (n+31)/32, 1 );
-	ztranspose3_32<<< grid, threads >>>( odata, ldo, idata, ldi, 
-                                             // m, m%32, n, n%32);
-                                             m, (32-m%32)%32, n, (32-n%32)%32);
+    /* Quick return */
+    if ( (m == 0) || (n == 0) )
+        return;
+
+    dim3 threads( ZSIZE_1SHARED, 8, 1 );
+    dim3 grid( (m+31)/32, (n+31)/32, 1 );
+    ztranspose3_32<<< grid, threads >>>( odata, ldo, idata, ldi, 
+                                         // m, m%32, n, n%32);
+                                         m, (32-m%32)%32, n, (32-n%32)%32);
 }
 
