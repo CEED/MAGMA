71601cfd184d011435f1a1b1f938478ba7eef2b0
Stanimire Tomov <tomov@icl.utk.edu>
2011-09-22 17:02:31 +0000

    added definition for the out-of-core QR
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@1151 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- include/magma_z.h
+++ include/magma_z.h
@@ -39,6 +39,9 @@ magma_int_t magma_zgeqlf( magma_int_t m, magma_int_t n,
 magma_int_t magma_zgeqrf( magma_int_t m, magma_int_t n, cuDoubleComplex *A, 
 			  magma_int_t lda, cuDoubleComplex *tau, cuDoubleComplex *work, 
 			  magma_int_t lwork, magma_int_t *info);
+magma_int_t magma_zgeqrf_ooc( magma_int_t m, magma_int_t n, cuDoubleComplex *A,
+			  magma_int_t lda, cuDoubleComplex *tau, cuDoubleComplex *work,
+		          magma_int_t lwork, magma_int_t *info);
 magma_int_t magma_zgetrf( magma_int_t m, magma_int_t n, cuDoubleComplex *A, 
 			  magma_int_t lda, magma_int_t *ipiv, 
 			  magma_int_t *info);
