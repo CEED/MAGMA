54461362117b67db8a2e01ec0e55819173b81f4f
Stanimire Tomov <tomov@icl.utk.edu>
2011-10-21 01:48:56 +0000

    incremental transpose for LU for multiGPUs
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@1218 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- magmablas/zdtoht.cu
+++ magmablas/zdtoht.cu
@@ -42,7 +42,7 @@ magmablas_zdtoht(cuDoubleComplex *dat, int ldda,
     cudaStreamCreate(&stream[1]);
 
     for(i=0; i<n; i+=nb){
-       /* Move data from GPU to CPU using two buffers; first transpose the data on the GPU */
+       /* Move data from GPU to CPU using 2 buffers; 1st transpose the data on the GPU */
        ib   = min(n-i, nb);
        cudaStreamSynchronize(stream[j%2]);
        magmablas_ztranspose2( dB + (j%2)*nb*lddb, lddb, dat+i, ldda, ib, m);
@@ -55,5 +55,47 @@ magmablas_zdtoht(cuDoubleComplex *dat, int ldda,
 
     cudaStreamDestroy( stream[0] );
     cudaStreamDestroy( stream[1] );
+}
+
+//===========================================================================
+//  This version is similar to the above but for multiGPUs. The distribution
+//  is 1D block cyclic. The input arrays are pointers for the corresponding
+//  GPUs. The streams are passed as argument, in contrast to the single GPU
+//  routine.
+//===========================================================================
+extern "C" void
+magmablas_zdtoht2(int num_gpus, cudaStream_t stream[][2],
+                  cuDoubleComplex **dat, int *ldda,
+                  cuDoubleComplex  *ha, int lda,
+                  cuDoubleComplex  **dB, int lddb,
+                  int m, int n , int nb)
+{
+    int i = 0, j[4] = {0, 0, 0, 0}, ib, k;
+
+    /* Quick return */
+    if ( (m == 0) || (n == 0) )
+        return;
+
+    if (lda < m || lddb < m){
+        printf("Wrong arguments in zdtoht.\n");
+        return;
+    }
+
+    for(i=0; i<n; i+=nb){
+       /* Move data from GPU to CPU using 2 buffers; 1st transpose the data on the GPU */
+       k = (i/nb)%num_gpus;
+       ib   = min(n-i, nb);
+       cudaSetDevice(k);
+
+       cudaStreamSynchronize(stream[k][j[k]%2]);
+       magmablas_ztranspose2( dB[k] + (j[k]%2)*nb*lddb, lddb, 
+                              dat[k]+i/(nb*num_gpus)*nb, ldda[k], ib, m);
+       cudaMemcpy2DAsync(ha+i*lda, lda*sizeof(cuDoubleComplex),
+                         dB[k] + (j[k]%2) * nb * lddb, lddb*sizeof(cuDoubleComplex),
+                         sizeof(cuDoubleComplex)*m, ib,
+                         cudaMemcpyDeviceToHost,
+                         stream[k][j[k]%2]);
+       j[k]++;
+    }
 }
 
--- magmablas/zhtodt.cu
+++ magmablas/zhtodt.cu
@@ -73,3 +73,51 @@ magmablas_zhtodt(cuDoubleComplex  *ha, int lda,
     cudaStreamDestroy( stream[1] );
 }
 
+//===========================================================================
+//  This version is similar to the above but for multiGPUs. The distribution
+//  is 1D block cyclic. The input arrays are pointers for the corresponding 
+//  GPUs. The streams are passed as argument, in contrast to the single GPU
+//  routine.
+//===========================================================================
+extern "C" void 
+magmablas_zhtodt2(int num_gpus, cudaStream_t stream[][2],
+                  cuDoubleComplex  *ha,  int  lda, 
+                  cuDoubleComplex **dat, int *ldda,
+                  cuDoubleComplex  **dB, int lddb,
+                  int m, int n , int nb)
+{
+    int i = 0, j[4] = {0, 0, 0, 0}, ib[4] ={nb, nb, nb, nb}, k = 0;
+
+    /* Quick return */
+    if ( (m == 0) || (n == 0) )
+        return;
+
+    if (lda < m || lddb < m){
+        printf("Wrong arguments in zhtodt2.\n");
+	return;
+    }
+
+    for(i=0; i<(n + num_gpus*nb); i+=nb){
+       k = (i/nb)%num_gpus;
+       cudaSetDevice(k);
+
+       if (i<n){
+          /* Move data from CPU to GPU in the second panel in the dB buffer */
+          ib[k]   = min(n-i, nb);
+          cudaMemcpy2DAsync(dB[k] + (j[k]%2)*nb*lddb, lddb*sizeof(cuDoubleComplex),
+                         ha+i*lda, lda*sizeof(cuDoubleComplex),
+                         sizeof(cuDoubleComplex)*m, ib[k], 
+                         cudaMemcpyHostToDevice,
+			 stream[k][j[k]%2]);
+       }
+       j[k]++;
+  
+       if (i> (num_gpus-1)*nb){
+          /* Make sure that the previous panel (i.e., j[k]%2) has arrived 
+             and transpose it directly into the dat matrix                  */
+          cudaStreamSynchronize( stream[k][ j[k]%2 ]);
+          magmablas_ztranspose2( dat[k]+ i/(nb*num_gpus)*nb -nb, ldda[k],
+                                 dB[k] +(j[k]%2)*nb*lddb, lddb, m, ib[k]);
+       }
+    }
+}
