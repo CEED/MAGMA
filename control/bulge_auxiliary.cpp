9921ec8aa70328b59e18c27b048175d22edcb21b
Azzam Haidar <haidar@icl.utk.edu>
2012-03-28 22:47:04 +0000

    new files for 2 stages reduction
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@1663 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ control/bulge_auxiliary.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2011      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ *
+ *
+ *     @author Azzam Haidar
+ *     @author Stan Tomov
+ *
+ *
+ */
+
+#include "common_magma.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif////////////////////////////////////////////////////////////////////////////////////////////////////
+extern "C"  magma_int_t plasma_ceildiv(magma_int_t a, magma_int_t b);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+extern "C" void findVTpos(magma_int_t N, magma_int_t NB, magma_int_t Vblksiz, magma_int_t sweep, magma_int_t st, magma_int_t *Vpos, magma_int_t *TAUpos, magma_int_t *Tpos, magma_int_t *myblkid)
+{
+  magma_int_t prevcolblknb, prevblkcnt, prevcolblkid;
+  magma_int_t curcolblknb, nbprevcolblk, mastersweep;
+  magma_int_t blkid, locj, LDV;
+  prevcolblknb = 0;
+  prevblkcnt   = 0;
+  curcolblknb  = 0;
+
+  nbprevcolblk = sweep/Vblksiz;
+  for (prevcolblkid = 0; prevcolblkid < nbprevcolblk; prevcolblkid++)
+  {
+       mastersweep  = prevcolblkid * Vblksiz;
+       prevcolblknb = plasma_ceildiv((N-(mastersweep+2)),NB);
+       prevblkcnt   = prevblkcnt + prevcolblknb;
+  }
+  curcolblknb = plasma_ceildiv((st-sweep),NB);
+  blkid       = prevblkcnt + curcolblknb -1;
+  locj        = sweep%Vblksiz;
+  LDV         = NB + Vblksiz -1;
+
+  *myblkid= blkid;
+  *Vpos   = blkid*Vblksiz*LDV  + locj*LDV + locj;
+  *TAUpos = blkid*Vblksiz + locj;
+  *Tpos   = blkid*Vblksiz*Vblksiz + locj*Vblksiz + locj;
+  //printf("voici  blkid  %d  locj %d  vpos %d tpos %d \n",blkid,locj,*Vpos,*Tpos);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+extern "C" void findVTsiz(magma_int_t N, magma_int_t NB, magma_int_t Vblksiz, magma_int_t *blkcnt, magma_int_t *LDV)
+{
+  magma_int_t colblk, nbcolblk;
+  magma_int_t curcolblknb, mastersweep;
+
+  *blkcnt   = 0;
+  nbcolblk = plasma_ceildiv((N-2),Vblksiz);
+  for (colblk = 0; colblk<nbcolblk; colblk++)
+  {
+        mastersweep = colblk * Vblksiz;
+        curcolblknb = plasma_ceildiv((N-(mastersweep+2)),NB);
+        *blkcnt      = *blkcnt + curcolblknb;
+        //printf("voici  nbcolblk %d    master sweep %d     blkcnt %d \n",nbcolblk, mastersweep,*blkcnt); 
+  }
+  *LDV= NB+Vblksiz-1;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
