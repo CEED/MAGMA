8cc2122d7e928f6b0e8d0e40f0cd7bd4fbdde98b
Mark Gates <mgates3@icl.utk.edu>
2012-05-02 01:26:55 +0000

    Cleanup release script. Add code to globally change version and date.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@1844 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- tools/MakeMagmaRelease.pl
+++ tools/MakeMagmaRelease.pl
@@ -4,116 +4,120 @@ use strict;
 use warnings;
 use Getopt::Std;
 
-my $major; 
-my $minor; 
-my $micro; 
+my $major;
+my $minor;
+my $micro;
 
 # default options
-my $DIRNAME;
-my $BASENAME;
-#my $svn    = "https://icl.cs.utk.edu/svn/magma/branches/sc_release";
-my $svn    = "https://icl.cs.utk.edu/svn/magma/trunk";
-#my $svninst= "https://icl.cs.utk.edu/svn/plasma/plasma-installer";
-my $user   = "";
-my $rc = 0;
+#my $svn     = "https://icl.cs.utk.edu/svn/magma/branches/sc_release";
+#my $svninst = "https://icl.cs.utk.edu/svn/plasma/plasma-installer";
+my $svn      = "https://icl.cs.utk.edu/svn/magma/trunk";
+my $user     = "";
+my $revision = "";
+my $rc       = 0;  # release candidate
 
-my @file2delete = (
-    "Makefile.gen",
-    "tools",
-    "quark",
-    "docs",
-    "include/Makefile",
-    "make.inc.cumin",
-    "make.inc.disco",
-    "make.inc.ig",
-    "make.inc.ig.pgi",
-    "Release-ToDo.txt",
-    "Release-ToDo-1.1.txt"
-    "BugsToFix.txt",
-    #"src/magma_zf77.cpp",
-    #"src/magma_zf77pgi.cpp",
-    "src/zgeqrf_mc.cpp",
-    "src/zgeqrf-v2.cpp",
-    "src/zgeqrf-v3.cpp",
-    "src/zgetrf_mc.cpp",
-    "src/zpotrf_mc.cpp",
-    "testing/fortran2.cpp",
-    "testing/*.txt",
-    "testing/testing_zgetrf_f.f",
-    "testing/testing_zgetrf_gpu_f.cuf",
-    "testing/testing_zgeqrf_mc.cpp",
-    "testing/testing_zgeqrf-v2.cpp",
-    "testing/testing_zpotrf_mc.cpp",
-    "testing/testing_zgetrf_mc.cpp",
-    "testing/testing_zswap.cpp",
-   );
+my @files2delete = qw(
+    Makefile.gen
+    tools
+    quark
+    docs
+    include/Makefile
+    make.inc.cumin
+    make.inc.disco
+    make.inc.ig
+    make.inc.ig.pgi
+    Release-ToDo.txt
+    Release-ToDo-1.1.txt
+    BugsToFix.txt
+    src/zgeqrf_mc.cpp
+    src/zgeqrf-v2.cpp
+    src/zgeqrf-v3.cpp
+    src/zgetrf_mc.cpp
+    src/zpotrf_mc.cpp
+    testing/fortran2.cpp
+    testing/*.txt
+    testing/testing_zgetrf_f.f
+    testing/testing_zgetrf_gpu_f.cuf
+    testing/testing_zgeqrf_mc.cpp
+    testing/testing_zgeqrf-v2.cpp
+    testing/testing_zpotrf_mc.cpp
+    testing/testing_zgetrf_mc.cpp
+    testing/testing_zswap.cpp
+);
+# Using qw() avoids need for "quotes", but comments aren't recognized inside qw()
+#src/magma_zf77.cpp
+#src/magma_zf77pgi.cpp
 
-my $RELEASE_PATH;
-my %opts;
-my $NUMREL = "";
 
-sub myCmd {
+sub myCmd
+{
     my ($cmd) = @_ ;
-    my $err = 0;
-
     print "---------------------------------------------------------------\n";
     print $cmd."\n";
     print "---------------------------------------------------------------\n";
-    $err = system($cmd);
+    my $err = system($cmd);
     if ($err != 0) {
         print "Error during execution of the following command:\n$cmd\n";
         exit;
-    }    
+    }
 }
 
-sub MakeRelease {
-
-    my $numversion = $major.'.'.$minor.'.'.$micro;
+sub MakeRelease
+{
+    my $numversion = "$major.$minor.$micro";
     my $cmd;
 
     if ( $rc > 0 ) {
-        $numversion = $numversion."-rc".$rc;
+        $numversion .= "-rc$rc";
     }
 
-    $RELEASE_PATH = $ENV{ PWD}."/magma_".$numversion;
+    my $RELEASE_PATH = $ENV{PWD}."/magma-$numversion";
 
-    # Sauvegarde du rep courant
-    my $dir = `pwd`; chop $dir;
+    # Save current directory
+    my $dir = `pwd`;
+    chomp $dir;
 
-    $cmd = 'svn export --force '.$NUMREL.' '.$user.' '.$svn.' '.$RELEASE_PATH;
+    $cmd = "svn export --force $revision $user $svn $RELEASE_PATH";
     myCmd($cmd);
-    
+
     chdir $RELEASE_PATH;
 
     # Change version in plasma.h
-    #myCmd("sed -i 's/PLASMA_VERSION_MAJOR[ ]*[0-9]/PLASMA_VERSION_MAJOR $major/' include/plasma.h"); 
-    #myCmd("sed -i 's/PLASMA_VERSION_MINOR[ ]*[0-9]/PLASMA_VERSION_MINOR $minor/' include/plasma.h");
-    #myCmd("sed -i 's/PLASMA_VERSION_MICRO[ ]*[0-9]/PLASMA_VERSION_MICRO $micro/' include/plasma.h");
+    #myCmd("perl -pi -e 's/PLASMA_VERSION_MAJOR +[0-9]+/PLASMA_VERSION_MAJOR $major/' include/plasma.h");
+    #myCmd("perl -pi -e 's/PLASMA_VERSION_MINOR +[0-9]+/PLASMA_VERSION_MINOR $minor/' include/plasma.h");
+    #myCmd("perl -pi -e 's/PLASMA_VERSION_MICRO +[0-9]+/PLASMA_VERSION_MICRO $micro/' include/plasma.h");
 
-    # Change the version in comments 
-    #myCmd("find -type f -exec sed -i 's/\@version[ ]*[.0-9]*/\@version $numversion/' {} \\;");
-
-    #Precision Generation
-    print "Generate the different precision\n"; 
+    # Change the version and date in comments
+    # TODO make a generic date tag to search for, instead of November 2011.
+    my($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime;
+    my @months = (
+        'January',   'February', 'March',    'April',
+        'May',       'June',     'July',     'August',
+        'September', 'October',  'November', 'December',
+    );
+    $year += 1900;
+    my $date = "$months[$mon] $year";
+    my $script = "s/MAGMA \\\(version [0-9.]+\\\)/MAGMA (version $numversion)/;";
+    $script .= " s/November 2011/$date/;";
+    myCmd("find . -type f -exec perl -pi -e '$script' {} \\;");
+    
+    # Precision Generation
+    print "Generate the different precisions\n";
     myCmd("touch make.inc");
     myCmd("make generation");
 
-    #Compile the documentation
-    #print "Compile the documentation\n"; 
+    # Compile the documentation
+    #print "Compile the documentation\n";
     #system("make -C ./docs");
     myCmd("rm -f make.inc");
-    
-    #Remove non required files (Makefile.gen)
-    foreach my $file (@file2delete){
-        print "Remove $file\n";
+
+    # Remove non-required files (e.g., Makefile.gen)
+    foreach my $file (@files2delete) {
         myCmd("rm -rf $RELEASE_PATH/$file");
     }
- 
-    # Remove 'include Makefile.gen from Makefile'
-    myCmd("find $RELEASE_PATH -name Makefile -exec sed -i '/Makefile.gen/ d' {} \\;");
 
     # Remove the lines relative to include directory in root Makefile
-    myCmd("sed -i '/cd include/ d' $RELEASE_PATH/Makefile");
+    myCmd("perl -ni -e 'print unless /cd include/' $RELEASE_PATH/Makefile");
 
     # Remove '.Makefile.gen files'
     myCmd("find $RELEASE_PATH -name .Makefile.gen -exec rm -f {} \\;");
@@ -123,76 +127,68 @@ sub MakeRelease {
     # Save the InstallationGuide if we want to do a plasma-installer release
     #myCmd("cp $RELEASE_PATH/InstallationGuide README-installer");
 
-    #Create tarball
+    # Create tarball
     print "Create the tarball\n";
-    $DIRNAME=`dirname $RELEASE_PATH`;
-    $BASENAME=`basename $RELEASE_PATH`;
-    chop $DIRNAME;
-    chop $BASENAME;
+    my $DIRNAME  = `dirname $RELEASE_PATH`;
+    my $BASENAME = `basename $RELEASE_PATH`;
+    chomp $DIRNAME;
+    chomp $BASENAME;
     myCmd("(cd $DIRNAME && tar cvzf ${BASENAME}.tar.gz $BASENAME)");
-
 }
 
 #sub MakeInstallerRelease {
 #
-#    my $numversion = $major.'.'.$minor.'.'.$micro;
+#    my $numversion = "$major.$minor.$micro";
 #    my $cmd;
 #
-#    $RELEASE_PATH = $ENV{ PWD}."/plasma-installer_".$numversion;
+#    $RELEASE_PATH = $ENV{ PWD}."/plasma-installer-$numversion";
 #
 #    # Sauvegarde du rep courant
-#    my $dir = `pwd`; chop $dir;
+#    my $dir = `pwd`; chomp $dir;
 #
-#    $cmd = 'svn export --force '.$NUMREL.' '.$user.' '.$svninst.' '.$RELEASE_PATH;
+#    $cmd = "svn export --force $revision $user $svninst $RELEASE_PATH";
 #    myCmd($cmd);
-#    
+#
 #    # Save the InstallationGuide if we want to do a plasma-installer release
 #    myCmd("cp README-installer $RELEASE_PATH/README");
 #
 #    #Create tarball
 #    print "Create the tarball\n";
-#    $DIRNAME=`dirname $RELEASE_PATH`;
-#    $BASENAME=`basename $RELEASE_PATH`;
-#    chop $DIRNAME;
-#    chop $BASENAME;
+#    my $DIRNAME  = `dirname $RELEASE_PATH`;
+#    my $BASENAME = `basename $RELEASE_PATH`;
+#    chomp $DIRNAME;
+#    chomp $BASENAME;
 #    myCmd("(cd $DIRNAME && tar cvzf ${BASENAME}.tar.gz $BASENAME)");
 #}
 
-sub Usage {
-    
-    print "MakeRelease.pl [ -h ][ -d Directory ] [ -u username ] [ -r numrelease ] Major Minor Micro\n";
+sub Usage
+{
+    print "MakeRelease.pl [-h] [-u username] [-r revision] Major Minor Micro\n";
     print "   -h   Print this help\n";
-    print "   -d   Choose directory for release\n";
     print "   -r   Choose svn release number\n";
     print "   -s   Choose magma directory for export\n";
     print "   -u   username\n";
-
 }
 
+my %opts;
 getopts("hd:u:r:s:c:",\%opts);
 
-if ( defined $opts{h} ){
+if ( defined $opts{h}  ) {
     Usage();
     exit;
 }
-
-if (defined $opts{d}){
-    $RELEASE_PATH = $opts{d};
-}
-if (defined $opts{u}){
+if ( defined $opts{u} ) {
     $user = "--username $opts{u}";
 }
-
-if (defined $opts{r}){
-    $NUMREL = "-r $opts{r}";
+if ( defined $opts{r} ) {
+    $revision = "-r $opts{r}";
 }
-if (defined $opts{s}){
+if ( defined $opts{s} ) {
     $svn = $opts{s};
 }
-if (defined $opts{c}){
+if ( defined $opts{c} ) {
     $rc = $opts{c};
 }
-
 if ( ($#ARGV + 1) < 3 ) {
     Usage();
     exit;
