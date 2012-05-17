309dace7320690b1113eee344a80f86a533a758f
Mark Gates <mgates3@icl.utk.edu>
2012-05-17 17:26:16 +0000

    Fix version and date to match rest of code (in interface_cuda)
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@1937 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- interface_cuda/set_get.cpp
+++ interface_cuda/set_get.cpp
@@ -1,9 +1,9 @@
 /*
- *   -- MAGMA (version 0.1) --
+ *   -- MAGMA (version 1.1) --
  *      Univ. of Tennessee, Knoxville
  *      Univ. of California, Berkeley
  *      Univ. of Colorado, Denver
- *      April 2012
+ *      November 2011
  *
  * @author Mark Gates
  * @precisions normal z -> s d c
