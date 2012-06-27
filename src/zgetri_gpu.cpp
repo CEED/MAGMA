783756ddaecf9ab58320abe62b15adc6fdbaa11c
Mark Gates <mgates3@icl.utk.edu>
2012-06-27 17:42:28 +0000

    Using cublas seems to solve NAN issues
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2097 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zgetri_gpu.cpp
+++ src/zgetri_gpu.cpp
@@ -11,18 +11,18 @@
 #include "common_magma.h"
 
 // === Define what BLAS to use ============================================
-#define PRECISION_z
-#if (defined(PRECISION_s) || defined(PRECISION_d))
-  #define magma_zgemm magmablas_zgemm
-  #define magma_ztrsm magmablas_ztrsm
-#endif
-
-#if (GPUSHMEM >= 200)
-#if (defined(PRECISION_s))
-    #undef  magma_sgemm
-    #define magma_sgemm magmablas_sgemm_fermi80
-#endif
-#endif
+// #define PRECISION_z
+// #if (defined(PRECISION_s) || defined(PRECISION_d))
+//   #define magma_zgemm magmablas_zgemm
+//   #define magma_ztrsm magmablas_ztrsm
+// #endif
+// 
+// #if (GPUSHMEM >= 200)
+// #if (defined(PRECISION_s))
+//     #undef  magma_sgemm
+//     #define magma_sgemm magmablas_sgemm_fermi80
+// #endif
+// #endif
 // === End defining what BLAS to use ======================================
 
 extern "C" magma_int_t
