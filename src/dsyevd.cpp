84fae5798f2a4e6a98cbdcf9ae398a32769fe58f
Mark Gates <mgates3@icl.utk.edu>
2012-07-26 15:50:29 +0000

    Cleanup code; remove offsets for Fortran-style indexing, fix return & info values.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2184 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/dsyevd.cpp
+++ src/dsyevd.cpp
@@ -1,4 +1,4 @@
-/*    
+/*
     -- MAGMA (version 1.1) --
        Univ. of Tennessee, Knoxville
        Univ. of California, Berkeley
@@ -6,17 +6,18 @@
        November 2011
 
        @author Stan Tomov
+       @author Mark Gates
 
        @precisions normal d -> s
 
 */
 #include "common_magma.h"
 
-extern "C" magma_int_t 
-magma_dsyevd(char jobz, char uplo, 
-             magma_int_t n, 
-             double *a, magma_int_t lda, 
-             double *w, 
+extern "C" magma_int_t
+magma_dsyevd(char jobz, char uplo,
+             magma_int_t n,
+             double *a, magma_int_t lda,
+             double *w,
              double *work, magma_int_t lwork,
              magma_int_t *iwork, magma_int_t liwork,
              magma_int_t *info)
@@ -27,56 +28,56 @@ magma_dsyevd(char jobz, char uplo,
        Univ. of Colorado, Denver
        November 2011
 
-    Purpose   
+    Purpose
     =======
-    DSYEVD computes all eigenvalues and, optionally, eigenvectors of a   
-    real symmetric matrix A.  If eigenvectors are desired, it uses a   
-    divide and conquer algorithm.   
+    DSYEVD computes all eigenvalues and, optionally, eigenvectors of
+    a real symmetric matrix A.  If eigenvectors are desired, it uses a
+    divide and conquer algorithm.
 
-    The divide and conquer algorithm makes very mild assumptions about   
-    floating point arithmetic. It will work on machines with a guard   
-    digit in add/subtract, or on those binary machines without guard   
-    digits which subtract like the Cray X-MP, Cray Y-MP, Cray C-90, or   
-    Cray-2. It could conceivably fail on hexadecimal or decimal machines   
-    without guard digits, but we know of none.   
+    The divide and conquer algorithm makes very mild assumptions about
+    floating point arithmetic. It will work on machines with a guard
+    digit in add/subtract, or on those binary machines without guard
+    digits which subtract like the Cray X-MP, Cray Y-MP, Cray C-90, or
+    Cray-2. It could conceivably fail on hexadecimal or decimal machines
+    without guard digits, but we know of none.
 
-    Arguments   
-    =========   
-    JOBZ    (input) CHARACTER*1   
-            = 'N':  Compute eigenvalues only;   
-            = 'V':  Compute eigenvalues and eigenvectors.   
+    Arguments
+    =========
+    JOBZ    (input) CHARACTER*1
+            = 'N':  Compute eigenvalues only;
+            = 'V':  Compute eigenvalues and eigenvectors.
 
-    UPLO    (input) CHARACTER*1   
-            = 'U':  Upper triangle of A is stored;   
-            = 'L':  Lower triangle of A is stored.   
+    UPLO    (input) CHARACTER*1
+            = 'U':  Upper triangle of A is stored;
+            = 'L':  Lower triangle of A is stored.
 
-    N       (input) INTEGER   
-            The order of the matrix A.  N >= 0.   
+    N       (input) INTEGER
+            The order of the matrix A.  N >= 0.
 
-    A       (input/output) DOUBLE_PRECISION array, dimension (LDA, N)   
-            On entry, the symmetric matrix A.  If UPLO = 'U', the   
-            leading N-by-N upper triangular part of A contains the   
-            upper triangular part of the matrix A.  If UPLO = 'L',   
-            the leading N-by-N lower triangular part of A contains   
-            the lower triangular part of the matrix A.   
-            On exit, if JOBZ = 'V', then if INFO = 0, A contains the   
-            orthonormal eigenvectors of the matrix A.   
-            If JOBZ = 'N', then on exit the lower triangle (if UPLO='L')   
-            or the upper triangle (if UPLO='U') of A, including the   
-            diagonal, is destroyed.   
+    A       (input/output) DOUBLE_PRECISION array, dimension (LDA, N)
+            On entry, the symmetric matrix A.  If UPLO = 'U', the
+            leading N-by-N upper triangular part of A contains the
+            upper triangular part of the matrix A.  If UPLO = 'L',
+            the leading N-by-N lower triangular part of A contains
+            the lower triangular part of the matrix A.
+            On exit, if JOBZ = 'V', then if INFO = 0, A contains the
+            orthonormal eigenvectors of the matrix A.
+            If JOBZ = 'N', then on exit the lower triangle (if UPLO='L')
+            or the upper triangle (if UPLO='U') of A, including the
+            diagonal, is destroyed.
 
-    LDA     (input) INTEGER   
-            The leading dimension of the array A.  LDA >= max(1,N).   
+    LDA     (input) INTEGER
+            The leading dimension of the array A.  LDA >= max(1,N).
 
-    W       (output) DOUBLE PRECISION array, dimension (N)   
-            If INFO = 0, the eigenvalues in ascending order.   
+    W       (output) DOUBLE PRECISION array, dimension (N)
+            If INFO = 0, the eigenvalues in ascending order.
 
-    WORK    (workspace/output) DOUBLE_PRECISION array, dimension (MAX(1,LWORK))   
-            On exit, if INFO = 0, WORK[0] returns the optimal LWORK.   
+    WORK    (workspace/output) DOUBLE_PRECISION array, dimension (MAX(1,LWORK))
+            On exit, if INFO = 0, WORK[0] returns the optimal LWORK.
 
-    LWORK   (input) INTEGER   
-            The length of the array WORK.   
-            If N <= 1,                LWORK must be at least 1.   
+    LWORK   (input) INTEGER
+            The length of the array WORK.
+            If N <= 1,                LWORK must be at least 1.
             If JOBZ  = 'N' and N > 1, LWORK must be at least 2*N + N*NB.
             If JOBZ  = 'V' and N > 1, LWORK must be at least 1 + 6*N + 2*N**2.
             NB can be obtained through magma_get_dsytrd_nb(N).
@@ -87,14 +88,14 @@ magma_dsyevd(char jobz, char uplo,
             and IWORK arrays, and no error message related to LWORK or
             LIWORK is issued by XERBLA.
 
-    IWORK   (workspace/output) INTEGER array, dimension (MAX(1,LIWORK))   
-            On exit, if INFO = 0, IWORK[0] returns the optimal LIWORK.   
+    IWORK   (workspace/output) INTEGER array, dimension (MAX(1,LIWORK))
+            On exit, if INFO = 0, IWORK[0] returns the optimal LIWORK.
 
-    LIWORK  (input) INTEGER   
-            The dimension of the array IWORK.   
-            If N <= 1,                LIWORK must be at least 1.   
-            If JOBZ  = 'N' and N > 1, LIWORK must be at least 1.   
-            If JOBZ  = 'V' and N > 1, LIWORK must be at least 3 + 5*N.   
+    LIWORK  (input) INTEGER
+            The dimension of the array IWORK.
+            If N <= 1,                LIWORK must be at least 1.
+            If JOBZ  = 'N' and N > 1, LIWORK must be at least 1.
+            If JOBZ  = 'V' and N > 1, LIWORK must be at least 3 + 5*N.
 
             If LIWORK = -1, then a workspace query is assumed; the
             routine only calculates the optimal sizes of the WORK and
@@ -102,24 +103,24 @@ magma_dsyevd(char jobz, char uplo,
             the WORK and IWORK arrays, and no error message related to
             LWORK or LIWORK is issued by XERBLA.
 
-    INFO    (output) INTEGER   
-            = 0:  successful exit   
-            < 0:  if INFO = -i, the i-th argument had an illegal value   
-            > 0:  if INFO = i and JOBZ = 'N', then the algorithm failed   
-                  to converge; i off-diagonal elements of an intermediate   
-                  tridiagonal form did not converge to zero;   
-                  if INFO = i and JOBZ = 'V', then the algorithm failed   
-                  to compute an eigenvalue while working on the submatrix   
-                  lying in rows and columns INFO/(N+1) through   
-                  mod(INFO,N+1).   
+    INFO    (output) INTEGER
+            = 0:  successful exit
+            < 0:  if INFO = -i, the i-th argument had an illegal value
+            > 0:  if INFO = i and JOBZ = 'N', then the algorithm failed
+                  to converge; i off-diagonal elements of an intermediate
+                  tridiagonal form did not converge to zero;
+                  if INFO = i and JOBZ = 'V', then the algorithm failed
+                  to compute an eigenvalue while working on the submatrix
+                  lying in rows and columns INFO/(N+1) through
+                  mod(INFO,N+1).
 
-    Further Details   
-    ===============   
-    Based on contributions by   
-       Jeff Rutter, Computer Science Division, University of California   
-       at Berkeley, USA   
+    Further Details
+    ===============
+    Based on contributions by
+       Jeff Rutter, Computer Science Division, University of California
+       at Berkeley, USA
 
-    Modified description of INFO. Sven, 16 Feb 05.   
+    Modified description of INFO. Sven, 16 Feb 05.
     =====================================================================   */
 
     char uplo_[2] = {uplo, 0};
@@ -129,7 +130,6 @@ magma_dsyevd(char jobz, char uplo,
     magma_int_t izero = 0;
     double d_one = 1.;
     
-    magma_int_t a_dim1, a_offset;
     double d__1;
 
     double eps;
@@ -173,10 +173,13 @@ magma_dsyevd(char jobz, char uplo,
         liwmin = 1;
     }
     else if ( wantz ) {
+        // for sytrd: e (n), tau (n), work (n*nb)
+        // for stedx: e (n),   ...  , z (n^2), work (1 + 4n + n^2)
         lwmin  = 1 + 6*n + 2*n*n;
         liwmin = 3 + 5*n;
     }
     else {
+        // for sytrd: e (n), tau (n), work (n*nb)
         lwmin  = 2*n + n*nb;
         liwmin = 1;
     }
@@ -193,15 +196,15 @@ magma_dsyevd(char jobz, char uplo,
 
     if (*info != 0) {
         magma_xerbla( __func__, -(*info) );
-        return MAGMA_ERR_ILLEGAL_VALUE;
+        return *info;
     }
     else if (lquery) {
-        return MAGMA_SUCCESS;
+        return *info;
     }
 
     /* Quick return if possible */
     if (n == 0) {
-        return MAGMA_SUCCESS;
+        return *info;
     }
 
     if (n == 1) {
@@ -209,26 +212,19 @@ magma_dsyevd(char jobz, char uplo,
         if (wantz) {
             a[0] = 1.;
         }
-        return MAGMA_SUCCESS;
+        return *info;
     }
-
-    a_dim1 = lda;
-    a_offset = 1 + a_dim1;
-    a -= a_offset;
-    --w;
-    --work;
-    --iwork;
 
     /* Get machine constants. */
     safmin = lapackf77_dlamch("Safe minimum");
-    eps = lapackf77_dlamch("Precision");
+    eps    = lapackf77_dlamch("Precision");
     smlnum = safmin / eps;
     bignum = 1. / smlnum;
     rmin = magma_dsqrt(smlnum);
     rmax = magma_dsqrt(bignum);
 
     /* Scale matrix to allowable range, if necessary. */
-    anrm = lapackf77_dlansy("M", uplo_, &n, &a[a_offset], &lda, &work[1]);
+    anrm = lapackf77_dlansy("M", uplo_, &n, a, &lda, work );
     iscale = 0;
     if (anrm > 0. && anrm < rmin) {
         iscale = 1;
@@ -238,12 +234,12 @@ magma_dsyevd(char jobz, char uplo,
         sigma = rmax / anrm;
     }
     if (iscale == 1) {
-        lapackf77_dlascl(uplo_, &izero, &izero, &d_one, &sigma, &n, &n, &a[a_offset], 
+        lapackf77_dlascl(uplo_, &izero, &izero, &d_one, &sigma, &n, &n, a,
                 &lda, info);
     }
 
     /* Call DSYTRD to reduce symmetric matrix to tridiagonal form. */
-    inde = 1;
+    inde   = 0;
     indtau = inde + n;
     indwrk = indtau + n;
     llwork = lwork - indwrk + 1;
@@ -251,27 +247,25 @@ magma_dsyevd(char jobz, char uplo,
     llwrk2 = lwork - indwk2 + 1;
   
 //#define ENABLE_TIMER
-#ifdef ENABLE_TIMER 
+#ifdef ENABLE_TIMER
     magma_timestr_t start, end;
-    
     start = get_current_time();
 #endif
 
-    magma_dsytrd(uplo_[0], n, &a[a_offset], lda, &w[1], &work[inde],
+    magma_dsytrd(uplo, n, a, lda, w, &work[inde],
                  &work[indtau], &work[indwrk], llwork, &iinfo);
     
-#ifdef ENABLE_TIMER    
+#ifdef ENABLE_TIMER
     end = get_current_time();
-    
     printf("time dsytrd = %6.2f\n", GetTimerValue(start,end)/1000.);
-#endif        
+#endif
 
-    /* For eigenvalues only, call DSTERF.  For eigenvectors, first call   
-       DSTEDC to generate the eigenvector matrix, WORK(INDWRK), of the   
-       tridiagonal matrix, then call DORMTR to multiply it to the Householder 
+    /* For eigenvalues only, call DSTERF.  For eigenvectors, first call
+       DSTEDC to generate the eigenvector matrix, WORK(INDWRK), of the
+       tridiagonal matrix, then call DORMTR to multiply it to the Householder
        transformations represented as Householder vectors in A. */
     if (! wantz) {
-        lapackf77_dsterf(&n, &w[1], &work[inde], info);
+        lapackf77_dsterf(&n, w, &work[inde], info);
     } else {
 
 #ifdef ENABLE_TIMER
@@ -279,45 +273,43 @@ magma_dsyevd(char jobz, char uplo,
 #endif
         
         if (MAGMA_SUCCESS != magma_dmalloc( &dwork, 3*n*(n/2 + 1) )) {
-            *info = -14;
-            return MAGMA_ERR_DEVICE_ALLOC;
+            *info = MAGMA_ERR_DEVICE_ALLOC;
+            return *info;
         }
         
-        magma_dstedx('A', n, 0., 0., 0, 0, &w[1], &work[inde],
+        magma_dstedx('A', n, 0., 0., 0, 0, w, &work[inde],
                      &work[indwrk], n, &work[indwk2],
-                     llwrk2, &iwork[1], liwork, dwork, info);
+                     llwrk2, iwork, liwork, dwork, info);
         
         magma_free( dwork );
         
-#ifdef ENABLE_TIMER  
+#ifdef ENABLE_TIMER
         end = get_current_time();
-        
         printf("time dstedx = %6.2f\n", GetTimerValue(start,end)/1000.);
         
         start = get_current_time();
 #endif
 
-        magma_dormtr(MagmaLeft, uplo, MagmaNoTrans, n, n, &a[a_offset], lda, &work[indtau],
+        magma_dormtr(MagmaLeft, uplo, MagmaNoTrans, n, n, a, lda, &work[indtau],
                      &work[indwrk], n, &work[indwk2], llwrk2, &iinfo);
         
-        lapackf77_dlacpy("A", &n, &n, &work[indwrk], &n, &a[a_offset], &lda);
+        lapackf77_dlacpy("A", &n, &n, &work[indwrk], &n, a, &lda);
 
-#ifdef ENABLE_TIMER    
+#ifdef ENABLE_TIMER
         end = get_current_time();
-        
         printf("time dormtr + copy = %6.2f\n", GetTimerValue(start,end)/1000.);
-#endif        
+#endif
 
     }
 
     /* If matrix was scaled, then rescale eigenvalues appropriately. */
     if (iscale == 1) {
         d__1 = 1. / sigma;
-        blasf77_dscal(&n, &d__1, &w[1], &ione);
+        blasf77_dscal(&n, &d__1, w, &ione);
     }
 
-    work[1]  = lwmin * (1. + dlamch_("Epsilon"));  // round up
-    iwork[1] = liwmin;
+    work[0]  = lwmin * (1. + dlamch_("Epsilon"));  // round up
+    iwork[0] = liwmin;
 
-    return MAGMA_SUCCESS;
+    return *info;
 } /* magma_dsyevd */
