8daf45a02963441394eff5e3c6670a30b710ba1f
Mark Gates <mgates3@icl.utk.edu>
2012-08-30 20:35:29 +0000

    Update argument processing to allow multiple -N and for consistency
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2221 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- testing/testing_zgeqrf.cpp
+++ testing/testing_zgeqrf.cpp
@@ -23,14 +23,6 @@
 #include "magma_lapack.h"
 #include "testings.h"
 
-// Flops formula
-#define PRECISION_z
-#if defined(PRECISION_z) || defined(PRECISION_c)
-#define FLOPS(m, n) ( 6.*FMULS_GEQRF(m, n) + 2.*FADDS_GEQRF(m, n) )
-#else
-#define FLOPS(m, n) (    FMULS_GEQRF(m, n) +    FADDS_GEQRF(m, n) )
-#endif
-
 /* ////////////////////////////////////////////////////////////////////////////
    -- Testing zgeqrf
 */
@@ -38,46 +30,66 @@ int main( int argc, char** argv)
 {
     TESTING_CUDA_INIT();
 
-    magma_timestr_t       start, end;
-    double           flops, gpu_perf, cpu_perf;
-    double           matnorm, work[1];
+    real_Double_t    gflops, gpu_perf, gpu_time, cpu_perf, cpu_time;
+    double           error, work[1];
     cuDoubleComplex  c_neg_one = MAGMA_Z_NEG_ONE;
     cuDoubleComplex *h_A, *h_R, *tau, *h_work, tmp[1];
 
     /* Matrix size */
     magma_int_t M = 0, N = 0, n2, lda, lwork;
-    magma_int_t size[10] = {1024,2048,3072,4032,5184,6016,7040,8064,9088,10112};
+    const int MAXTESTS = 10;
+    magma_int_t msize[MAXTESTS] = { 1024, 2048, 3072, 4032, 5184, 6016, 7040, 8064, 9088, 10112 };
+    magma_int_t nsize[MAXTESTS] = { 1024, 2048, 3072, 4032, 5184, 6016, 7040, 8064, 9088, 10112 };
 
     magma_int_t i, info, min_mn, nb;
     magma_int_t ione     = 1;
     magma_int_t ISEED[4] = {0,0,0,1};
+    magma_int_t checkres;
 
-    if (argc != 1){
-        for(i = 1; i<argc; i++){
-            if (strcmp("-N", argv[i])==0)
-                N = atoi(argv[++i]);
-            else if (strcmp("-M", argv[i])==0)
-                M = atoi(argv[++i]);
-        }
-        if ( M == 0 ) {
-            M = N;
-        }
-        if ( N == 0 ) {
-            N = M;
-        }
-        if (N>0 && M>0)
-            printf("  testing_zgeqrf -M %d -N %d\n\n", (int) M, (int) N);
-        else
-            {
-                printf("\nUsage: \n");
-                printf("  testing_zgeqrf -M %d -N %d\n\n", (int) M, (int) N);
+    checkres = getenv("MAGMA_TESTINGS_CHECK") != NULL;
+
+    // process command line arguments
+    printf( "\nUsage: %s -N <m,n> -c\n", argv[0] );
+    printf( "  -N can be repeated up to %d times. If only m is given, then m=n.\n", MAXTESTS );
+    printf( "  -c or setting $MAGMA_TESTINGS_CHECK runs LAPACK and checks result.\n\n" );
+    int ntest = 0;
+    for( int i = 1; i < argc; ++i ) {
+        if ( strcmp("-N", argv[i]) == 0 && i+1 < argc ) {
+            magma_assert( ntest < MAXTESTS, "error: -N repeated more than maximum %d tests\n", MAXTESTS );
+            int m, n;
+            info = sscanf( argv[++i], "%d,%d", &m, &n );
+            if ( info == 2 && m > 0 && n > 0 ) {
+                msize[ ntest ] = m;
+                nsize[ ntest ] = n;
+            }
+            else if ( info == 1 && m > 0 ) {
+                msize[ ntest ] = m;
+                nsize[ ntest ] = m;  // implicitly
+            }
+            else {
+                printf( "error: -N %s is invalid; ensure m > 0, n > 0.\n", argv[i] );
                 exit(1);
             }
+            M = max( M, msize[ ntest ] );
+            N = max( N, msize[ ntest ] );
+            ntest++;
+        }
+        else if ( strcmp("-M", argv[i]) == 0 ) {
+            printf( "-M has been replaced in favor of -N m,n to allow -N to be repeated.\n\n" );
+            exit(1);
+        }
+        else if ( strcmp("-c", argv[i]) == 0 ) {
+            checkres = true;
+        }
+        else {
+            printf( "invalid argument: %s\n", argv[i] );
+            exit(1);
+        }
     }
-    else {
-        printf("\nUsage: \n");
-        printf("  testing_zgeqrf -M %d -N %d\n\n", 1024, 1024);
-        M = N = size[9];
+    if ( ntest == 0 ) {
+        ntest = MAXTESTS;
+        M = msize[ntest-1];
+        N = nsize[ntest-1];
     }
 
     n2  = M * N;
@@ -95,16 +107,15 @@ int main( int argc, char** argv)
 
     TESTING_MALLOC( h_work, cuDoubleComplex, lwork );
 
-    printf("  M     N   CPU GFlop/s   GPU GFlop/s    ||R||_F / ||A||_F\n");
-    printf("==========================================================\n");
-    for(i=0; i<10; i++){
-         if (argc == 1){
-            M = N = size[i];
-        }
+    printf("  M     N     CPU GFlop/s (sec)   GPU GFlop/s (sec)   ||R||_F / ||A||_F\n");
+    printf("=======================================================================\n");
+    for( i = 0; i < ntest; ++i ) {
+        M = msize[i];
+        N = nsize[i];
         min_mn= min(M, N);
         lda   = M;
         n2    = lda*N;
-        flops = FLOPS( (double)M, (double)N ) / 1000000;
+        gflops = FLOPS_ZGEQRF( M, N ) / 1e9;
 
         /* Initialize the matrix */
         lapackf77_zlarnv( &ione, ISEED, &n2, h_A );
@@ -113,45 +124,47 @@ int main( int argc, char** argv)
         /* ====================================================================
            Performs operation using MAGMA
            =================================================================== */
-        start = get_current_time();
+        gpu_time = magma_wtime();
         magma_zgeqrf(M, N, h_R, lda, tau, h_work, lwork, &info);
-        end = get_current_time();
-        if (info < 0)
-            printf("Argument %d of magma_zgeqrf had an illegal value.\n", (int) -info);
+        gpu_time = magma_wtime() - gpu_time;
+        gpu_perf = gflops / gpu_time;
+        if (info != 0)
+            printf("magma_zgeqrf returned error %d.\n", (int) info);
         
-        gpu_perf = flops / GetTimerValue(start, end);
+        if ( checkres ) {
+            /* =====================================================================
+               Performs operation using LAPACK
+               =================================================================== */
+            cpu_time = magma_wtime();
+            lapackf77_zgeqrf(&M, &N, h_A, &lda, tau, h_work, &lwork, &info);
+            cpu_time = magma_wtime() - cpu_time;
+            cpu_perf = gflops / cpu_time;
+            if (info != 0)
+                printf("lapackf77_zgeqrf returned error %d.\n", (int) info);
 
-        /* =====================================================================
-           Performs operation using LAPACK
-           =================================================================== */
-        start = get_current_time();
-        lapackf77_zgeqrf(&M, &N, h_A, &lda, tau, h_work, &lwork, &info);
-        end = get_current_time();
-        if (info < 0)
-            printf("Argument %d of lapack_zgeqrf had an illegal value.\n", (int) -info);
+            /* =====================================================================
+               Check the result compared to LAPACK
+               =================================================================== */
+            error = lapackf77_zlange("f", &M, &N, h_A, &lda, work);
+            blasf77_zaxpy(&n2, &c_neg_one, h_A, &ione, h_R, &ione);
+            error = lapackf77_zlange("f", &M, &N, h_R, &lda, work) / error;
 
-        cpu_perf = flops / GetTimerValue(start, end);
-
-        /* =====================================================================
-           Check the result compared to LAPACK
-           =================================================================== */
-        matnorm = lapackf77_zlange("f", &M, &N, h_A, &lda, work);
-        blasf77_zaxpy(&n2, &c_neg_one, h_A, &ione, h_R, &ione);
-
-        printf("%5d %5d  %6.2f         %6.2f        %e\n",
-               (int) M, (int) N, cpu_perf, gpu_perf,
-               lapackf77_zlange("f", &M, &N, h_R, &lda, work) / matnorm);
-
-        if (argc != 1)
-            break;
+            printf("%5d %5d   %7.2f (%7.2f)   %7.2f (%7.2f)   %8.2e\n",
+                   (int) M, (int) N, cpu_perf, cpu_time, gpu_perf, gpu_time, error );
+        }
+        else {
+            printf("%5d %5d     ---   (  ---  )   %7.2f (%7.2f)     ---  \n",
+                   (int) M, (int) N, gpu_perf, gpu_time);
+        }
     }
 
     /* Memory clean up */
     TESTING_FREE( tau );
     TESTING_FREE( h_A );
-    TESTING_HOSTFREE( h_R );
     TESTING_FREE( h_work );
+    TESTING_HOSTFREE( h_R );
 
     /* Shutdown */
     TESTING_CUDA_FINALIZE();
+    return 0;
 }
