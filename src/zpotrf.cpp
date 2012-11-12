66b37693f071a2344ee64ec8352671be9a4e53e2
Stanimire Tomov <tomov@icl.utk.edu>
2012-11-12 20:02:55 +0000

    do not redefine BLAS for Kepler in Cholesky
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2512 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zpotrf.cpp
+++ src/zpotrf.cpp
@@ -12,12 +12,12 @@
 
 // === Define what BLAS to use ============================================
 #define PRECISION_z
-#if (defined(PRECISION_s) || defined(PRECISION_d))
+#if (GPUSHMEM <= 200) && (defined(PRECISION_s) || defined(PRECISION_d))
   #define magma_zgemm magmablas_zgemm
   #define magma_ztrsm magmablas_ztrsm
 #endif
 
-#if (GPUSHMEM >= 200)
+#if (GPUSHMEM == 200)
 #if (defined(PRECISION_s))
      #undef  magma_sgemm
      #define magma_sgemm magmablas_sgemm_fermi80
