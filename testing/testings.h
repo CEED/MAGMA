72098349d056c74c5d84294e8dd0dfc0007e5910
Mark Gates <mgates3@icl.utk.edu>
2012-11-29 22:44:41 +0000

    Add function to parse command line arguments
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2571 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- testing/testings.h
+++ testing/testings.h
@@ -79,15 +79,15 @@
     }
 
 
-#define TESTING_FREE(ptr)                                                  \
-    magma_free_cpu(ptr);
+#define TESTING_FREE( ptr )                                                \
+    magma_free_cpu( ptr );
 
 
-#define TESTING_HOSTFREE(ptr)                                              \
+#define TESTING_HOSTFREE( ptr )                                            \
     magma_free_pinned( ptr );
 
 
-#define TESTING_DEVFREE(ptr)                                               \
+#define TESTING_DEVFREE( ptr )                                             \
     magma_free( ptr );
 
 
@@ -106,6 +106,51 @@ void magma_dhpd( magma_int_t N, double*          A, magma_int_t lda );
 void magma_shpd( magma_int_t N, float*           A, magma_int_t lda );
 
 void magma_assert( bool condition, const char* msg, ... );
+
+#define MAX_NTEST 50
+
+typedef struct magma_opts
+{
+    // matrix size
+    magma_int_t ntest;
+    magma_int_t msize[ MAX_NTEST ];
+    magma_int_t nsize[ MAX_NTEST ];
+    magma_int_t ksize[ MAX_NTEST ];
+    magma_int_t mmax;
+    magma_int_t nmax;
+    magma_int_t kmax;
+    
+    // scalars
+    magma_int_t device;
+    magma_int_t nb;
+    magma_int_t nrhs;
+    magma_int_t nstream;
+    magma_int_t ngpu;
+    magma_int_t niter;
+    magma_int_t nthread;
+    magma_int_t itype;     // hegvd: problem type
+    magma_int_t svd_work;  // gesvd
+    magma_int_t version;   // hemm_mgpu, hetrd
+    double      fraction;  // hegvdx
+    
+    // boolean arguments
+    int check;
+    int lapack;
+    int warmup;
+    int all;
+    
+    // lapack flags
+    char uplo;
+    char transA;
+    char transB;
+    char jobu;    // gesvd:  no left  singular vectors
+    char jobvt;   // gesvd:  no right singular vectors
+    char jobz;    // heev:   no eigen vectors
+    char jobvr;   // geev:   no right eigen vectors
+    char jobvl;   // geev:   no left  eigen vectors
+} magma_opts;
+
+void parse_opts( int argc, char** argv, magma_opts *opts );
 
 #ifdef __cplusplus
 }
