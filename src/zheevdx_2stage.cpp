f65588746684c7cef1c452190d87b7a67c40769e
Azzam Haidar <haidar@icl.utk.edu>
2012-12-01 19:45:10 +0000

    updating ldv, Raffaele Please could you update the other macro that use ldv  for example the function get_lq2
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2578 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zheevdx_2stage.cpp
+++ src/zheevdx_2stage.cpp
@@ -262,7 +262,7 @@ magma_zheevdx_2stage(char jobz, char range, char uplo,
     magma_int_t Vblksiz = magma_zbulge_get_Vblksiz(n, nb);
 
     magma_int_t ldt = Vblksiz;
-    magma_int_t ldv = nb + Vblksiz + 1;
+    magma_int_t ldv = nb + Vblksiz - 1;
     magma_int_t blkcnt = magma_bulge_get_blkcnt(n, nb, Vblksiz);
 
     magma_int_t lq2 = blkcnt * Vblksiz * (ldt + ldv + 1);
