33aa3a03a5d3948c4ac53eac20e5f5c12ef23026
Raffaele Solca <rasolca@itp.phys.ethz.ch>
2012-12-03 16:38:41 +0000

    testing for real 2-stage general eigensolver + updated Makefiles
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2596 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ testing/testing_dsygvdx_2stage.cpp
@@ -0,0 +1,347 @@
+/*
+    -- MAGMA (version 1.0) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2010
+
+    @author Raffaele Solca
+
+    @precisions normal d -> s
+
+*/
+
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <cuda_runtime_api.h>
+#include <cublas.h>
+
+// includes, project
+#include "magma.h"
+#include "magma_lapack.h"
+#include "testings.h"
+
+#define absv(v1) ((v1)>0? (v1): -(v1))
+
+extern"C" {
+    magma_int_t magma_dsygvdx_2stage(magma_int_t itype, char jobz, char range, char uplo, magma_int_t n,
+                                     double *a, magma_int_t lda, double *b, magma_int_t ldb,
+                                     double vl, double vu, magma_int_t il, magma_int_t iu,
+                                     magma_int_t *m, double *w, double *work, magma_int_t lwork,
+                                     magma_int_t *iwork, magma_int_t liwork, magma_int_t *info);
+
+    /*magma_int_t magma_dsygvdx_2stage_m(magma_int_t nrgpu, magma_int_t itype, char jobz, char range, char uplo, magma_int_t n,
+                                       double *a, magma_int_t lda, double *b, magma_int_t ldb,
+                                       double vl, double vu, magma_int_t il, magma_int_t iu,
+                                       magma_int_t *m, double *w, double *work, magma_int_t lwork,
+                                       magma_int_t *iwork, magma_int_t liwork, magma_int_t *info);
+    */
+    magma_int_t magma_dbulge_get_lq2(magma_int_t n);
+}
+/* ////////////////////////////////////////////////////////////////////////////
+   -- Testing dsygvdx
+*/
+int main( int argc, char** argv)
+{
+
+//#define USE_MGPU
+#ifdef USE_MGPU
+    TESTING_CUDA_INIT_MGPU();
+#else
+    TESTING_CUDA_INIT();
+#endif
+    magma_int_t nrgpu =1;
+
+    double *h_A, *h_R, *h_B, *h_S, *h_work;
+    double *w1, *w2;
+    magma_int_t *iwork;
+    double gpu_time, cpu_time;
+
+    magma_timestr_t start, end;
+
+    /* Matrix size */
+    magma_int_t N=0, n2;
+    magma_int_t size[4] = {1024,2048,4100,6001};
+
+    magma_int_t i, itype, info;
+    magma_int_t ione = 1, izero = 0;
+    magma_int_t five = 5;
+
+    double d_zero    =  0.;
+    double d_one     =  1.;
+    double d_neg_one = -1.;
+    double d_ten     = 10.;
+    magma_int_t ISEED[4] = {0,0,0,1};
+
+    magma_int_t il,iu,m1,m2;
+    double vl,vu;
+
+    double fraction_ev = 0;
+
+    //const char *uplo = MagmaLowerStr;
+    char *uplo = (char*)MagmaLowerStr;
+    //char *uplo = (char*)MagmaUpperStr;
+    char *jobz = (char*)MagmaVectorsStr;
+    char range = 'A';
+    itype = 1;
+
+    magma_int_t checkres;
+    double result[2];
+
+    int flagN = 0;
+
+    if (argc != 1){
+        for(i = 1; i<argc; i++){
+            if (strcmp("-N", argv[i])==0){
+                N = atoi(argv[++i]);
+                if (N>0){
+                   printf("  testing_dsygvdx -N %d\n\n", (int) N);
+                   flagN=1;
+                }
+                else {
+                   printf("\nUsage: \n");
+                   printf("  testing_dsygvdx -N %d\n\n", (int) N);
+                   exit(1);
+                }
+            }
+            if (strcmp("-ngpu", argv[i])==0){
+                nrgpu = atoi(argv[++i]);
+                if (nrgpu>0){
+                   printf("  testing_dsygvdx -ngpu %d\n\n", (int) nrgpu);
+                }
+                else {
+                   printf("\nUsage: \n");
+                   printf("  testing_dsygvdx -ngpu %d\n\n", (int) nrgpu);
+                   exit(1);
+                }
+            }
+            if (strcmp("-itype", argv[i])==0){
+                itype = atoi(argv[++i]);
+                if (itype>0 && itype <= 3){
+                   printf("  testing_dsygvdx -itype %d\n\n", (int) itype);
+                }
+                else {
+                   printf("\nUsage: \n");
+                   printf("  testing_dsygvdx -itype %d\n\n", (int) itype);
+                   exit(1);
+                }
+            }
+            if (strcmp("-FE", argv[i])==0){
+                fraction_ev = atof(argv[++i]);
+                if (fraction_ev > 0 && fraction_ev <= 1){
+                    printf("  testing_dsygvdx -FE %f\n\n", fraction_ev);
+                }
+                else {
+                    fraction_ev = 0;
+                }
+            }
+            if (strcmp("-L", argv[i])==0){
+              uplo = (char*)MagmaLowerStr;
+              printf("  testing_dsygvdx -L");
+            }
+            if (strcmp("-U", argv[i])==0){
+              uplo = (char*)MagmaUpperStr;
+              printf("  testing_dsygvdx -U");
+            }
+
+        }
+
+    } else {
+        printf("\nUsage: \n");
+        printf("  testing_dsygvdx -L/U -N %d -itype %d\n\n", 1024, 1);
+    }
+
+    if(!flagN)
+        N = size[3];
+
+    checkres  = getenv("MAGMA_TESTINGS_CHECK") != NULL;
+
+    n2  = N * N;
+
+    /* Allocate host memory for the matrix */
+    TESTING_MALLOC(   h_A, double, n2);
+    TESTING_MALLOC(   h_B, double, n2);
+    TESTING_MALLOC(    w1, double,  N);
+    TESTING_MALLOC(    w2, double,  N);
+    TESTING_HOSTALLOC(h_R, double, n2);
+    TESTING_HOSTALLOC(h_S, double, n2);
+
+    magma_int_t nb = magma_get_dsytrd_nb(N);
+    magma_int_t lwork = magma_dbulge_get_lq2(N) + 1 + 6*N + 2*N*N;
+    magma_int_t liwork = 3 + 5*N;
+
+    TESTING_HOSTALLOC(h_work, double,      lwork);
+    TESTING_MALLOC(    iwork, magma_int_t, liwork);
+
+    printf("  N     M     GPU Time(s) \n");
+    printf("==========================\n");
+    for(i=0; i<4; i++){
+        if (!flagN){
+            N = size[i];
+            n2 = N*N;
+        }
+        if (fraction_ev == 0){
+            il = N / 10;
+            iu = N / 5+il;
+        }
+        else {
+            il = 1;
+            iu = (int)(fraction_ev*N);
+            if (iu < 1) iu = 1;
+        }
+
+        /* Initialize the matrix */
+        lapackf77_dlarnv( &ione, ISEED, &n2, h_A );
+
+        lapackf77_dlarnv( &ione, ISEED, &n2, h_B );
+        /* increase the diagonal */
+        {
+          magma_int_t i, j;
+          for(i=0; i<N; i++) {
+            h_B[i*N+i] += N;
+          }
+        }
+        lapackf77_dlacpy( MagmaUpperLowerStr, &N, &N, h_A, &N, h_R, &N );
+        lapackf77_dlacpy( MagmaUpperLowerStr, &N, &N, h_B, &N, h_S, &N );
+
+#ifdef USE_MGPU
+        magma_dsygvdx_2stage_m(nrgpu, itype, jobz[0], range, uplo[0],
+                               N, h_R, N, h_S, N, vl, vu, il, iu, &m1, w1,
+                               h_work, lwork,
+                               iwork, liwork,
+                               &info);
+#else
+        magma_dsygvdx_2stage(itype, jobz[0], range, uplo[0],
+                             N, h_R, N, h_S, N, vl, vu, il, iu, &m1, w1,
+                             h_work, lwork,
+                             iwork, liwork,
+                             &info);
+#endif
+
+        lapackf77_dlacpy( MagmaUpperLowerStr, &N, &N, h_A, &N, h_R, &N );
+        lapackf77_dlacpy( MagmaUpperLowerStr, &N, &N, h_B, &N, h_S, &N );
+
+
+        /* ====================================================================
+           Performs operation using MAGMA
+           =================================================================== */
+        start = get_current_time();
+#ifdef USE_MGPU
+        magma_dsygvdx_2stage_m(nrgpu, itype, jobz[0], range, uplo[0],
+                               N, h_R, N, h_S, N, vl, vu, il, iu, &m1, w1,
+                               h_work, lwork,
+                               iwork, liwork,
+                               &info);
+#else
+        magma_dsygvdx_2stage(itype, jobz[0], range, uplo[0],
+                             N, h_R, N, h_S, N, vl, vu, il, iu, &m1, w1,
+                             h_work, lwork,
+                             iwork, liwork,
+                             &info);
+#endif
+        end = get_current_time();
+
+        gpu_time = GetTimerValue(start,end)/1000.;
+
+        if ( checkres ) {
+          /* =====================================================================
+             Check the results following the LAPACK's [ds]sygvdx routine.
+             A x = lambda B x is solved
+             and the following 3 tests computed:
+             (1)    | A Z - B Z D | / ( |A||Z| N )  (itype = 1)
+                    | A B Z - Z D | / ( |A||Z| N )  (itype = 2)
+                    | B A Z - Z D | / ( |A||Z| N )  (itype = 3)
+             (2)    | S(with V) - S(w/o V) | / | S |
+             =================================================================== */
+          double temp1, temp2;
+          double *tau;
+          double* rwork= h_work+N*N;
+
+          result[0] = 1.;
+          result[0] /= lapackf77_dlansy("1",uplo, &N, h_A, &N, rwork);
+          result[0] /= lapackf77_dlange("1",&N , &m1, h_R, &N, rwork);
+
+          if (itype == 1){
+            blasf77_dsymm("L", uplo, &N, &m1, &d_one, h_A, &N, h_R, &N, &d_zero, h_work, &N);
+            for(int i=0; i<m1; ++i)
+              blasf77_dscal(&N, &w1[i], &h_R[i*N], &ione);
+            blasf77_dsymm("L", uplo, &N, &m1, &d_neg_one, h_B, &N, h_R, &N, &d_one, h_work, &N);
+            result[0] *= lapackf77_dlange("1", &N, &m1, h_work, &N, rwork)/N;
+          }
+          else if (itype == 2){
+            blasf77_dsymm("L", uplo, &N, &m1, &d_one, h_B, &N, h_R, &N, &d_zero, h_work, &N);
+            for(int i=0; i<m1; ++i)
+              blasf77_dscal(&N, &w1[i], &h_R[i*N], &ione);
+            blasf77_dsymm("L", uplo, &N, &m1, &d_one, h_A, &N, h_work, &N, &d_neg_one, h_R, &N);
+            result[0] *= lapackf77_dlange("1", &N, &m1, h_R, &N, rwork)/N;
+          }
+          else if (itype == 3){
+            blasf77_dsymm("L", uplo, &N, &m1, &d_one, h_A, &N, h_R, &N, &d_zero, h_work, &N);
+            for(int i=0; i<m1; ++i)
+              blasf77_dscal(&N, &w1[i], &h_R[i*N], &ione);
+            blasf77_dsymm("L", uplo, &N, &m1, &d_one, h_B, &N, h_work, &N, &d_neg_one, h_R, &N);
+            result[0] *= lapackf77_dlange("1", &N, &m1, h_R, &N, rwork)/N;
+          }
+
+
+          lapackf77_dlacpy( MagmaUpperLowerStr, &N, &N, h_A, &N, h_R, &N );
+          lapackf77_dlacpy( MagmaUpperLowerStr, &N, &N, h_B, &N, h_S, &N );
+
+          magma_dsygvdx(itype, 'N', range, uplo[0],
+                        N, h_R, N, h_S, N, vl, vu, il, iu, &m2, w2,
+                        h_work, lwork,
+                        iwork, liwork,
+                        &info);
+
+          temp1 = temp2 = 0;
+          for(int j=0; j<m2; j++){
+            temp1 = max(temp1, absv(w1[j]));
+            temp1 = max(temp1, absv(w2[j]));
+            temp2 = max(temp2, absv(w1[j]-w2[j]));
+          }
+          result[1] = temp2 / temp1;
+        }
+
+
+        /* =====================================================================
+           Print execution time
+           =================================================================== */
+        printf("%5d %5d     %6.2f\n",
+               (int) N, (int) m1, gpu_time);
+        if ( checkres ){
+          printf("Testing the eigenvalues and eigenvectors for correctness:\n");
+          if(itype==1)
+             printf("(1)    | A Z - B Z D | / (|A| |Z| N) = %e\n", result[0]);
+          else if(itype==2)
+             printf("(1)    | A B Z - Z D | / (|A| |Z| N) = %e\n", result[0]);
+          else if(itype==3)
+             printf("(1)    | B A Z - Z D | / (|A| |Z| N) = %e\n", result[0]);
+
+          printf("(2)    | D(w/ Z)-D(w/o Z)|/ |D| = %e\n\n", result[1]);
+        }
+
+        if (flagN)
+            break;
+    }
+
+    cudaSetDevice(0);
+    /* Memory clean up */
+    TESTING_FREE(       h_A);
+    TESTING_FREE(       h_B);
+    TESTING_FREE(        w1);
+    TESTING_FREE(        w2);
+    TESTING_FREE(     iwork);
+    TESTING_HOSTFREE(h_work);
+    TESTING_HOSTFREE(   h_R);
+    TESTING_HOSTFREE(   h_S);
+
+    /* Shutdown */
+#ifdef USE_MGPU
+    TESTING_CUDA_FINALIZE_MGPU();
+#else
+     TESTING_CUDA_FINALIZE();
+#endif
+}
