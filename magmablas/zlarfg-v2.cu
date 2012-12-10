db47502c2cf9daf7fa316150248b7b706efdd7e6
Stanimire Tomov <tomov@icl.utk.edu>
2012-12-10 20:56:43 +0000

    1st attempt to fix a bug
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2628 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- magmablas/zlarfg-v2.cu
+++ magmablas/zlarfg-v2.cu
@@ -54,6 +54,7 @@ void magma_zlarfg_gpu_kernel( int n, cuDoubleComplex* dx0, cuDoubleComplex* dx,
  
             // todo: deal with badly scaled vectors (see lapack's larfg)
             *dtau = (beta - alpha) / beta;
+            if (blockIdx.x == gridDim.x-1)
             *dx0  = beta;
 
             scale = 1. / (alpha - beta);
@@ -67,6 +68,7 @@ void magma_zlarfg_gpu_kernel( int n, cuDoubleComplex* dx0, cuDoubleComplex* dx,
 
             // todo: deal with badly scaled vectors (see lapack's larfg)
             *dtau = MAGMA_Z_MAKE((beta - alphar)/beta, -alphai/beta);
+            if (blockIdx.x == gridDim.x-1)
             *dx0  = MAGMA_Z_MAKE(beta, 0.);
             
             alpha = MAGMA_Z_MAKE( MAGMA_Z_REAL(alpha) - beta, MAGMA_Z_IMAG(alpha));
