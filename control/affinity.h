48121749bc555e3a82275f16750c902513b6d605
Raffaele Solca <rasolca@itp.phys.ethz.ch>
2013-01-28 18:43:24 +0000

    added affinity interface
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2681 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ control/affinity.h
@@ -0,0 +1,45 @@
+/*
+ -- MAGMA (version 1.1) --
+ Univ. of Tennessee, Knoxville
+ Univ. of California, Berkeley
+ Univ. of Colorado, Denver
+ November 2011
+
+ @author Raffaele Solca
+
+ */
+#include "common_magma.h"
+
+#ifndef _MAGMA_AFFINITY
+#define _MAGMA_AFFINITY
+
+#ifdef SETAFFINITY
+
+class affinity_set
+{
+
+public:
+
+    affinity_set();
+
+    affinity_set(int cpu_nr);
+
+    void add(int cpu_nr);
+
+    int get_affinity();
+
+    int set_affinity();
+
+    void print_affinity(int id, const char* s);
+
+    void print_set(int id, const char* s);
+
+private:
+
+    cpu_set_t set;
+};
+
+#endif
+
+#endif
+
