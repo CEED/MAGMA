dde810987753fc1d38eae7ba7490033a0660c872
Mark Gates <mgates3@icl.utk.edu>
2013-04-12 23:43:11 +0000

    replace cu(Float|Double) with magma$1 (in interface)
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2774 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- interface_cuda/zset_get.cpp
+++ interface_cuda/zset_get.cpp
@@ -22,13 +22,13 @@
 extern "C"
 void magma_zsetvector_internal(
     magma_int_t n,
-    cuDoubleComplex const* hx_src, magma_int_t incx,
-    cuDoubleComplex*       dy_dst, magma_int_t incy,
+    magmaDoubleComplex const* hx_src, magma_int_t incx,
+    magmaDoubleComplex*       dy_dst, magma_int_t incy,
     const char* func, const char* file, int line )
 {
     cublasStatus_t status;
     status = cublasSetVector(
-        n, sizeof(cuDoubleComplex),
+        n, sizeof(magmaDoubleComplex),
         hx_src, incx,
         dy_dst, incy );
     check_xerror( status, func, file, line );
@@ -38,13 +38,13 @@ void magma_zsetvector_internal(
 extern "C"
 void magma_zgetvector_internal(
     magma_int_t n,
-    cuDoubleComplex const* dx_src, magma_int_t incx,
-    cuDoubleComplex*       hy_dst, magma_int_t incy,
+    magmaDoubleComplex const* dx_src, magma_int_t incx,
+    magmaDoubleComplex*       hy_dst, magma_int_t incy,
     const char* func, const char* file, int line )
 {
     cublasStatus_t status;
     status = cublasGetVector(
-        n, sizeof(cuDoubleComplex),
+        n, sizeof(magmaDoubleComplex),
         dx_src, incx,
         hy_dst, incy );
     check_xerror( status, func, file, line );
@@ -54,14 +54,14 @@ void magma_zgetvector_internal(
 extern "C"
 void magma_zsetvector_async_internal(
     magma_int_t n,
-    cuDoubleComplex const* hx_src, magma_int_t incx,
-    cuDoubleComplex*       dy_dst, magma_int_t incy,
+    magmaDoubleComplex const* hx_src, magma_int_t incx,
+    magmaDoubleComplex*       dy_dst, magma_int_t incy,
     cudaStream_t stream,
     const char* func, const char* file, int line )
 {
     cublasStatus_t status;
     status = cublasSetVectorAsync(
-        n, sizeof(cuDoubleComplex),
+        n, sizeof(magmaDoubleComplex),
         hx_src, incx,
         dy_dst, incy, stream );
     check_xerror( status, func, file, line );
@@ -71,14 +71,14 @@ void magma_zsetvector_async_internal(
 extern "C"
 void magma_zgetvector_async_internal(
     magma_int_t n,
-    cuDoubleComplex const* dx_src, magma_int_t incx,
-    cuDoubleComplex*       hy_dst, magma_int_t incy,
+    magmaDoubleComplex const* dx_src, magma_int_t incx,
+    magmaDoubleComplex*       hy_dst, magma_int_t incy,
     cudaStream_t stream,
     const char* func, const char* file, int line )
 {
     cublasStatus_t status;
     status = cublasGetVectorAsync(
-        n, sizeof(cuDoubleComplex),
+        n, sizeof(magmaDoubleComplex),
         dx_src, incx,
         hy_dst, incy, stream );
     check_xerror( status, func, file, line );
@@ -90,13 +90,13 @@ void magma_zgetvector_async_internal(
 extern "C"
 void magma_zsetmatrix_internal(
     magma_int_t m, magma_int_t n,
-    cuDoubleComplex const* hA_src, magma_int_t lda,
-    cuDoubleComplex*       dB_dst, magma_int_t ldb,
+    magmaDoubleComplex const* hA_src, magma_int_t lda,
+    magmaDoubleComplex*       dB_dst, magma_int_t ldb,
     const char* func, const char* file, int line )
 {
     cublasStatus_t status;
     status = cublasSetMatrix(
-        m, n, sizeof(cuDoubleComplex),
+        m, n, sizeof(magmaDoubleComplex),
         hA_src, lda,
         dB_dst, ldb );
     check_xerror( status, func, file, line );
@@ -106,13 +106,13 @@ void magma_zsetmatrix_internal(
 extern "C"
 void magma_zgetmatrix_internal(
     magma_int_t m, magma_int_t n,
-    cuDoubleComplex const* dA_src, magma_int_t lda,
-    cuDoubleComplex*       hB_dst, magma_int_t ldb,
+    magmaDoubleComplex const* dA_src, magma_int_t lda,
+    magmaDoubleComplex*       hB_dst, magma_int_t ldb,
     const char* func, const char* file, int line )
 {
     cublasStatus_t status;
     status = cublasGetMatrix(
-        m, n, sizeof(cuDoubleComplex),
+        m, n, sizeof(magmaDoubleComplex),
         dA_src, lda,
         hB_dst, ldb );
     check_xerror( status, func, file, line );
@@ -122,14 +122,14 @@ void magma_zgetmatrix_internal(
 extern "C"
 void magma_zsetmatrix_async_internal(
     magma_int_t m, magma_int_t n,
-    cuDoubleComplex const* hA_src, magma_int_t lda,
-    cuDoubleComplex*       dB_dst, magma_int_t ldb,
+    magmaDoubleComplex const* hA_src, magma_int_t lda,
+    magmaDoubleComplex*       dB_dst, magma_int_t ldb,
     cudaStream_t stream,
     const char* func, const char* file, int line )
 {
     cublasStatus_t status;
     status = cublasSetMatrixAsync(
-        m, n, sizeof(cuDoubleComplex),
+        m, n, sizeof(magmaDoubleComplex),
         hA_src, lda,
         dB_dst, ldb, stream );
     check_xerror( status, func, file, line );
@@ -139,14 +139,14 @@ void magma_zsetmatrix_async_internal(
 extern "C"
 void magma_zgetmatrix_async_internal(
     magma_int_t m, magma_int_t n,
-    cuDoubleComplex const* dA_src, magma_int_t lda,
-    cuDoubleComplex*       hB_dst, magma_int_t ldb,
+    magmaDoubleComplex const* dA_src, magma_int_t lda,
+    magmaDoubleComplex*       hB_dst, magma_int_t ldb,
     cudaStream_t stream,
     const char* func, const char* file, int line )
 {
     cublasStatus_t status;
     status = cublasGetMatrixAsync(
-        m, n, sizeof(cuDoubleComplex),
+        m, n, sizeof(magmaDoubleComplex),
         dA_src, lda,
         hB_dst, ldb, stream );
     check_xerror( status, func, file, line );
@@ -156,15 +156,15 @@ void magma_zgetmatrix_async_internal(
 extern "C"
 void magma_zcopymatrix_internal(
     magma_int_t m, magma_int_t n,
-    cuDoubleComplex const* dA_src, magma_int_t lda,
-    cuDoubleComplex*       dB_dst, magma_int_t ldb,
+    magmaDoubleComplex const* dA_src, magma_int_t lda,
+    magmaDoubleComplex*       dB_dst, magma_int_t ldb,
     const char* func, const char* file, int line )
 {
     cudaError_t status;
     status = cudaMemcpy2D(
-        dB_dst, ldb*sizeof(cuDoubleComplex),
-        dA_src, lda*sizeof(cuDoubleComplex),
-        m*sizeof(cuDoubleComplex), n, cudaMemcpyDeviceToDevice );
+        dB_dst, ldb*sizeof(magmaDoubleComplex),
+        dA_src, lda*sizeof(magmaDoubleComplex),
+        m*sizeof(magmaDoubleComplex), n, cudaMemcpyDeviceToDevice );
     check_xerror( status, func, file, line );
 }
 
@@ -172,16 +172,16 @@ void magma_zcopymatrix_internal(
 extern "C"
 void magma_zcopymatrix_async_internal(
     magma_int_t m, magma_int_t n,
-    cuDoubleComplex const* dA_src, magma_int_t lda,
-    cuDoubleComplex*       dB_dst, magma_int_t ldb,
+    magmaDoubleComplex const* dA_src, magma_int_t lda,
+    magmaDoubleComplex*       dB_dst, magma_int_t ldb,
     cudaStream_t stream,
     const char* func, const char* file, int line )
 {
     cudaError_t status;
     status = cudaMemcpy2DAsync(
-        dB_dst, ldb*sizeof(cuDoubleComplex),
-        dA_src, lda*sizeof(cuDoubleComplex),
-        m*sizeof(cuDoubleComplex), n, cudaMemcpyDeviceToDevice, stream );
+        dB_dst, ldb*sizeof(magmaDoubleComplex),
+        dA_src, lda*sizeof(magmaDoubleComplex),
+        m*sizeof(magmaDoubleComplex), n, cudaMemcpyDeviceToDevice, stream );
     check_xerror( status, func, file, line );
 }
 
