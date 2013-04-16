b4062d09f257f1dd9782ee8c12ea661f908e9719
Mark Gates <mgates3@icl.utk.edu>
2013-04-16 22:27:06 +0000

    Fix alpha and beta. Check arguments. Update docs and tester.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2798 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- magmablas/zher2k_mgpu.cpp
+++ magmablas/zher2k_mgpu.cpp
@@ -29,7 +29,7 @@
     Arguments
     ==========
 
-    UPLO   - (input) CHARACTER*1.
+    UPLO     (input) CHARACTER*1.
              On entry, UPLO specifies whether the upper or lower
              triangular part of the array C is to be referenced as
              follows:
@@ -40,7 +40,9 @@
                 UPLO = 'L' or 'l'   Only the lower triangular part of C
                                     is to be referenced.
 
-    TRANS  - (input) CHARACTER*1.
+             **** current only Lower case is implemented.
+
+    TRANS    (input) CHARACTER*1.
              On entry, TRANS specifies the operation to be performed as
              follows:
 
@@ -50,20 +52,22 @@
                 TRANS = 'C' or 'c'
                   C := alpha*A**H*B + conjg( alpha )*B**H*A + beta*C.
 
-    N      - (input) INTEGER.
+             **** current only NoTrans case is implemented.
+
+    N        (input) INTEGER.
              On entry, N specifies the order of the matrix C. N must be
              at least zero.
 
-    K      - (input) INTEGER.
+    K        (input) INTEGER.
              On entry with TRANS = 'N' or 'n', K specifies the number
              of columns of the matrices A and B, and on entry with
              TRANS = 'C' or 'c', K specifies the number of rows of the
              matrices A and B. K must be at least zero.
 
-    ALPHA  - (input) COMPLEX*16.
+    ALPHA    (input) COMPLEX*16.
              On entry, ALPHA specifies the scalar alpha.
 
-    dA     - (input) COMPLEX*16 array of DIMENSION ( LDA, ka ), where ka is
+    dA       (input) COMPLEX*16 array of DIMENSION ( LDA, ka ), where ka is
              k when TRANS = 'N' or 'n', and is n otherwise.
              Before entry with TRANS = 'N' or 'n', the leading n by k
              part of the array A must contain the matrix A, otherwise
@@ -72,13 +76,17 @@
              
              [TODO: describe distribution: duplicated on all GPUs.]
 
-    LDA    - (input) INTEGER.
+    LDA      (input) INTEGER.
              On entry, LDA specifies the first dimension of A as declared
              in the calling (sub) program. When TRANS = 'N' or 'n'
              then LDA must be at least max( 1, n ), otherwise LDA must
              be at least max( 1, k ).
 
-    dB     - (input) COMPLEX*16 array of DIMENSION ( LDB, kb ), where kb is
+    AOFFSET  (input) INTEGER
+             Row offset to start sub-matrix of dA. Uses dA(aoffset:aoffset+n, :).
+             0 <= aoffset < lda.
+             
+    dB       (input) COMPLEX*16 array of DIMENSION ( LDB, kb ), where kb is
              k when TRANS = 'N' or 'n', and is n otherwise.
              Before entry with TRANS = 'N' or 'n', the leading n by k
              part of the array B must contain the matrix B, otherwise
@@ -87,16 +95,20 @@
              
              [TODO: describe distribution: duplicated on all GPUs.]
 
-    LDB    - (input) INTEGER.
+    LDB      (input) INTEGER.
              On entry, LDB specifies the first dimension of B as declared
              in the calling (sub) program. When TRANS = 'N' or 'n'
              then LDB must be at least max( 1, n ), otherwise LDB must
              be at least max( 1, k ).
 
-    BETA   - (input) DOUBLE PRECISION.
+    BOFFSET  (input) INTEGER
+             Row offset to start sub-matrix of dB. Uses dB(boffset:boffset+n, :).
+             0 <= boffset < ldb.
+             
+    BETA     (input) DOUBLE PRECISION.
              On entry, BETA specifies the scalar beta.
 
-    dC     - (input/output) COMPLEX*16 array of DIMENSION ( LDC, n ).
+    dC       (input/output) COMPLEX*16 array of DIMENSION ( LDC, n ).
              Before entry with UPLO = 'U' or 'u', the leading n by n
              upper triangular part of the array C must contain the upper
              triangular part of the Hermitian matrix and the strictly
@@ -117,27 +129,26 @@
              
              [TODO: describe distribution: 1D column block-cyclic across GPUs.]
 
-    LDC    - (input) INTEGER.
+    LDC      (input) INTEGER.
              On entry, LDC specifies the first dimension of C as declared
-             in the calling (sub) program. LDC must be at least
-             max( 1, n ).
+             in the calling (sub) program. LDC must be at least max( 1, n ).
 
-    OFFSET - (input) INTEGER.
-             Row and column to start sub-matrix of dC.
-             For example, if offset = 10, then sub-matrix starts at dC(10, 10).
-             Offset can be any number, 0 <= offset < ldc.
+    COFFSET  (input) INTEGER.
+             Row and column offset to start sub-matrix of dC.
+             Uses dC(coffset:coffset+n, coffset:coffset+n).
+             0 <= coffset < ldc.
 
-    NGPU   - (input) INTEGER.
+    NGPU     (input) INTEGER.
              Number of GPUs over which matrix C is distributed.
 
-    NB     - (input) INTEGER.
+    NB       (input) INTEGER.
              Block size used for distribution of C.
 
-    STREAMS- (input) array of CUDA streams, of dimension NGPU by 20.
+    STREAMS  (input) array of CUDA streams, of dimension NGPU by 20.
              Streams to use for running multiple GEMMs in parallel.
              Only up to NSTREAM streams are used on each GPU.
 
-    NSTREAM- (input) INTEGER.
+    NSTREAM  (input) INTEGER.
              Number of streams to use on each device
              
 */
@@ -145,15 +156,52 @@
 extern "C"
 void magmablas_zher2k_mgpu2(
     char uplo, char trans, magma_int_t n, magma_int_t k,
-    cuDoubleComplex alpha, cuDoubleComplex *dA[], magma_int_t lda, magma_int_t aoff,
-                           cuDoubleComplex *dB[], magma_int_t ldb, magma_int_t boff,
-    double beta,           cuDoubleComplex *dC[], magma_int_t ldc,  magma_int_t offset,
+    cuDoubleComplex alpha, cuDoubleComplex *dA[], magma_int_t lda, magma_int_t aoffset,
+                           cuDoubleComplex *dB[], magma_int_t ldb, magma_int_t boffset,
+    double beta,           cuDoubleComplex *dC[], magma_int_t ldc, magma_int_t coffset,
     magma_int_t ngpu, magma_int_t nb, cudaStream_t streams[][20], magma_int_t nstream )
 {
-    #define dA(dev, i, j) (dA[dev] + (i) + (j)*lda + (aoff) )
-    #define dB(dev, i, j) (dB[dev] + (i) + (j)*ldb + (boff) )
+    #define dA(dev, i, j) (dA[dev] + (i) + (j)*lda + (aoffset) )
+    #define dB(dev, i, j) (dB[dev] + (i) + (j)*ldb + (boffset) )
     #define dC(dev, i, j) (dC[dev] + (i) + (j)*ldc)
     
+    /* Check arguments */
+    magma_int_t info = 0;
+    if ( ! (uplo == 'l' || uplo == 'L')) {
+        info = -1;  // 'u' not yet handled
+    } else if ( ! (trans == 'n' || trans == 'N')) {
+        info = -2;  // 'c' not yet handled
+    } else if ( n < 0 ) {
+        info = -3;
+    } else if ( k < 0 ) {
+        info = -4;
+    } else if ( ((trans == 'n' || trans == 'N') && lda < max(1,n)) ||
+                ((trans == 'c' || trans == 'C') && lda < max(1,k)) ) {
+        info = -7;
+    } else if ( aoffset < 0 || aoffset > lda ) {
+        info = -8;
+    } else if ( ((trans == 'n' || trans == 'N') && ldb < max(1,n)) ||
+                ((trans == 'c' || trans == 'C') && ldb < max(1,k)) ) {
+        info = -10;
+    } else if ( boffset < 0 || boffset > ldb ) {
+        info = -11;
+    } else if ( ldc < max(1,n) ) {
+        info = -13;
+    } else if ( coffset < 0 || coffset > ldc ) {
+        info = -14;
+    } else if ( ngpu <= 0 ) {
+        info = -15;
+    } else if ( nb <= 0 ) {
+        info = -16;
+    } else if ( nstream <= 0 ) {
+        info = -18;
+    }
+    if ( info != 0 ) {
+        magma_xerbla( __func__, -(info) );
+        return;
+    }
+    
+    const cuDoubleComplex c_one = MAGMA_Z_ONE;
     cuDoubleComplex cbeta = MAGMA_Z_MAKE( beta, 0. );
     
     magma_int_t ib, ioff, iblock, idev, di, s;
@@ -164,12 +212,12 @@ void magmablas_zher2k_mgpu2(
     magmablasGetKernelStream( &cqueue );
     
     // loop over all blocks
-    // Faster to have two loops: first does A*B', second does B*A'
+    // Faster to have two loops: first loop does C_hat = alpha*A*B' + beta*C
     // blockoffset is offset within first block; for subsequent blocks it is 0
-    magma_int_t blockoffset = offset % nb;
+    magma_int_t blockoffset = coffset % nb;
     for( magma_int_t i = 0; i < n; i += ib ) {
         ib     = min( nb-blockoffset, n-i );  // block size
-        ioff   = i + offset;                  // global index in parent matrix
+        ioff   = i + coffset;                 // global index in parent matrix
         iblock = (ioff / nb) / ngpu;          // local block id
         idev   = (ioff / nb) % ngpu;          // device with this block
         di     = iblock*nb + blockoffset;     // local index in parent matrix
@@ -178,7 +226,7 @@ void magmablas_zher2k_mgpu2(
         s = iblock % nstream;
         magmablasSetKernelStream( streams[ idev ][ s ] );
         
-        // C[i:n,i] += A[i:n,0] * B[i,0]'
+        // C[i:n,i] = alpha * A[i:n,0] * B[i,0]' + beta*C[i:n,i]
         //printf( "zgemm  n=%4d, ib=%4d, k=%4d, i=%4d\n", n-i, ib, k, i );
         magma_zgemm( MagmaNoTrans, MagmaConjTrans, n-i, ib, k,
                      alpha, dA(idev,i,0), lda,
@@ -186,10 +234,13 @@ void magmablas_zher2k_mgpu2(
                      cbeta, dC(idev,ioff,di), ldc );
         blockoffset = 0;
     }
-    blockoffset = offset % nb;
+    
+    // second loop does C = conj(alpha)*B*A' + C_hat
+    alpha = MAGMA_Z_CNJG( alpha );
+    blockoffset = coffset % nb;
     for( magma_int_t i = 0; i < n; i += ib ) {
         ib     = min( nb-blockoffset, n-i );  // block size
-        ioff   = i + offset;                  // global index in parent matrix
+        ioff   = i + coffset;                 // global index in parent matrix
         iblock = (ioff / nb) / ngpu;          // local block id
         idev   = (ioff / nb) % ngpu;          // device with this block
         di     = iblock*nb + blockoffset;     // local index in parent matrix
@@ -198,12 +249,12 @@ void magmablas_zher2k_mgpu2(
         s = iblock % nstream;
         magmablasSetKernelStream( streams[ idev ][ s ] );
         
-        // C[i:n,i] += B[i:n,0] * A[i,0]'
+        // C[i:n,i] += conj(alpha) * B[i:n,0] * A[i,0]'
         //printf( "zgemm  n=%4d, ib=%4d, k=%4d, i=%4d\n", n-i, ib, k, i );
         magma_zgemm( MagmaNoTrans, MagmaConjTrans, n-i, ib, k,
                      alpha, dB(idev,i,0), ldb,
                             dA(idev,i,0), lda,
-                     cbeta, dC(idev,ioff,di), ldc );
+                     c_one, dC(idev,ioff,di), ldc );
         blockoffset = 0;
     }
     
--- testing/testing_zher2k_mgpu.cpp
+++ testing/testing_zher2k_mgpu.cpp
@@ -25,14 +25,17 @@
 #include "magma_lapack.h"
 #include "testings.h"
 
+#undef ICHI
+#ifdef ICHI
 // version from zhetrd_mgpu.cpp (i.e., Ichi's code)
 // FIX DIMENSION of streams there before changing this -- otherwise it will segfault.
 extern "C" void
 magma_zher2k_mgpu(
     int num_gpus, char uplo, char trans, int nb, int n, int k,
-    cuDoubleComplex alpha, cuDoubleComplex **db, int lddb, 
+    cuDoubleComplex alpha, cuDoubleComplex **db, int lddb,
     double beta,           cuDoubleComplex **dc, int lddc, int offset,
     int num_streams, cudaStream_t streams[][10]);
+#endif
 
 
 /* ////////////////////////////////////////////////////////////////////////////
@@ -43,124 +46,68 @@ int main( int argc, char** argv)
     TESTING_CUDA_INIT();
 
     cuDoubleComplex c_neg_one = MAGMA_Z_NEG_ONE;
-    double d_one = 1.0;
+    cuDoubleComplex alpha = MAGMA_Z_MAKE( 1.2345, 4.321 );
+    double beta = 3.14159;
     
-    real_Double_t    gflops, gpu_perf=0., cpu_perf=0., gpu_time=0., cpu_time=0.;
-    real_Double_t    gpu_perf2=0., gpu_time2=0.;
-    double           error=0., error2=0., work[1];
+    real_Double_t    gflops, gpu_perf, cpu_perf, gpu_time, cpu_time;
+    double           error, work[1];
     cuDoubleComplex *hA, *hR, *hR2, *hV, *hW;
     cuDoubleComplex *dV[MagmaMaxGPUs], *dW[MagmaMaxGPUs], *dA[MagmaMaxGPUs];
-
-    /* Matrix size */
-    magma_int_t n, size, lda, ldda;
-    const int MAXTESTS = 10;
-    // sizes are 1024*N - 32
-    magma_int_t nsize[MAXTESTS] = { 1024, 2048, 3072, 4032, 5184, 6016, 7040, 8064, 9088, 10112 };
-    magma_int_t k       = 64;
-    magma_int_t nb      = 64;
-    magma_int_t nstream = 2;
-    magma_int_t count   = 1;
-    magma_int_t ngpu    = magma_num_gpus();
-    
+    magma_int_t n, k, size, lda, ldda, nb, ngpu, nstream;
     magma_int_t ione     = 1;
-    magma_int_t iseed[4] = {0,0,0,1};
-        
-    printf( "Usage: %s -N n -K k -nb nb -nstream nstream -ngpu ngpu -count count -c\n"
-            "    -N can be repeated %d times.\n"
-            "    -ngpu or setting $MAGMA_NUM_GPUS sets number of GPUs to use.\n"
-            "    -c or setting $MAGMA_TESTINGS_CHECK runs LAPACK and checks result.\n\n",
-            argv[0], MAXTESTS );
+    magma_int_t ISEED[4] = {0,0,0,1};
 
-    int checkres = (getenv("MAGMA_TESTINGS_CHECK") != NULL);
-
-    int ntest = 0;
-    int nmax = 0;
-    for( int i = 1; i < argc; i++ ) {
-        if ( strcmp("-N", argv[i]) == 0 && i+1 < argc ) {
-            magma_assert( ntest < MAXTESTS, "error: -N repeated more than maximum %d tests\n", MAXTESTS );
-            nsize[ntest] = atoi( argv[++i] );
-            magma_assert( nsize[ntest] > 0, "error: -N %s is invalid; must be > 0.\n", argv[i] );
-            nmax = max( nmax, nsize[ntest] );
-            ntest++;
-        }
-        else if ( strcmp("-K", argv[i]) == 0 && i+1 < argc ) {
-            k = atoi( argv[++i] );
-            magma_assert( k > 0, "error: -K %s is invalid; must be > 0.\n", argv[i] );
-        }
-        else if ( strcmp("-NB", argv[i]) == 0 && i+1 < argc ) {
-            nb = atoi( argv[++i] );
-            magma_assert( nb > 0, "error: -NB %s is invalid; must be > 0.\n", argv[i] );
-        }
-        else if ( strcmp("-count", argv[i]) == 0 && i+1 < argc ) {
-            count = atoi( argv[++i] );
-            magma_assert( count > 0, "error: -count %s is invalid; must be > 0.\n", argv[i] );
-        }
-        else if ( strcmp("-nstream", argv[i]) == 0 && i+1 < argc ) {
-            nstream = atoi( argv[++i] );
-            magma_assert( nstream > 0 && nstream <= 10,
-                    "error: -nstream %s is invalid; must be > 0 and <= 10.\n", argv[i] );
-        }
-        else if ( strcmp("-ngpu", argv[i]) == 0 && i+1 < argc ) {
-            ngpu = atoi( argv[++i] );
-            magma_assert( ngpu > 0, "error: -ngpu %s is invalid; must be > 0.\n", argv[i] );
-        }
-        else if ( strcmp("-c", argv[i]) == 0 ) {
-            checkres = true;
-        }
-        else {
-            printf( "invalid argument: %s\n", argv[i] );
-            exit(1);
-        }
-    }
-    if ( ntest == 0 ) {
-        ntest = MAXTESTS;
-        nmax = nsize[ntest-1];
-    }
-    assert( nmax > 0 );
-    
-    // allocate memory for largest problem
-    n = nmax;
-    lda    = n;
-    ldda   = ((n + 31)/32)*32;
-
-    TESTING_MALLOC( hA,  cuDoubleComplex, lda*n );
-    TESTING_MALLOC( hR,  cuDoubleComplex, lda*n );
-    TESTING_MALLOC( hR2, cuDoubleComplex, lda*n );
-    TESTING_MALLOC( hV,  cuDoubleComplex, lda*k*2 );
-    //TESTING_MALLOC( hW,  cuDoubleComplex, lda*k );
-    
     cudaStream_t streams[MagmaMaxGPUs][20];
     
-    for( int d = 0; d < ngpu; ++d ) {
-        magma_int_t nlocal = ((n / nb) / ngpu + 1) * nb;
-        cudaSetDevice( d );
-        TESTING_DEVALLOC( dA[d], cuDoubleComplex, ldda*nlocal );
-        TESTING_DEVALLOC( dV[d], cuDoubleComplex, ldda*k*2      );
-        //TESTING_DEVALLOC( dW[d], cuDoubleComplex, ldda*k      );
-        for( int i = 0; i < nstream; ++i ) {
-            cudaStreamCreate( &streams[d][i] );
-        }
-    }
+    magma_opts opts;
+    parse_opts( argc, argv, &opts );
     
-    printf( "k %d, nb %d, ngpu %d, nstream %d\n", (int) k, (int) nb, (int) ngpu, (int) nstream );
-    printf("    n     k    nb offset  CPU GFlop/s (sec)   GPU GFlop/s (sec)   Ichi's code (sec)  |R|/|A|   Ichi |R|/|A|\n");
-    printf("==========================================================================================\n");
-    for( int i = 0; i < ntest; ++i ) {
-        for( int offset = 0; offset < n; offset += min(k,nb) ) {
-            for( int cnt = 0; cnt < count; ++cnt ) {
-                n = nsize[i];
-                assert( n > 0 && k > 0 );
-                
-                lda  = n;
-                ldda = ((n + 31)/32)*32;
-                gflops = FLOPS_ZHER2K( (double)k, (double)n-offset ) / 1e9;
+    ngpu    = opts.ngpu;
+    nb      = (opts.nb      > 0 ? opts.nb      : 64);
+    nstream = (opts.nstream > 0 ? opts.nstream :  2);
+    
+    printf( "version 1: magmablas_zher2k_mgpu2     %s\n", (opts.version==1 ? "(enabled)" : ""));
+    printf( "version 2: magmablas_zher2k_mgpu_spec %s\n", (opts.version==2 ? "(enabled)" : ""));
+#ifdef ICHI
+    printf( "version 3: magma_zher2k_mgpu (Ichi)   %s\n", (opts.version==3 ? "(enabled)" : ""));
+#endif
+    printf( "\n" );
+    
+    printf( "nb %d, ngpu %d, nstream %d\n", (int) nb, (int) ngpu, (int) nstream );
+    printf("    n     k    nb offset  CPU GFlop/s (sec)   GPU GFlop/s (sec)   |R|/(|V|*|W|+|A|)\n");
+    printf("===================================================================================\n");
+    for( int i = 0; i < opts.ntest; ++i ) {
+        n = opts.nsize[i];
+        k = opts.ksize[i];
         
+        for( int offset = 0; offset < n; offset += min(k,nb) ) {
+            for( int iter = 0; iter < opts.niter; ++iter ) {
+                lda    = n;
+                ldda   = ((n + 31)/32)*32;
+                gflops = FLOPS_ZHER2K( k, n-offset ) / 1e9;
+                
+                TESTING_MALLOC( hA,  cuDoubleComplex, lda*n );
+                TESTING_MALLOC( hR,  cuDoubleComplex, lda*n );
+                TESTING_MALLOC( hR2, cuDoubleComplex, lda*n );
+                TESTING_MALLOC( hV,  cuDoubleComplex, lda*k*2 );
+                //TESTING_MALLOC( hW,  cuDoubleComplex, lda*k );
+                for( int d = 0; d < ngpu; ++d ) {
+                    magma_int_t nlocal = ((n / nb) / ngpu + 1) * nb;
+                    cudaSetDevice( d );
+                    TESTING_DEVALLOC( dA[d], cuDoubleComplex, ldda*nlocal );
+                    TESTING_DEVALLOC( dV[d], cuDoubleComplex, ldda*k*2      );
+                    //TESTING_DEVALLOC( dW[d], cuDoubleComplex, ldda*k      );
+                    for( int i = 0; i < nstream; ++i ) {
+                        cudaStreamCreate( &streams[d][i] );
+                    }
+                }
+                
                 size = lda*n;
-                lapackf77_zlarnv( &ione, iseed, &size, hA );
+                lapackf77_zlarnv( &ione, ISEED, &size, hA );
                 size = lda*k*2;
-                lapackf77_zlarnv( &ione, iseed, &size, hV );
+                lapackf77_zlarnv( &ione, ISEED, &size, hV );
                 hW = hV + lda*k;
-                //lapackf77_zlarnv( &ione, iseed, &size, hW );
+                //lapackf77_zlarnv( &ione, ISEED, &size, hW );
                 
                 /* ====================================================================
                    Performs operation using MAGMA
@@ -175,130 +122,91 @@ int main( int argc, char** argv)
                 
                 cudaDeviceSynchronize();
                 gpu_time = magma_wtime();
-
-
- 
-               magmablas_zher2k_mgpu2(
-                    MagmaLower, MagmaNoTrans, n-offset, k,
-                    c_neg_one, dV, ldda,0,
-                               dW, ldda,0,
-                    d_one,     dA, ldda, offset,
-                    ngpu, nb, streams, nstream );
-/*
-                magmablas_zher2k_mgpu_spec(
-                    MagmaLower, MagmaNoTrans, n-offset, k,
-                    c_neg_one, dV, ldda,0,
-                               dW, ldda,0,
-                    d_one,     dA, ldda, offset,
-                    ngpu, nb, streams, nstream );
-*/
-
-               /*
-                for( int d = 0; d < ngpu; ++d ) {
-                    cudaSetDevice( d );
-                    cudaDeviceSynchronize();
+                
+                if ( opts.version == 1 ) {
+                    magmablas_zher2k_mgpu2(
+                        MagmaLower, MagmaNoTrans, n-offset, k,
+                        alpha, dV, ldda, 0,
+                               dW, ldda, 0,
+                        beta,  dA, ldda, offset,
+                        ngpu, nb, streams, nstream );
                 }
-                cudaSetDevice( 0 );
-                */
-
-                    cudaDeviceSynchronize();
-
+                else if ( opts.version == 2 ) {
+                    magmablas_zher2k_mgpu_spec(
+                        MagmaLower, MagmaNoTrans, n-offset, k,
+                        alpha, dV, ldda, 0,
+                               dW, ldda, 0,
+                        beta,  dA, ldda, offset,
+                        ngpu, nb, streams, nstream );
+                }
+                else {
+#ifdef ICHI
+                    magma_zher2k_mgpu(
+                        ngpu, MagmaLower, MagmaNoTrans, nb, n-offset, k,
+                        alpha, dV, ldda,
+                               //dW, ldda,
+                        beta,  dA, ldda, offset,
+                        nstream, streams );
+#endif
+                }
+                
+                cudaDeviceSynchronize();
                 gpu_time = magma_wtime() - gpu_time;
                 gpu_perf = gflops / gpu_time;
                 
                 // Get dA back to the CPU to compare with the CPU result.
                 magmablas_zgetmatrix_1D_bcyclic( n, n, dA, ldda, hR, lda, ngpu, nb );
                 
-                /* ====================================================================
-                   Performs operation using MAGMA (Ichi's code)
-                   =================================================================== */
-#if 0
-                magmablas_zsetmatrix_1D_bcyclic( n, n, hA, lda, dA, ldda, ngpu, nb );
-                for( int d = 0; d < ngpu; ++d ) {
-                    cudaSetDevice( d );
-                    magma_zsetmatrix( n, k, hV, lda, dV[d], ldda );
-                    magma_zsetmatrix( n, k, hW, lda, dW[d], ldda );
-                }
-                
-                cudaDeviceSynchronize();
-                gpu_time2 = magma_wtime();
-                magma_zher2k_mgpu(
-                    ngpu, MagmaLower, MagmaNoTrans, nb, n-offset, k,
-                    c_neg_one, dV, ldda,
-                             //dW, ldda,
-                    d_one,     dA, ldda, offset,
-                    nstream, streams );
-                cudaDeviceSynchronize();
-                gpu_time2 = magma_wtime() - gpu_time2;
-                gpu_perf2 = gflops / gpu_time2;
-                
-                // Get dA back to the CPU to compare with the CPU result.
-                magmablas_zgetmatrix_1D_bcyclic( n, n, dA, ldda, hR2, lda, ngpu, nb );
-#endif
-        
                 /* =====================================================================
                    Performs operation using LAPACK
                    =================================================================== */
-                if ( checkres ) {
-                    // store ||A||
-                    error = lapackf77_zlange("f", &n, &n, hA, &lda, work );
-                    
-                    //printf( "A=" ); magma_zprint( n, n, hA, lda );
-                    //printf( "V=" ); magma_zprint( n, k, hV, lda );
-                    //printf( "W=" ); magma_zprint( n, k, hW, lda );
+                if ( opts.lapack || opts.check ) {
+                    // store ||V||*||W|| + ||A||
+                    int n_offset = n - offset;
+                    error  = lapackf77_zlange("f", &n_offset, &k, hV, &lda, work );
+                    error *= lapackf77_zlange("f", &n_offset, &k, hW, &lda, work );
+                    error += lapackf77_zlange("f", &n_offset, &n_offset, &hA[offset + offset*lda], &lda, work );
                     
                     cpu_time = magma_wtime();
-                    n -= offset;
-                    blasf77_zher2k( "Lower", "NoTrans", &n, &k,
-                                    &c_neg_one, hV, &lda,
-                                                hW, &lda,
-                                    &d_one,     &hA[ offset + offset*lda ], &lda );
-                    n += offset;
+                    blasf77_zher2k( "Lower", "NoTrans", &n_offset, &k,
+                                    &alpha, hV, &lda,
+                                            hW, &lda,
+                                    &beta,  &hA[offset + offset*lda], &lda );
                     cpu_time = magma_wtime() - cpu_time;
                     cpu_perf = gflops / cpu_time;
                     
-                    //printf( "Ahat ="   );  magma_zprint( n, n, hA,  lda );
-                    //printf( "dAhat ="  );  magma_zprint( n, n, hR,  lda );
-                    //printf( "dAhat2 =" );  magma_zprint( n, n, hR2, lda );
-            
                     // compute relative error ||R||/||A||, where R := A_magma - A_lapack = R - A
                     size = lda*n;
-#if 0
-                    blasf77_zaxpy( &size, &c_neg_one, hA, &ione, hR2, &ione );
-                    error2 = lapackf77_zlanhe("fro", "Lower", &n, hR2, &lda, work) / error;
-#endif
-                    
                     blasf77_zaxpy( &size, &c_neg_one, hA, &ione, hR, &ione );
-                    error = lapackf77_zlanhe("fro", "Lower", &n, hR, &lda, work) / error;
+                    error = lapackf77_zlanhe("fro", "Lower", &n_offset, &hR[offset + offset*lda], &lda, work) / error;
                     
-                    printf( "%5d %5d %5d %5d   %7.1f (%7.4f)   %7.1f (%7.4f)   %7.1f (%7.4f)   %8.2e   %8.2e\n",
+                    printf( "%5d %5d %5d %5d   %7.1f (%7.4f)   %7.1f (%7.4f)   %8.2e\n",
                             (int) n, (int) k, (int) nb, (int) offset,
-                            cpu_perf, cpu_time, gpu_perf, gpu_time, gpu_perf2, gpu_time2, error, error2 );
+                            cpu_perf, cpu_time, gpu_perf, gpu_time, error );
+                            //, gpu_perf2, gpu_time2, error, error2 );
                 }
                 else {
-                    printf( "%5d %5d %5d %5d     ---   (  ---  )   %7.1f (%7.4f)   %7.1f (%7.4f)     ---        ---\n",
+                    printf( "%5d %5d %5d %5d     ---   (  ---  )   %7.1f (%7.4f)     ---\n",
                             (int) n, (int) k, (int) nb, (int) offset,
-                            /*cpu_perf, cpu_time,*/ gpu_perf, gpu_time, gpu_perf2, gpu_time2 /*, error, error2*/ );
+                            gpu_perf, gpu_time );
                 }
-            } // count
+                
+                TESTING_FREE( hA );
+                TESTING_FREE( hR );
+                TESTING_FREE( hR2 );
+                TESTING_FREE( hV );
+                //TESTING_FREE( hW );
+                for( int d = 0; d < ngpu; ++d ) {
+                    cudaSetDevice( d );
+                    TESTING_DEVFREE( dA[d] );
+                    TESTING_DEVFREE( dV[d] );
+                    //TESTING_DEVFREE( dW[d] );
+                }
+            }
         } // offset
         printf( "\n" );
     }
     
-    /* Memory clean up */
-    for( int d = 0; d < ngpu; ++d ) {
-        cudaSetDevice( d );
-        TESTING_DEVFREE( dA[d] );
-        TESTING_DEVFREE( dV[d] );
-        //TESTING_DEVFREE( dW[d] );
-    }
-    
-    TESTING_FREE( hA );
-    TESTING_FREE( hR );
-    TESTING_FREE( hV );
-    //TESTING_FREE( hW );
-    
-    /* Shutdown */
     TESTING_CUDA_FINALIZE();
     return 0;
 }
