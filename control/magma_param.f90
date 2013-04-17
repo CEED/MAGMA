8d965c6f9db708b696a963fe1b45625cf305b7b9
Mark Gates <mgates3@icl.utk.edu>
2013-04-17 13:51:06 +0000

    whitespace
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2802 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- control/magma_param.f90
+++ control/magma_param.f90
@@ -8,12 +8,11 @@
 
 module magma_param
 
-  implicit none
+    implicit none
 
-  integer, parameter :: sizeof_complex_16 = 16
-  integer, parameter :: sizeof_complex    = 8
-  integer, parameter :: sizeof_double     = 8
-  integer, parameter :: sizeof_real       = 4
-  
+    integer, parameter :: sizeof_complex_16 = 16
+    integer, parameter :: sizeof_complex    = 8
+    integer, parameter :: sizeof_double     = 8
+    integer, parameter :: sizeof_real       = 4
+
 end module magma_param
-
