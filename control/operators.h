ed3d51456dc6b356fe43da358b87ddfc160044a6
Mark Gates <mgates3@icl.utk.edu>
2013-04-17 21:47:54 +0000

    Conform to C++ conventions (inline, operator return value). Replace cu(Float|Double) with magma$1.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2804 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- control/operators.h
+++ control/operators.h
@@ -9,142 +9,150 @@
  *  November 2011
  *
  **/
-#ifndef _MAGMA_OPERATORS_H_
-#define _MAGMA_OPERATORS_H_
+#ifndef MAGMA_OPERATORS_H
+#define MAGMA_OPERATORS_H
+
+// __host__ and __device__ are defined in CUDA headers.
+#include "magma.h"
 
 /*************************************************************
- *              cuDoubleComplex
+ *              magmaDoubleComplex
  */
 
-__host__ __device__ static __inline__ cuDoubleComplex 
-operator-(const cuDoubleComplex &a)
+__host__ __device__ static inline magmaDoubleComplex 
+operator - (const magmaDoubleComplex &a)
 {
     return make_cuDoubleComplex(-a.x, -a.y);
 }
 
-__host__ __device__ static __inline__ cuDoubleComplex 
-operator+(const cuDoubleComplex a, const cuDoubleComplex b)
+__host__ __device__ static inline magmaDoubleComplex 
+operator + (const magmaDoubleComplex a, const magmaDoubleComplex b)
 {
     return make_cuDoubleComplex(a.x + b.x, a.y + b.y);
 }
 
-__host__ __device__ static __inline__ void
-operator+=(cuDoubleComplex &a, const cuDoubleComplex b)
+__host__ __device__ static inline magmaDoubleComplex&
+operator += (magmaDoubleComplex &a, const magmaDoubleComplex b)
 {
     a.x += b.x; a.y += b.y;
+    return a;
 }
 
-__host__ __device__ static __inline__ cuDoubleComplex 
-operator-(const cuDoubleComplex a, const cuDoubleComplex b)
+__host__ __device__ static inline magmaDoubleComplex 
+operator - (const magmaDoubleComplex a, const magmaDoubleComplex b)
 {
     return make_cuDoubleComplex(a.x - b.x, a.y - b.y);
 }
 
-__host__ __device__ static __inline__ void
-operator-=(cuDoubleComplex &a, const cuDoubleComplex b)
+__host__ __device__ static inline magmaDoubleComplex&
+operator -= (magmaDoubleComplex &a, const magmaDoubleComplex b)
 {
     a.x -= b.x; a.y -= b.y;
+    return a;
 }
 
-__host__ __device__ static __inline__ cuDoubleComplex 
-operator*(const cuDoubleComplex a, const cuDoubleComplex b)
+__host__ __device__ static inline magmaDoubleComplex 
+operator * (const magmaDoubleComplex a, const magmaDoubleComplex b)
 {
     return make_cuDoubleComplex(a.x * b.x - a.y * b.y, a.y * b.x + a.x * b.y);
 }
 
-__host__ __device__ static __inline__ cuDoubleComplex 
-operator*(const cuDoubleComplex a, const double s)
+__host__ __device__ static inline magmaDoubleComplex 
+operator * (const magmaDoubleComplex a, const double s)
 {
     return make_cuDoubleComplex(a.x * s, a.y * s);
 }
 
-__host__ __device__ static __inline__ cuDoubleComplex 
-operator*(const double s, const cuDoubleComplex a)
+__host__ __device__ static inline magmaDoubleComplex 
+operator * (const double s, const magmaDoubleComplex a)
 {
     return make_cuDoubleComplex(a.x * s, a.y * s);
 }
 
-__host__ __device__ static __inline__ void 
-operator*=(cuDoubleComplex &a, const cuDoubleComplex b)
+__host__ __device__ static inline magmaDoubleComplex& 
+operator *= (magmaDoubleComplex &a, const magmaDoubleComplex b)
 {
   double tmp = a.y * b.x + a.x * b.y;
   a.x = a.x * b.x - a.y * b.y;
   a.y = tmp;
+    return a;
 }
 
-__host__ __device__ static __inline__ void 
-operator*=(cuDoubleComplex &a, const double s)
+__host__ __device__ static inline magmaDoubleComplex& 
+operator *= (magmaDoubleComplex &a, const double s)
 {
     a.x *= s; a.y *= s;
+    return a;
 }
 
 /*************************************************************
- *              cuFloatComplex
+ *              magmaFloatComplex
  */
 
-__host__ __device__ static __inline__ cuFloatComplex 
-operator-(const cuFloatComplex &a)
+__host__ __device__ static inline magmaFloatComplex 
+operator - (const magmaFloatComplex &a)
 {
     return make_cuFloatComplex(-a.x, -a.y);
 }
 
-__host__ __device__ static __inline__ cuFloatComplex 
-operator+(const cuFloatComplex a, const cuFloatComplex b)
+__host__ __device__ static inline magmaFloatComplex 
+operator + (const magmaFloatComplex a, const magmaFloatComplex b)
 {
     return make_cuFloatComplex(a.x + b.x, a.y + b.y);
 }
 
-__host__ __device__ static __inline__ void
-operator+=(cuFloatComplex &a, const cuFloatComplex b)
+__host__ __device__ static inline magmaFloatComplex&
+operator += (magmaFloatComplex &a, const magmaFloatComplex b)
 {
     a.x += b.x; a.y += b.y;
+    return a;
 }
 
-__host__ __device__ static __inline__ cuFloatComplex 
-operator-(const cuFloatComplex a, const cuFloatComplex b)
+__host__ __device__ static inline magmaFloatComplex 
+operator - (const magmaFloatComplex a, const magmaFloatComplex b)
 {
     return make_cuFloatComplex(a.x - b.x, a.y - b.y);
 }
 
-__host__ __device__ static __inline__ void
-operator-=(cuFloatComplex &a, const cuFloatComplex b)
+__host__ __device__ static inline magmaFloatComplex&
+operator -= (magmaFloatComplex &a, const magmaFloatComplex b)
 {
     a.x -= b.x; a.y -= b.y;
+    return a;
 }
 
-__host__ __device__ static __inline__ cuFloatComplex 
-operator*(const cuFloatComplex a, const cuFloatComplex b)
+__host__ __device__ static inline magmaFloatComplex 
+operator * (const magmaFloatComplex a, const magmaFloatComplex b)
 {
     return make_cuFloatComplex(a.x * b.x - a.y * b.y, a.y * b.x + a.x * b.y);
 }
 
-__host__ __device__ static __inline__ cuFloatComplex 
-operator*(const cuFloatComplex a, const float s)
+__host__ __device__ static inline magmaFloatComplex 
+operator * (const magmaFloatComplex a, const float s)
 {
     return make_cuFloatComplex(a.x * s, a.y * s);
 }
 
-__host__ __device__ static __inline__ cuFloatComplex 
-operator*(const float s, const cuFloatComplex a)
+__host__ __device__ static inline magmaFloatComplex 
+operator * (const float s, const magmaFloatComplex a)
 {
     return make_cuFloatComplex(a.x * s, a.y * s);
 }
 
-__host__ __device__ static __inline__ void 
-operator*=(cuFloatComplex &a, const cuFloatComplex b)
+__host__ __device__ static inline magmaFloatComplex& 
+operator *= (magmaFloatComplex &a, const magmaFloatComplex b)
 {
   float tmp = a.y * b.x + a.x * b.y;
   a.x = a.x * b.x - a.y * b.y;
   a.y = tmp;
+    return a;
 }
 
-__host__ __device__ static __inline__ void 
-operator*=(cuFloatComplex &a, const float s)
+__host__ __device__ static inline magmaFloatComplex& 
+operator *= (magmaFloatComplex &a, const float s)
 {
     a.x *= s; a.y *= s;
+    return a;
 }
 
-#endif
-
-
-
+#endif /* MAGMA_OPERATORS_H */
