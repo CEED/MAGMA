fa8d7fc140891a8a2a46d3c61473f1fe1fbf5920
Mark Gates <mgates3@icl.utk.edu>
2013-05-31 12:27:39 +0000

    add row block-cyclic distribution, and clarify column name
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2904 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/dsyevdx_2stage_m.cpp
+++ src/dsyevdx_2stage_m.cpp
@@ -389,7 +389,7 @@ magma_dsyevdx_2stage_m(magma_int_t nrgpu, char jobz, char range, char uplo,
     }
 
     t2 = get_current_time();
-    magmablas_dsetmatrix_1D_bcyclic( n, n, a, lda, da, ldda, nrgpu, distblk);
+    magma_dsetmatrix_1D_col_bcyclic( n, n, a, lda, da, ldda, nrgpu, distblk );
     magma_setdevice(0);
 
     tband1 = get_current_time();
