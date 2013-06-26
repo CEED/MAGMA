61ab6a1cf63bfd6b6fed3df0dcab9afbe46844ed
Mark Gates <mgates3@icl.utk.edu>
2013-06-26 23:56:32 +0000

    tidy up transpose code (indentation, etc.)
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3016 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- magmablas/ztranspose-v2.cu
+++ magmablas/ztranspose-v2.cu
@@ -16,93 +16,93 @@ __global__ void ztranspose3_32( magmaDoubleComplex       *B, int ldb,
                                 const magmaDoubleComplex *A, int lda,
                                 int m, int m32, int n, int n32)
 {
-         __shared__ magmaDoubleComplex a[32][ZSIZE_1SHARED+1];
+    __shared__ magmaDoubleComplex sA[32][ZSIZE_1SHARED+1];
 
-        int inx = threadIdx.x;
-        int iny = threadIdx.y;
-        int ibx = blockIdx.x*32;
-        int iby = blockIdx.y*32;
+    int inx = threadIdx.x;
+    int iny = threadIdx.y;
+    int ibx = blockIdx.x*32;
+    int iby = blockIdx.y*32;
 
-        A += ibx + inx + __mul24( iby + iny, lda );
-        B += iby + inx + __mul24( ibx + iny, ldb );
+    A += ibx + inx + __mul24( iby + iny, lda );
+    B += iby + inx + __mul24( ibx + iny, ldb );
 
-        int t2 = iby+iny;
-        if (ibx+inx<m)
-        {
-          if (t2   <n) {
-             a[iny+0][inx] = A[0*lda];
-                   if        (t2+ 8<n) {
-                a[iny+8][inx] = A[8*lda];
-                            if (t2 + 16<n) {
-                   a[iny+16][inx] = A[16*lda];
-                               if (t2 + 24<n)
-                      a[iny+24][inx] = A[24*lda];
+    int t2 = iby+iny;
+    if (ibx+inx < m) {
+        if (t2   < n) {
+            sA[iny+0][inx] = A[0*lda];
+            if (t2+ 8 < n) {
+                sA[iny+8][inx] = A[8*lda];
+                if (t2 + 16 < n) {
+                    sA[iny+16][inx] = A[16*lda];
+                    if (t2 + 24 < n) {
+                        sA[iny+24][inx] = A[24*lda];
+                    }
                 }
-                   }
-                 }
-              }
-
-        __syncthreads();
+            }
+        }
+    }
+    __syncthreads();
 
 #if defined(PRECISION_s) || defined(PRECISION_d) || defined(PRECISION_c)
-        if (iby + inx < n){
-            if (ibx + iny <m){
-                B[0*ldb] = a[inx][iny+0];
-                if (ibx + iny + 8 <m){
-                    B[8*ldb] = a[inx][iny+8];
-                    if (ibx + iny +16 <m){
-                        B[16*ldb] = a[inx][iny+16];
-                        if (ibx + iny + 24 <m)
-                            B[24*ldb] = a[inx][iny+24];
+    if (iby + inx < n) {
+        if (ibx + iny < m) {
+            B[0*ldb] = sA[inx][iny+0];
+            if (ibx + iny + 8 < m) {
+                B[8*ldb] = sA[inx][iny+8];
+                if (ibx + iny +16 < m) {
+                    B[16*ldb] = sA[inx][iny+16];
+                    if (ibx + iny + 24 < m) {
+                        B[24*ldb] = sA[inx][iny+24];
                     }
                 }
             }
         }
+    }
 #else /* defined(PRECISION_z) */
-        if (iby + inx < n){
-            if (ibx + iny <m){
-                B[0*ldb] = a[inx][iny+0];
-                if (ibx + iny + 8 <m){
-                    B[8*ldb] = a[inx][iny+8];
-                }
-            }                
-            if (iby + inx + 16 < n) {
-                if (ibx + iny <m){
-                    B[0*ldb+16] = a[inx+16][iny+0];
-                    if (ibx + iny + 8 < m){
-                        B[8*ldb+16] = a[inx+16][iny+8];
-                    }
+    if (iby + inx < n) {
+        if (ibx + iny < m) {
+            B[0*ldb] = sA[inx][iny+0];
+            if (ibx + iny + 8 < m) {
+                B[8*ldb] = sA[inx][iny+8];
+            }
+        }                
+        if (iby + inx + 16 < n) {
+            if (ibx + iny < m) {
+                B[0*ldb+16] = sA[inx+16][iny+0];
+                if (ibx + iny + 8 < m) {
+                    B[8*ldb+16] = sA[inx+16][iny+8];
                 }
             }
         }
-        
-        __syncthreads();
-        A += ZSIZE_1SHARED;
-        B += __mul24( 16, ldb);
+    }
+    
+    __syncthreads();
+    A += ZSIZE_1SHARED;
+    B += __mul24( 16, ldb );
 
-        a[iny+0][inx] = A[0*lda];
-        a[iny+8][inx] = A[8*lda];
-        a[iny+16][inx] = A[16*lda];
-        a[iny+24][inx] = A[24*lda];
+    sA[iny+ 0][inx] = A[ 0*lda];
+    sA[iny+ 8][inx] = A[ 8*lda];
+    sA[iny+16][inx] = A[16*lda];
+    sA[iny+24][inx] = A[24*lda];
 
-        __syncthreads();
+    __syncthreads();
 
-        if (iby + inx < n){
-            if (ibx + iny + 16 <m){
-                B[0*ldb] = a[inx][iny+0];
-                if (ibx + iny + 24 <m){
-                    B[8*ldb] = a[inx][iny+8];
-                }
-            }                
-            if (iby + inx + 16 < n) {
-                if (ibx + iny + 16 <m){
-                    B[0*ldb+16] = a[inx+16][iny+0];
-                    if (ibx + iny + 24 <m){
-                        B[8*ldb+16] = a[inx+16][iny+8];
-                    }
+    if (iby + inx < n) {
+        if (ibx + iny + 16 < m) {
+            B[0*ldb] = sA[inx][iny+0];
+            if (ibx + iny + 24 < m) {
+                B[8*ldb] = sA[inx][iny+8];
+            }
+        }                
+        if (iby + inx + 16 < n) {
+            if (ibx + iny + 16 < m) {
+                B[0*ldb+16] = sA[inx+16][iny+0];
+                if (ibx + iny + 24 < m) {
+                    B[8*ldb+16] = sA[inx+16][iny+8];
                 }
             }
         }
+    }
 #endif
 
 }
@@ -113,60 +113,60 @@ __global__ void ztranspose2_32( magmaDoubleComplex       *B, int ldb,
                                 const magmaDoubleComplex *A, int lda, 
                                 int m, int m32, int n, int n32)
 {        
-        __shared__ magmaDoubleComplex a[32][ZSIZE_1SHARED+1];
-        
-        int inx = threadIdx.x;
-        int iny = threadIdx.y;
-        int ibx = blockIdx.x*32;
-        int iby = blockIdx.y*32;
-        
-        int dx, dy;
-        if (ibx+32<m)
-           dx = 0;
-        else
-           dx = m32;
+    __shared__ magmaDoubleComplex sA[32][ZSIZE_1SHARED+1];
+    
+    int inx = threadIdx.x;
+    int iny = threadIdx.y;
+    int ibx = blockIdx.x*32;
+    int iby = blockIdx.y*32;
+    
+    int dx, dy;
+    if (ibx+32 < m)
+       dx = 0;
+    else
+       dx = m32;
 
-        if (iby+32<n)
-           dy = 0;
-        else
-           dy = n32;
+    if (iby+32 < n)
+       dy = 0;
+    else
+       dy = n32;
 
-        A += ibx + inx -dx + __mul24( iby + iny - dy, lda );
-        B += iby + inx -dy + __mul24( ibx + iny - dx, ldb );
-        
-        a[iny+0][inx] = A[0*lda];
-        a[iny+8][inx] = A[8*lda];
-        a[iny+16][inx] = A[16*lda];
-        a[iny+24][inx] = A[24*lda];
-        
-        __syncthreads();
+    A += ibx + inx - dx + __mul24( iby + iny - dy, lda );
+    B += iby + inx - dy + __mul24( ibx + iny - dx, ldb );
+    
+    sA[iny+0][inx] = A[0*lda];
+    sA[iny+8][inx] = A[8*lda];
+    sA[iny+16][inx] = A[16*lda];
+    sA[iny+24][inx] = A[24*lda];
+    
+    __syncthreads();
         
 #if defined(PRECISION_s) || defined(PRECISION_d) || defined(PRECISION_c)
-        B[0*ldb] = a[inx][iny+0];
-        B[8*ldb] = a[inx][iny+8];
-        B[16*ldb] = a[inx][iny+16];
-        B[24*ldb] = a[inx][iny+24];
+    B[ 0*ldb] = sA[inx][iny+0];
+    B[ 8*ldb] = sA[inx][iny+8];
+    B[16*ldb] = sA[inx][iny+16];
+    B[24*ldb] = sA[inx][iny+24];
 #else /* defined(PRECISION_z) */
-        B[0*ldb]    = a[inx][iny+0];
-        B[8*ldb]    = a[inx][iny+8];
-        B[0*ldb+16] = a[inx+16][iny+0];
-        B[8*ldb+16] = a[inx+16][iny+8];
-
-        __syncthreads();
-        A += ZSIZE_1SHARED;
-        B += __mul24( 16, ldb);
-
-        a[iny+0][inx] = A[0*lda];
-        a[iny+8][inx] = A[8*lda];
-        a[iny+16][inx] = A[16*lda];
-        a[iny+24][inx] = A[24*lda];
-
-        __syncthreads();
-
-        B[0*ldb] = a[inx][iny+0];
-        B[8*ldb] = a[inx][iny+8];
-        B[0*ldb+16] = a[inx+16][iny+0];
-        B[8*ldb+16] = a[inx+16][iny+8];
+    B[0*ldb]    = sA[inx][iny+0];
+    B[8*ldb]    = sA[inx][iny+8];
+    B[0*ldb+16] = sA[inx+16][iny+0];
+    B[8*ldb+16] = sA[inx+16][iny+8];
+    
+    __syncthreads();
+    A += ZSIZE_1SHARED;
+    B += __mul24( 16, ldb );
+    
+    sA[iny+ 0][inx] = A[ 0*lda];
+    sA[iny+ 8][inx] = A[ 8*lda];
+    sA[iny+16][inx] = A[16*lda];
+    sA[iny+24][inx] = A[24*lda];
+    
+    __syncthreads();
+    
+    B[0*ldb]    = sA[inx   ][iny+0];
+    B[8*ldb]    = sA[inx   ][iny+8];
+    B[0*ldb+16] = sA[inx+16][iny+0];
+    B[8*ldb+16] = sA[inx+16][iny+8];
 #endif
 } 
 
@@ -186,15 +186,14 @@ magmablas_ztranspose2(magmaDoubleComplex       *odata, magma_int_t ldo,
 
     dim3 threads( ZSIZE_1SHARED, 8, 1 );
     dim3 grid( (m+31)/32, (n+31)/32, 1 );
-    ztranspose3_32<<< grid, threads, 0, magma_stream >>>( odata, ldo, idata, ldi, 
-                                         // m, m%32, n, n%32);
-                                         m, (32-m%32)%32, n, (32-n%32)%32);
+    ztranspose3_32<<< grid, threads, 0, magma_stream >>>(
+        odata, ldo, idata, ldi, m, (32-m%32)%32, n, (32-n%32)%32 );
 }
 
 extern "C" void
 magmablas_ztranspose2s(magmaDoubleComplex       *odata, magma_int_t ldo,
                        const magmaDoubleComplex *idata, magma_int_t ldi,
-                       magma_int_t m, magma_int_t n, magma_queue_t *stream )
+                       magma_int_t m, magma_int_t n, magma_queue_t stream )
 {
     /* Quick return */
     if ( (m == 0) || (n == 0) )
@@ -202,7 +201,6 @@ magmablas_ztranspose2s(magmaDoubleComplex       *odata, magma_int_t ldo,
 
     dim3 threads( ZSIZE_1SHARED, 8, 1 );
     dim3 grid( (m+31)/32, (n+31)/32, 1 );
-    ztranspose3_32<<< grid, threads, 0, *stream >>>( odata, ldo, idata, ldi,
-                                         // m, m%32, n, n%32);
-                                         m, (32-m%32)%32, n, (32-n%32)%32);
+    ztranspose3_32<<< grid, threads, 0, stream >>>(
+        odata, ldo, idata, ldi, m, (32-m%32)%32, n, (32-n%32)%32 );
 }
