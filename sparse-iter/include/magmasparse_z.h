d514960de61d4711560f72e3b03b88047627b4c6
Stanimire Tomov <tomov@icl.utk.edu>
2013-06-27 04:08:55 +0000

    put the descriptor in magmasparse_z to get generated and add descriptor for BCSR matrix
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3025 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/include/magmasparse_z.h
+++ sparse-iter/include/magmasparse_z.h
@@ -17,6 +17,40 @@
 
 #define PRECISION_z
 
+/* ////////////////////////////////////////////////////////////////////////////
+   -- MAGMA_SPARSE Matrix Descriptors
+*/
+/* CSR Matrix descriptor */
+typedef struct {
+    int type;
+
+    magma_int_t   m;
+    magma_int_t   n;
+    magma_int_t nnz;
+
+    magmaDoubleComplex *d_val;
+    magma_int_t *d_rowptr;
+    magma_int_t *d_colind;
+
+} magma_zmatrix_t;
+
+
+/* BCSR Matrix descriptor *
+typedef struct {
+    int type;
+
+    magma_int_t   rows_block;
+    magma_int_t   cols_block;
+
+    magma_int_t nrow_blocks;
+    magma_int_t  nnz_blocks;
+
+    magmaDoubleComplex *d_val;
+    magma_int_t *d_rowptr;
+    magma_int_t *d_colind;
+
+} magma_zbcsr_t;
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -25,43 +59,51 @@ extern "C" {
 /* ////////////////////////////////////////////////////////////////////////////
    -- MAGMA_SPARSE Auxiliary functions
 */
-magma_int_t read_z_csr_from_binary( magma_int_t* n_row, magma_int_t* n_col, 
-                                    magma_int_t* nnz, magmaDoubleComplex **val, 
-                                    magma_int_t **row, magma_int_t **col,
-                                    const char * filename);
+magma_int_t 
+read_z_csr_from_binary( magma_int_t* n_row, magma_int_t* n_col, 
+                        magma_int_t* nnz, magmaDoubleComplex **val, 
+                        magma_int_t **row, magma_int_t **col,
+                        const char * filename);
 
-magma_int_t read_z_csr_from_mtx( magma_int_t* n_row, magma_int_t* n_col, 
-                                 magma_int_t* nnz, magmaDoubleComplex **val, 
-                                 magma_int_t **row, magma_int_t **col, 
-                                 const char *filename);
+magma_int_t 
+read_z_csr_from_mtx(    magma_int_t* n_row, magma_int_t* n_col, 
+                        magma_int_t* nnz, magmaDoubleComplex **val, 
+                        magma_int_t **row, magma_int_t **col, 
+                        const char *filename);
 
-magma_int_t write_z_csr_mtx( magma_int_t n_row, magma_int_t n_col, magma_int_t nnz, 
-                             magmaDoubleComplex **val, magma_int_t **row, 
-                             magma_int_t **col, magma_int_t MajorType,
-                             const char *filename );
+magma_int_t 
+write_z_csr_mtx(        magma_int_t n_row, magma_int_t n_col, magma_int_t nnz, 
+                        magmaDoubleComplex **val, magma_int_t **row, 
+                        magma_int_t **col, magma_int_t MajorType,
+                        const char *filename );
 
-magma_int_t print_z_csr( magma_int_t n_row, magma_int_t n_col, magma_int_t nnz, 
-                         magmaDoubleComplex **val, magma_int_t **row, 
-                         magma_int_t **col );
+magma_int_t 
+print_z_csr(            magma_int_t n_row, magma_int_t n_col, magma_int_t nnz, 
+                        magmaDoubleComplex **val, magma_int_t **row, 
+                        magma_int_t **col );
 
-magma_int_t print_z_csr_mtx( magma_int_t n_row, magma_int_t n_col, magma_int_t nnz, 
-                             magmaDoubleComplex **val, magma_int_t **row, 
-                             magma_int_t **col, magma_int_t MajorType );
+magma_int_t 
+print_z_csr_mtx(        magma_int_t n_row, magma_int_t n_col, magma_int_t nnz, 
+                        magmaDoubleComplex **val, magma_int_t **row, 
+                        magma_int_t **col, magma_int_t MajorType );
 
-magma_int_t z_array2csr( magma_int_t *m, magma_int_t *n, magma_int_t *nnz,
-                         magmaDoubleComplex*a, magmaDoubleComplex **val, 
-                         magma_int_t **row, magma_int_t **col );
+magma_int_t 
+z_array2csr(            magma_int_t *m, magma_int_t *n, magma_int_t *nnz,
+                        magmaDoubleComplex*a, magmaDoubleComplex **val, 
+                        magma_int_t **row, magma_int_t **col );
 
-magma_int_t z_csr2array( magma_int_t *m, magma_int_t *n, magma_int_t *nnz, 
-                         magmaDoubleComplex *val, magma_int_t *row, 
-                         magma_int_t *col, magmaDoubleComplex*b );
+magma_int_t 
+z_csr2array(            magma_int_t *m, magma_int_t *n, magma_int_t *nnz, 
+                        magmaDoubleComplex *val, magma_int_t *row, 
+                        magma_int_t *col, magmaDoubleComplex*b );
 
-magma_int_t z_transpose_csr( magma_int_t n_rows, magma_int_t n_cols, 
-                             magma_int_t nnz, magmaDoubleComplex *val, 
-                             magma_int_t *row, magma_int_t *col, 
-                             magma_int_t *new_n_rows, magma_int_t *new_n_cols, 
-                             magma_int_t *new_nnz, magmaDoubleComplex **new_val, 
-                             magma_int_t **new_row, magma_int_t **new_col );
+magma_int_t 
+z_transpose_csr(        magma_int_t n_rows, magma_int_t n_cols, 
+                        magma_int_t nnz, magmaDoubleComplex *val, 
+                        magma_int_t *row, magma_int_t *col, 
+                        magma_int_t *new_n_rows, magma_int_t *new_n_cols, 
+                        magma_int_t *new_nnz, magmaDoubleComplex **new_val, 
+                        magma_int_t **new_row, magma_int_t **new_col );
 
 /* ////////////////////////////////////////////////////////////////////////////
    -- MAGMA_SPARSE function definitions / Data on CPU
