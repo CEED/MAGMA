69671bffdbab62ec20daa8ba05f6193cf88fd4f1
Hartwig Anzt <hanzt@icl.utk.edu>
2013-06-30 17:20:41 +0000

    SpMV wrapper for easier usage
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3096 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ sparse-iter/blas/magma_z_blaswrapper.cpp
@@ -0,0 +1,68 @@
+/*
+    -- MAGMA (version 1.1) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2011
+
+       @precisions normal z -> c d s
+       @author Hartwig Anzt
+
+*/
+#include "common_magma.h"
+#include "../include/magmasparse_types.h"
+#include "../include/magmasparse.h"
+
+
+
+
+/*  -- MAGMA (version 1.1) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2011
+
+    Purpose
+    =======
+
+    For a given input matrix A and vectors x, y and scalars alpha, beta
+    the wrapper determines the suitable SpMV computing
+              y = alpha * A * x + beta * y.  
+    Arguments
+    =========
+
+    magma_z_sparse_matrix A       sparse matrix A    
+    magma_z_vector x              input vector x  
+    magma_z_vector y              input vector y      
+    magmaDoubleComplex alpha      scalar alpha
+    magmaDoublComplex beta        scalar beta
+
+    =====================================================================  */
+
+magma_int_t
+magma_z_SpMV(     magmaDoubleComplex alpha, magma_z_sparse_matrix A, 
+                magma_z_vector x, magmaDoubleComplex beta, magma_z_vector y ){
+
+    if( A.memory_location != x.memory_location || x.memory_location != y.memory_location ){
+        printf(" error: linear algebra objects are not located in same memory!\n");
+        // return some MAGMA_ERROR
+    }
+
+    // DEV case
+    if( A.memory_location == x.memory_location == y.memory_location == Magma_DEV){
+         if( A.storage_type == Magma_CSR )
+             magma_zgecsrmv( MagmaNoTransStr, A.num_rows, A.num_cols, alpha, A.val, A.row, A.col, x.val, beta, y.val );
+         if( A.storage_type == Magma_ELLPACK && A.major_type == Magma_RowMajor )
+             magma_ztrellmv( MagmaNoTransStr, A.num_rows, A.num_cols, A.max_nnz_row, alpha, A.val, A.col, x.val, beta, y.val );
+         if( A.storage_type == Magma_ELLPACK && A.major_type == Magma_ColMajor )
+             magma_zgeellmv( MagmaNoTransStr, A.num_rows, A.num_cols, A.max_nnz_row, alpha, A.val, A.col, x.val, beta, y.val );
+
+    }
+    // CPU case missing!
+     
+ 
+
+
+
+    return MAGMA_SUCCESS;
+}
