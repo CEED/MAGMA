27682117a972ca7a988bcbfbad330994e0662660
Hartwig Anzt <hanzt@icl.utk.edu>
2013-06-30 22:14:02 +0000

    testing SpMV
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3098 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ sparse-iter/testing/testing_z_mv.cpp
@@ -0,0 +1,90 @@
+/*
+    -- MAGMA (version 1.1) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2011
+
+       @precisions normal z -> s d c
+       @author Hartwig Anzt
+*/
+
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <cuda_runtime_api.h>
+#include <cublas.h>
+
+// includes, project
+#include "flops.h"
+#include "magma.h"
+#include "magma_lapack.h"
+#include "testings.h"
+
+/* ////////////////////////////////////////////////////////////////////////////
+   -- Testing matrix IO
+*/
+int main( int argc, char** argv)
+{
+    TESTING_INIT();
+
+    magma_opts opts;
+    parse_opts( argc, argv, &opts );
+    
+    printf("If running lapack (option --lapack), MAGMA and CUBLAS error are both computed\n"
+           "relative to CPU BLAS result. Else, MAGMA error is computed relative to CUBLAS result.\n\n"
+           "transA = %c, transB = %c\n", opts.transA, opts.transB );
+    printf("    M     N     K   MAGMA Gflop/s (ms)  CUBLAS Gflop/s (ms)   CPU Gflop/s (ms)  MAGMA error  CUBLAS error\n");
+    printf("=========================================================================================================\n");
+
+
+    const char *filename[] =
+    {
+     "test_matrices/Trefethen_20.mtx",
+     "test_matrices/Trefethen_2000.mtx",
+     "test_matrices/Trefethen_20_new.mtx",
+     "test_matrices/Trefethen_20_new2.mtx",
+     "test_matrices/Trefethen_20_new3.mtx"
+    };
+
+    magma_z_sparse_matrix A, B, C, D, E, Z;
+    magma_z_vector x, y, z;
+
+    magmaDoubleComplex one = MAGMA_Z_MAKE(1.0, 0.0);
+    magmaDoubleComplex zero = MAGMA_Z_MAKE(0.0, 0.0);
+    const char *N="N";
+
+  
+    magma_z_csr_mtx( &A, filename[1] );
+    //print_z_csr_matrix( A.num_rows, A.num_cols, A.nnz, &A.val, &A.row, &A.col );
+
+
+    magma_z_vinit( &x, 10, one );
+    magma_z_vtransfer( x, &y, Magma_CPU, Magma_DEV);
+    magma_z_vtransfer( x, &z, Magma_CPU, Magma_DEV);
+    magma_z_vvisu( y, 0,10);
+    magma_z_vvisu( y, 0,10);
+
+
+    magma_z_mtransfer( A, &B, Magma_CPU, Magma_DEV);
+    magma_z_mtransfer( B, &C, Magma_DEV, Magma_DEV);
+    magma_z_mtransfer( B, &D, Magma_DEV, Magma_CPU);
+
+    magma_z_mconvert( D, &E, Magma_CSR, Magma_ELLPACK, Magma_RowMajor, Magma_RowMajor);
+    magma_z_mconvert( E, &Z, Magma_ELLPACK, Magma_CSR, Magma_RowMajor, Magma_RowMajor);
+
+
+    magma_zgecsrmv( N, B.num_rows, B.num_cols, one, A.val, A.row, A.col, y.val, one, z.val);
+
+    magma_z_vvisu( z, 0,10);
+
+
+
+
+
+
+    TESTING_FINALIZE();
+    return 0;
+}
