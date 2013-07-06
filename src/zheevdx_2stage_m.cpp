1e0474ac332470b8d27ea1cd53a9e71e5441504d
Azzam Haidar <haidar@icl.utk.edu>
2013-07-06 15:04:14 +0000

    fix precision error
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3156 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zheevdx_2stage_m.cpp
+++ src/zheevdx_2stage_m.cpp
@@ -243,7 +243,7 @@ magma_zheevdx_2stage_m(magma_int_t nrgpu, char jobz, char range, char uplo,
     magma_int_t ldt = Vblksiz;
     magma_int_t ldv = nb + Vblksiz;
     magma_int_t blkcnt = magma_bulge_get_blkcnt(n, nb, Vblksiz);
-    magma_int_t lq2 = magma_dbulge_get_lq2(n, threads);
+    magma_int_t lq2 = magma_zbulge_get_lq2(n, threads);
 
     if (wantz) {
         lwmin = lq2 + 2 * n + n * n;
