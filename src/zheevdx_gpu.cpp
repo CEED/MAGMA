60fe548c45a305c08d49182659462cfb394f6556
Stanimire Tomov <tomov@icl.utk.edu>
2013-08-09 02:36:03 +0000

    add cpu code for small size
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3319 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zheevdx_gpu.cpp
+++ src/zheevdx_gpu.cpp
@@ -279,20 +279,22 @@ magma_zheevdx_gpu(char jobz, char range, char uplo,
         return *info;
     }
 
-    /* Quick return if possible */
-    if (n == 0) {
-        return *info;
-    }
-
-    if (n == 1) {
-        magmaDoubleComplex tmp;
-        magma_zgetvector( 1, da, 1, &tmp, 1 );
-        w[0] = MAGMA_Z_REAL(tmp);
-        if (wantz) {
-            tmp = MAGMA_Z_ONE;
-            magma_zsetvector( 1, &tmp, 1, da, 1 );
-        }
-        return *info;
+    /* Check if matrix is very small then just call LAPACK on CPU, no need for GPU */
+    if (n <= 128) {
+        #ifdef ENABLE_DEBUG
+        printf("--------------------------------------------------------------\n");
+        printf("  warning matrix too small N=%d NB=%d, calling lapack on CPU  \n", (int) n, (int) nb);
+        printf("--------------------------------------------------------------\n");
+        #endif
+        magmaDoubleComplex *a = (magmaDoubleComplex *) malloc( n * n * sizeof(magmaDoubleComplex) );
+        magma_zgetmatrix(n, n, da, ldda, a, n);
+        lapackf77_zheevd(jobz_, uplo_,
+                         &n, a, &n,
+                         w, work, &lwork,
+                         rwork, &lrwork,
+                         iwork, &liwork, info);
+        magma_zsetmatrix( n, n, a, n, da, ldda);
+        free(a);
     }
 
     magma_queue_t stream;
