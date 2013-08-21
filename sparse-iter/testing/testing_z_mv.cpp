30ba994bd67933a6e339aebdc4616448afe89b84
Ichi Yamazaki <iyamazak@utk.edu>
2013-08-21 19:36:19 +0000

    compare CSR/ELLPACK storages and with MKL
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3372 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/testing/testing_z_mv.cpp
+++ sparse-iter/testing/testing_z_mv.cpp
@@ -22,6 +22,7 @@
 #include "magma.h"
 #include "magma_lapack.h"
 #include "testings.h"
+#include "mkl_spblas.h"
 
 /* ////////////////////////////////////////////////////////////////////////////
    -- Testing matrix vector product
@@ -30,77 +31,129 @@ int main( int argc, char** argv)
 {
     TESTING_INIT();
 
-    magma_opts opts;
-    parse_opts( argc, argv, &opts );
-    
-    printf("If running lapack (option --lapack), MAGMA and CUBLAS error are both computed\n"
-           "relative to CPU BLAS result. Else, MAGMA error is computed relative to CUBLAS result.\n\n"
-           "transA = %c, transB = %c\n", opts.transA, opts.transB );
-    printf("    M     N     K   MAGMA Gflop/s (ms)  CUBLAS Gflop/s (ms)   CPU Gflop/s (ms)  MAGMA error  CUBLAS error\n");
-    printf("=========================================================================================================\n");
-
-
     const char *filename[] =
     {
-     "test_matrices/Trefethen_2000.mtx",
-     "test_matrices/Pres_Poisson.mtx",
-     "test_matrices/bloweybq.mtx",
-     "test_matrices/ecology2.mtx",
-     "test_matrices/apache2.mtx",
-     "test_matrices/crankseg_2.mtx",
-     "test_matrices/bmwcra_1.mtx",
-     "test_matrices/F1.mtx",
-     "test_matrices/audikw_1.mtx",
-     "test_matrices/circuit5M.mtx",
-     "test_matrices/boneS10.mtx",
-     "test_matrices/parabolic_fem.mtx",
-     "test_matrices/inline_1.mtx",
-     "test_matrices/ldoor.mtx"
+     "test_matrices/Trefethen_20.mtx",       // 0
+     "test_matrices/Trefethen_200.mtx",      // 1
+     "test_matrices/Trefethen_2000.mtx",     // 2
+     "test_matrices/G3_circuit.mtx",         // 3
+     "test_matrices/test.mtx",               // 4
+     "test_matrices/bcsstk01.mtx",           // 5
+     "test_matrices/Pres_Poisson.mtx",       // 6
+     "test_matrices/bloweybq.mtx",           // 7
+     "test_matrices/ecology2.mtx",           // 8
+     "test_matrices/apache2.mtx",            // 9
+     "test_matrices/crankseg_2.mtx",         // 10
+     "test_matrices/bmwcra_1.mtx",           // 11
+     "test_matrices/F1.mtx",                 // 12
+     "test_matrices/audikw_1.mtx",           // 13
+     "test_matrices/boneS10.mtx",            // 14
+     "test_matrices/parabolic_fem.mtx",      // 15
     };
-for(magma_int_t matrix=0; matrix<10; matrix++){
 
+    int id = -1, matrix = 0, i, ione = 1, *pntre, num_rblocks, num_cblocks;
+    double start, end, work[1];
+    for( int i = 1; i < argc; ++i ) {
+        if ( strcmp("--id", argv[i]) == 0 ) {
+            id = atoi( argv[++i] );
+        }
+    }
+    if (id > -1) printf( "\n    Usage: ./testing_z_mv --id %d\n\n",id );
 
-    magma_z_sparse_matrix A, B, C, D, E, Z;
-    magma_z_vector x, y, z;
+    for(matrix=0; matrix<16; matrix++)
+    {
+        magma_z_sparse_matrix hA, hB, hC, dA, dB;
+        magma_z_vector hx, hy, dx, dy;
 
-    magmaDoubleComplex one = MAGMA_Z_MAKE(1.0, 0.0);
-    magmaDoubleComplex zero = MAGMA_Z_MAKE(0.0, 0.0);
-    const char *N="N";
-
+        magmaDoubleComplex one = MAGMA_Z_MAKE(1.0, 0.0);
+        magmaDoubleComplex zero = MAGMA_Z_MAKE(0.0, 0.0);
   
-    magma_z_csr_mtx( &A, filename[matrix] );
-    //print_z_csr_matrix( A.num_rows, A.num_cols, A.nnz, &A.val, &A.row, &A.col );
+        // init matrix on CPU
+        if (id > -1) matrix = id;
+        magma_z_csr_mtx( &hA, filename[matrix] );
+        printf( "\nmatrix read: %d-by-%d with %d nonzeros\n\n",hA.num_rows,hA.num_cols,hA.nnz );
+        // conver to ELLPACKT and copy to GPU
+        magma_z_mconvert(  hA, &hB, Magma_CSR, Magma_ELLPACKT);
+        magma_z_mtransfer( hB, &dB, Magma_CPU, Magma_DEV);
+        magma_z_mfree(&hB);
 
+        // init CPU vectors
+        hC.blocksize = min(hA.num_rows, 8);
+        num_rblocks = (hA.num_rows+hC.blocksize-1)/hC.blocksize;
+        num_cblocks = (hA.num_cols+hC.blocksize-1)/hC.blocksize;
+        magma_z_vinit( &hx, Magma_CPU, num_cblocks*hC.blocksize, one );
+        magma_z_vinit( &hy, Magma_CPU, num_rblocks*hC.blocksize, zero );
+        printf( " num_rblocks=%d, block_size=%d\n",num_rblocks,hC.blocksize );
 
-    magma_z_vinit( &x, Magma_CPU, A.num_cols, one );
-    magma_z_vinit( &y, Magma_DEV, A.num_cols, one );
-    magma_z_vinit( &z, Magma_DEV, A.num_cols, zero );
-    magma_z_vtransfer( x, &z, Magma_CPU, Magma_DEV);
-    magma_z_vvisu( y, 0,10);
+        // calling MKL with CSR
+        pntre = (int*)malloc( (hA.num_rows+1)*sizeof(int) );
+        pntre[0] = 0;
+        for (i=0; i<hA.num_rows; i++ ) pntre[i] = hA.row[i+1];
+        start = magma_wtime(); 
+        for (i=0; i<10; i++ )
+        mkl_zcsrmv( "N", &hA.num_rows, &hA.num_cols, &one, "GFNC", hA.val, hA.col, hA.row, pntre, hx.val, &zero, hy.val );
+        end = magma_wtime();
+        printf( "\n > MKL  : %.2e seconds (CSR).\n",(end-start)/10 );
+        free(pntre);
+        //magma_zprint( 10,1, hy.val,10 );
 
+        // calling MKL with BSR
+        if( hA.num_rows / hC.blocksize < 10000) {
+            // conver to BSR for MKL
+            magma_z_mconvert( hA, &hC, Magma_CSR, Magma_BCSR);
+            pntre = (int*)malloc( (num_rblocks+1)*sizeof(int) );
+            pntre[0] = 0;
+            for (i=0; i<num_rblocks; i++ ) pntre[i] = hC.row[i+1];
+            start = magma_wtime(); 
+            for (i=0; i<10; i++ )
+            mkl_zbsrmv( "N", &num_rblocks, &num_cblocks, &hC.blocksize, &one, "GFNC", hC.val, hC.col, hC.row, pntre, hx.val, &zero, hy.val );
+            end = magma_wtime();
+            printf( " > MKL  : %.2e seconds (BSR, %dx%d,%d).\n",(end-start)/10,num_rblocks,num_cblocks,hC.blocksize );
+            free(pntre);
+            magma_z_mfree(&hC);
+            //magma_zprint( 10,1, hy.val,10 );
+        } else {
+            printf( " > MKL  : ?? seconds (BSR, %dx%d,%d), too big.\n",num_rblocks,num_cblocks,hC.blocksize );
+        }
+        // init GPU vectors
+        magma_z_vinit( &dx, Magma_DEV, hA.num_cols, one );
+        magma_z_vinit( &dy, Magma_DEV, hA.num_cols, zero );
+        //magma_z_vvisu( dx, 0,10);
 
-    magma_z_mtransfer( A, &B, Magma_CPU, Magma_DEV);
+        // copy matrix to GPU
+        magma_z_mtransfer( hA, &dA, Magma_CPU, Magma_DEV);
 
-    magma_z_mconvert( A, &C, Magma_CSR, Magma_BCSR);
-    magma_z_mconvert( C, &D, Magma_BCSR, Magma_CSR);
-    magma_z_mtransfer( D, &E, Magma_CPU, Magma_DEV);
+        // SpMV on GPU (CSR)
+        magma_device_sync(); start = magma_wtime(); 
+        for (i=0; i<10; i++)
+        magma_z_spmv( one, dA, dx, zero, dy);
+        magma_device_sync(); end = magma_wtime(); 
+        printf( " > MAGMA: %.2e seconds (CSR).\n",(end-start)/10 );
+        //magma_z_vvisu( dy, 0,10);
 
+        // SpMV on GPU (ELLPACKT)
+        magma_device_sync(); start = magma_wtime(); 
+        for (i=0; i<10; i++)
+        magma_z_spmv( one, dB, dx, zero, dy);
+        magma_device_sync(); end = magma_wtime(); 
+        printf( " > MAGMA: %.2e seconds (ELLPACKT).\n",(end-start)/10 );
+        //magma_z_vvisu( dy, 0,10);
+        magma_zgetvector( hA.num_rows, dy.val,1, hx.val,1 );
+        for( i=0; i<hA.num_rows; i++ ) hx.val[i] = MAGMA_Z_SUB( hx.val[i], hy.val[i] );
+        printf( " >> error = %.2e\n\n",lapackf77_zlange( "F", &hA.num_rows, &ione, hx.val, &hA.num_rows, work ) );
 
+        // free CPU memory
+        magma_z_mfree(&hA);
+        magma_z_vfree(&hx);
+        magma_z_vfree(&hy);
+        // free GPU memory
+        magma_z_mfree(&dA);
+        magma_z_mfree(&dB);
+        magma_z_vfree(&dx);
+        magma_z_vfree(&dy);
 
-
-    magma_zgecsrmv( N, B.num_rows, B.num_cols, one, B.val, B.row, B.col, y.val, one, z.val);
-    magma_z_spmv( one, B, y, zero, z);
-
-    magma_z_vvisu( z, 0,10);
-
-    magma_z_spmv( one, E, y, zero, z);
-
-    magma_z_vvisu( z, 0,10);
-
-}
-
-
-
+        if (id > -1) break;
+    }
 
     TESTING_FINALIZE();
     return 0;
