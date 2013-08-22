04bf300e3d967147fc24b2d5e751abd97cf4ae28
Ichi Yamazaki <iyamazak@utk.edu>
2013-08-22 15:32:12 +0000

    adding MKL SpMV
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3381 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- tools/magmasubs.py
+++ tools/magmasubs.py
@@ -671,6 +671,8 @@ subs = {
     ('smpkgeelltmv',   'dmpkgeelltmv',   'cmpkgeelltmv',   'zmpkgeelltmv'    ),
     ('sp1',            'dp1',            'cp1',            'zp1'             ),
     ('spipelined',     'dpipelined',     'cpipelined',     'zpipelined'      ),
+    ('mkl_scsrmv',     'mkl_dcsrmv',     'mkl_ccsrmv',     'mkl_zcsrmv'      ),
+    ('mkl_sbsrmv',     'mkl_dbsrmv',     'mkl_cbsrmv',     'mkl_zbsrmv'      ),
 
     # ----- SPARSE Iterative Solvers
     ('scg',            'dcg',            'ccg',            'zcg'             ),
