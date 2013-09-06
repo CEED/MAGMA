5ee0bf3d735694dde73293660cc049711fe66737
Stanimire Tomov <tomov@icl.utk.edu>
2013-09-06 14:38:05 +0000

    testing for zgegqr
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3414 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ testing/testing_zgegqr_gpu.cpp
@@ -0,0 +1,155 @@
+/*
+    -- MAGMA (version 1.1) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       November 2011
+
+       @precisions normal z -> s d c
+       @author Stan Tomov
+
+*/
+
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <cuda_runtime_api.h>
+#include <cublas.h>
+
+// includes, project
+#include "flops.h"
+#include "magma.h"
+#include "magma_lapack.h"
+#include "testings.h"
+
+extern "C" magma_int_t
+magma_zgegqr_gpu( magma_int_t m, magma_int_t n,
+                  magmaDoubleComplex *dA,   magma_int_t ldda,
+                  magmaDoubleComplex *dwork, magmaDoubleComplex *work,
+                  magma_int_t *info );
+
+
+/* ////////////////////////////////////////////////////////////////////////////
+   -- Testing zgegqr
+*/
+int main( int argc, char** argv)
+{
+    TESTING_INIT();
+
+    real_Double_t    gflops, gpu_perf, gpu_time, cpu_perf, cpu_time;
+    double           e1, e2, work[1];
+    magmaDoubleComplex  c_neg_one = MAGMA_Z_NEG_ONE;
+    magmaDoubleComplex *h_A, *h_R, *tau, *dtau, *h_work, tmp[1];
+    magmaDoubleComplex *d_A, *dwork;
+    magma_int_t M, N, n2, lda, ldda, lwork, info, min_mn;
+    magma_int_t ione     = 1;
+    magma_int_t ISEED[4] = {0,0,0,1};
+
+    magma_opts opts;
+    parse_opts( argc, argv, &opts );
+    opts.lapack |= opts.check;  // check (-c) implies lapack (-l)
+    
+    printf("  M     N     CPU GFlop/s (ms)    GPU GFlop/s (ms)    ||I - Q'Q||_F    \n");
+    printf("=======================================================================\n");
+    for( int i = 0; i < opts.ntest; ++i ) {
+        for( int iter = 0; iter < opts.niter; ++iter ) {
+            M = opts.msize[i];
+            N = opts.nsize[i];
+            min_mn = min(M, N);
+            lda    = M;
+            n2     = lda*N;
+            ldda   = ((M+31)/32)*32;
+            gflops = FLOPS_ZGEQRF( M, N ) / 1e9 +  FLOPS_ZUNGQR( M, N, N ) / 1e9;
+            
+            lwork = -1;
+            lapackf77_zgeqrf(&M, &N, h_A, &M, tau, tmp, &lwork, &info);
+            lwork = (magma_int_t)MAGMA_Z_REAL( tmp[0] );
+            lwork = max(lwork, 3*N*N);
+            
+            TESTING_MALLOC(    tau, magmaDoubleComplex, min_mn );
+            TESTING_MALLOC(    h_A, magmaDoubleComplex, n2     );
+            TESTING_MALLOC(    h_R, magmaDoubleComplex, n2     );
+            TESTING_DEVALLOC(  d_A, magmaDoubleComplex, ldda*N );
+            TESTING_DEVALLOC( dtau, magmaDoubleComplex, min_mn );
+            TESTING_DEVALLOC(dwork, magmaDoubleComplex,    N*N );
+            TESTING_HOSTALLOC( h_work, magmaDoubleComplex,  lwork );
+            
+            /* Initialize the matrix */
+            lapackf77_zlarnv( &ione, ISEED, &n2, h_A );
+            lapackf77_zlacpy( MagmaUpperLowerStr, &M, &N, h_A, &lda, h_R, &lda );
+            magma_zsetmatrix( M, N, h_R, lda, d_A, ldda );
+            
+            // warmup
+            magma_zgegqr_gpu( M, N, d_A, ldda, dwork, h_work, &info );
+
+            magma_zsetmatrix( M, N, h_R, lda, d_A, ldda );
+            
+            /* ====================================================================
+               Performs operation using MAGMA
+               =================================================================== */
+            gpu_time = magma_sync_wtime( 0 );
+            magma_zgegqr_gpu( M, N, d_A, ldda, dwork, h_work, &info );
+            gpu_time = magma_sync_wtime( 0 ) - gpu_time;
+
+            gpu_perf = gflops / gpu_time;
+            if (info != 0)
+                printf("magma_zgegqr returned error %d: %s.\n",
+                       (int) info, magma_strerror( info ));
+            
+            if ( opts.lapack ) {
+                /* =====================================================================
+                   Performs operation using LAPACK
+                   =================================================================== */
+                cpu_time = magma_wtime();
+
+                /* Orthogonalize on the CPU */
+                lapackf77_zgeqrf(&M, &N, h_A, &lda, tau, h_work, &lwork, &info);
+                lapackf77_zungqr(&M, &N, &N, h_A, &lda, tau, h_work, &lwork, &info );
+
+                cpu_time = magma_wtime() - cpu_time;
+                cpu_perf = gflops / cpu_time;
+                if (info != 0)
+                    printf("lapackf77_zungqr returned error %d: %s.\n",
+                           (int) info, magma_strerror( info ));
+                
+                /* =====================================================================
+                   Check the result compared to LAPACK
+                   =================================================================== */
+                magma_zgetmatrix( M, N, d_A, ldda, h_R, M );
+
+                magmaDoubleComplex one = MAGMA_Z_ONE, zero = MAGMA_Z_ZERO;
+                blasf77_zgemm("t", "n", &N, &N, &M, &one, h_R, &M, h_R, &M, &zero, h_work, &N);
+                for(int ii=0; ii<N*N; ii+=(N+1)) h_work[ii] -= one;
+
+                e1    = lapackf77_zlange("f", &N, &N, h_work, &N, work);
+
+                blasf77_zgemm("t", "n", &N, &N, &M, &one, h_A, &M, h_A, &M, &zero, h_work, &N);
+                for(int ii=0; ii<N*N; ii+=(N+1)) h_work[ii] -= one;
+                e2    = lapackf77_zlange("f", &N, &N, h_work, &N, work);
+                
+                printf("%5d %5d   %7.2f (%7.2f)   %7.2f (%7.2f)   %8.2e  %8.2e\n",
+                       (int) M, (int) N, cpu_perf, 1000.*cpu_time, gpu_perf, 1000.*gpu_time, e1, e2 );
+            }
+            else {
+                printf("%5d %5d     ---   (  ---  )   %7.2f (%7.2f)     ---  \n",
+                       (int) M, (int) N, gpu_perf, 1000.*gpu_time );
+            }
+            
+            TESTING_FREE(      tau );
+            TESTING_FREE(      h_A );
+            TESTING_HOSTFREE(h_work);
+            TESTING_FREE(    h_R );
+            TESTING_DEVFREE( d_A  );
+            TESTING_DEVFREE( dtau );
+            TESTING_DEVFREE( dwork );
+        }
+        if ( opts.niter > 1 ) {
+            printf( "\n" );
+        }
+    }
+    
+    TESTING_FINALIZE();
+    return 0;
+}
