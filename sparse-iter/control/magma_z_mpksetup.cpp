a56e2c9590166de95f97c68dfe8a99f5f9baccbf
Hartwig Anzt <hanzt@icl.utk.edu>
2013-09-20 02:17:50 +0000

    mpk kernel using offset and chunksize
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3454 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/control/magma_z_mpksetup.cpp
+++ sparse-iter/control/magma_z_mpksetup.cpp
@@ -43,9 +43,9 @@ using namespace std;
     =========
 
     magma_sparse_matrix A                input matrix A
-    magma_sparse_matrix *B                output matrix B
-    magma_int_t num_procs                number of processors for matrix power kernel
-    magma_int_t procs_id                 processor id
+    magma_sparse_matrix *B               output matrix B
+    magma_int_t offset                   first row to compute
+    magma_int_t chunksize                number of rows to compute
     magma_int_t s                        matrix powers
 
     =====================================================================  */
@@ -55,8 +55,8 @@ using namespace std;
 magma_int_t 
 magma_z_mpksetup(  magma_z_sparse_matrix A, 
                    magma_z_sparse_matrix *B, 
-                   magma_int_t num_procs, 
-                   magma_int_t procs_id, 
+                   magma_int_t offset, 
+                   magma_int_t chunksize, 
                    magma_int_t s ){
 
     if( A.memory_location == Magma_CPU ){
@@ -67,16 +67,16 @@ magma_z_mpksetup(  magma_z_sparse_matrix A,
             B->memory_location = A.memory_location;
             magma_imalloc_cpu( &B->row, A.num_rows+1 );
                 
-            magma_int_t blocksize = ( A.num_rows+num_procs-1) / num_procs ;
-            magma_int_t local_blocksize = min( blocksize, A.num_rows - procs_id * blocksize);
-            //printf("local block size: %d\n", local_blocksize);
+            magma_int_t start = offset;
+            magma_int_t end = offset + chunksize;
+            printf("local block size: %d offset: %d\n", chunksize, offset);
 
             magma_int_t *z, i, j, count, num_new_nnz;
             magma_imalloc_cpu( &z, A.num_rows );
 
             for( i=0; i<A.num_rows; i++ )
                     z[i] = 0;
-            for( i=procs_id*(blocksize); i<(procs_id)*(blocksize)+ local_blocksize; i++ )
+            for( i=start; i<end; i++ )
                     z[i] = 1;
 
             // determine the rows of A needed in local matrix B 
@@ -117,7 +117,7 @@ magma_z_mpksetup(  magma_z_sparse_matrix A,
         else{
             magma_z_sparse_matrix C, D;
             magma_z_mconvert( A, &C, A.storage_type, Magma_CSR );
-            magma_z_mpksetup(  C, &D, num_procs, procs_id, s );
+            magma_z_mpksetup(  C, &D, offset, chunksize, s );
             magma_z_mconvert( D, B, Magma_CSR, A.storage_type );
             magma_z_mfree(&C);
             magma_z_mfree(&D);
@@ -126,7 +126,7 @@ magma_z_mpksetup(  magma_z_sparse_matrix A,
     else{
         magma_z_sparse_matrix C, D;
         magma_z_mtransfer( A, &C, A.memory_location, Magma_CPU );
-        magma_z_mpksetup(  C, &D, num_procs, procs_id, s );
+        magma_z_mpksetup(  C, &D, offset, chunksize, s );
         magma_z_mtransfer( D, B, Magma_CPU, A.memory_location );
         magma_z_mfree(&C);
         magma_z_mfree(&D);
@@ -156,8 +156,8 @@ magma_z_mpksetup(  magma_z_sparse_matrix A,
     =========
 
     magma_sparse_matrix A                input matrix A
-    magma_int_t num_procs                number of processors for matrix power kernel
-    magma_int_t procs_id                 processor id
+    magma_int_t offset                   first row to compute
+    magma_int_t chunksize                number of rows to compute
     magma_int_t s                        matrix powers
     magma_int_t *num_add_rows            number of additional rows
 
@@ -165,22 +165,22 @@ magma_z_mpksetup(  magma_z_sparse_matrix A,
 
 magma_int_t 
 magma_z_mpkinfo(   magma_z_sparse_matrix A, 
-                   magma_int_t num_procs, 
-                   magma_int_t procs_id, 
+                   magma_int_t offset, 
+                   magma_int_t chunksize, 
                    magma_int_t s,    
                    magma_int_t *num_add_rows ){
 
     if( A.memory_location == Magma_CPU ){
         if( A.storage_type == Magma_CSR ){
-            magma_int_t blocksize = ( A.num_rows+num_procs-1) / num_procs ;
-            magma_int_t local_blocksize = min( blocksize, A.num_rows - procs_id * blocksize);
-            printf("local block size: %d\n", local_blocksize);
+            magma_int_t start = offset;
+            magma_int_t end = offset + chunksize;
+            printf("local block size: %d\n", chunksize);
 
             magma_int_t *z, i, j, count;
             magma_imalloc_cpu( &z, A.num_rows );
             for( i=0; i<A.num_rows; i++ )
                     z[i] = 0;
-            for( i=procs_id*(blocksize); i<(procs_id)*(blocksize)+ local_blocksize; i++ )
+            for( i=start; i<end; i++ )
                     z[i] = 1;
 
             // determine the rows of A needed in local matrix B 
@@ -194,11 +194,11 @@ magma_z_mpkinfo(   magma_z_sparse_matrix A,
             }
 
             (*num_add_rows) = 0;
-            for( i=0; i<procs_id*blocksize; i++){
+            for( i=0; i<start; i++){
                 if( z[i] != 0 )
                     (*num_add_rows)++;
             }
-            for( i=(procs_id+1)*blocksize; i<A.num_rows; i++){
+            for( i=end; i<A.num_rows; i++){
                 if( z[i] != 0 )
                     (*num_add_rows)++;
             }
@@ -210,13 +210,13 @@ magma_z_mpkinfo(   magma_z_sparse_matrix A,
             magma_int_t *add_rows;
             magma_imalloc_cpu( &(add_rows), (*num_add_rows) );
             (*num_add_rows) = 0;
-            for( i=0; i<procs_id*blocksize; i++){
+            for( i=0; i<start; i++){
                 if( z[i] != 0 ){
                     (add_rows)[(*num_add_rows)] = i;
                     (*num_add_rows)++;
                 }
             }
-            for( i=(procs_id+1)*blocksize; i<A.num_rows; i++){
+            for( i=end; i<A.num_rows; i++){
                 if( z[i] != 0 ){
                     (add_rows)[(*num_add_rows)] = i;
                     (*num_add_rows)++;
@@ -234,14 +234,14 @@ magma_z_mpkinfo(   magma_z_sparse_matrix A,
         else{
             magma_z_sparse_matrix C;
             magma_z_mconvert( A, &C, A.storage_type, Magma_CSR );
-            magma_z_mpkinfo(  C, num_procs, procs_id, s, num_add_rows );
+            magma_z_mpkinfo(  C, offset, chunksize, s, num_add_rows );
             magma_z_mfree(&C);
         }
     }
     else{
         magma_z_sparse_matrix C;
         magma_z_mtransfer( A, &C, A.memory_location, Magma_CPU );
-        magma_z_mpkinfo(  C, num_procs, procs_id, s, num_add_rows );
+        magma_z_mpkinfo(  C, offset, chunksize, s, num_add_rows );
         magma_z_mfree(&C);
     }
 
