892f22fadd305511fa87dbe62f15415257f7a3c1
Hartwig Anzt <hanzt@icl.utk.edu>
2013-09-20 20:45:01 +0000

    new testfile for matrix power kernel on multi-GPU
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3461 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/testing/testing_z_mpk.cpp
+++ sparse-iter/testing/testing_z_mpk.cpp
@@ -60,6 +60,7 @@ int main( int argc, char** argv)
      "test_matrices/boneS10.mtx",            // 14
      "test_matrices/parabolic_fem.mtx",      // 15
      "test_matrices/fv1.mtx",                // 16
+     "test_matrices/lap_25.mtx",             // 17
     };
 
     int id = -1, matrix = 0, i, ione = 1, *pntre, num_rblocks, num_cblocks;
@@ -71,13 +72,14 @@ int main( int argc, char** argv)
     }
     if (id > -1) printf( "\n    Usage: ./testing_z_mv --id %d\n\n",id );
 
-    for(matrix=4; matrix<5; matrix++)
+    for(matrix=17; matrix<18; matrix++)
     {
-        magma_z_sparse_matrix hA, hB, hB1, hB2, hB3, dA, hC;
-        magma_z_vector a,b,c;
+        magma_z_sparse_matrix hA, hB1, hB2, hB3, dA, hC;
         magma_int_t num_add_rows, *add_rows;
         magmaDoubleComplex one  = MAGMA_Z_MAKE(1.0, 0.0);
         magmaDoubleComplex zero = MAGMA_Z_MAKE(0.0, 0.0);
+
+
   
         // init matrix on CPU
         if (id > -1) matrix = id;
@@ -88,18 +90,70 @@ int main( int argc, char** argv)
         //magma_z_mconvert( hA, &hB, hA.storage_type, Magma_ELLPACK);
         //magma_z_mtransfer( hB, &dA, hB.memory_location, Magma_DEV);
 
+        //MagmaMaxGPUs
+        magma_int_t num_gpus = 2;
+        magma_int_t num_procs=num_gpus, big_blocksize, *blocksize, *offset;
+        magma_imalloc_cpu( &(blocksize), num_procs );
+        magma_imalloc_cpu( &(offset), num_procs );
+        big_blocksize = floor( hA.num_rows / num_procs );
+        for( int gpu=0; gpu<num_procs-1; gpu++ ){
+            blocksize[gpu] = big_blocksize;
+        }
+        blocksize[num_procs-1] = hA.num_rows -  (num_procs-1) * big_blocksize;
 
-        for(int k=0; k<5; k++){
-            printf("\n \n \n -----------------\nk=%d\n",k ); 
-            // possibility to print information about additional rows
-            //magma_z_mpkinfo( hA, 3, 0, k, &num_add_rows, add_rows );
-            magma_z_mpksetup( hA, &hB1, 3, 0, k );
-            //magma_z_mvisu( hB1 );
-            magma_z_mpksetup( hA, &hB2, 3, 1, k );
-            //magma_z_mvisu( hB2 );
-            magma_z_mpksetup( hA, &hB3, 3, 2, k );
-            //magma_z_mvisu( hB3 );
+        offset[0] = 0;
+        for( int gpu=0; gpu<num_procs-1; gpu++ ){
+            offset[gpu+1] = offset[gpu] + blocksize[gpu];
+        }
+
+        // GPU stream
+        magma_queue_t stream[num_procs];
+        for( int gpu=0; gpu<num_procs; gpu++ )
+            magma_queue_create( &stream[gpu] );
+
+        for(int sk=2; sk<5; sk++){
+            printf("k: %d\n", sk);
+            magma_z_sparse_matrix hB[MagmaMaxGPUs];
+            magma_z_sparse_matrix dB[MagmaMaxGPUs];
+            magma_z_vector a[MagmaMaxGPUs][6];
+            magma_z_mpksetup(  hA, hB, num_procs, offset, blocksize, sk );
+            for( int gpu=0; gpu<num_procs; gpu++ ){
+                magma_setdevice(gpu);
+                magmablasSetKernelStream(stream[gpu]);
+                magma_z_mtransfer( hB[gpu], &dB[gpu], Magma_CPU, Magma_DEV);
+                for(int j=0; j<8; j++)
+                    magma_z_vinit( &a[gpu][j], Magma_DEV, hA.num_rows, one );
+            }
+
             
+/*
+            // >>> gather q[k-1] into qt[d] <<<
+            for (d=0; d<num_gpus; d++) {
+                magma_setdevice(d);
+                magmablasSetKernelStream(stream[d][0]);
+                for( i=0; i<num_gpus; i++ ) {
+                    if( i == d ) {
+                        magma_zcopy(dofs(d), qval(d,k+j-1), 1, &(qt[i].val[frow(d)]), 1);
+                    } else {
+                        magma_zcopymatrix_async(dofs(d),1, qval(d,k+j-1), ld(d), &(qt[i].val[frow(d)]), dofs(d), stream[d][0]);
+                        magma_event_record( event[d][i], stream[d][0] );
+                    }
+                }
+            }*/
+            for(int j=0; j<sk; j++){
+                // compute spmv
+                for (int d=0; d<num_gpus; d++) {
+                    magma_setdevice(d);
+                    magmablasSetKernelStream(stream[d]);
+                    // r = A q[k] 
+                    //rt[d].val = qval(d,k+j); rt[d].num_rows = rt[d].nnz = dofs(d);
+                    magma_z_spmv( one, dB[d], a[d][j], zero, a[d][j+1] );
+
+                    //  r = r - B(j) q[j]
+                    //magma_zaxpy( dofs(d),-B[j], qval(d,k+j-1), 1, qval(d,k+j), 1);// shift for newton basis
+                }
+            }
+
 
         }
 
