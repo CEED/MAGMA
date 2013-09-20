c509e4c78cd3e91f59f18bb677e49c87a530e1cb
Mark Gates <mgates3@icl.utk.edu>
2013-09-20 21:39:53 +0000

    Update warning message text
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@3464 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- control/magma_threadsetting.cpp
+++ control/magma_threadsetting.cpp
@@ -13,7 +13,7 @@
 /***************************************************************************//**
  * switch lapack thread_num initialization
  **/
-#if defined(_OPENMP) 
+#if defined(_OPENMP)
 #include <omp.h>
 #endif
 
@@ -27,27 +27,28 @@ void magma_setlapack_numthreads(magma_int_t num_threads)
 #if defined(_OPENMP)
     omp_set_num_threads( num_threads );
     #if defined(MAGMA_WITH_MKL)
-        mkl_set_num_threads( num_threads );
+    mkl_set_num_threads( num_threads );
     #endif
 #elif defined(MAGMA_WITH_MKL)
-        mkl_set_num_threads( num_threads );
-        #ifndef NO_WARNING            
-        printf("==========================================================================================\n");
-        printf("  WARNING a parallel section (D&C) that use OPENMP could not perform in parallel because  \n");
-        printf("  it need to be compiled with multi thread library and add -fopenmp(gcc)/-openmp(icc)     \n");
-        printf("  to both compilation and linkage flags.                                                  \n");
-        printf("  If you wish to remove the printout of this WARNING, please compile with -DNO_WARNING    \n");
-        printf("==========================================================================================\n");
-        #endif
+    mkl_set_num_threads( num_threads );
+    #ifndef NO_WARNING
+    printf("==============================================================================\n"
+           "WARNING: a parallel section of MAGMA could not be run in parallel because\n"
+           "OpenMP was not enabled; add -fopenmp (for gcc) or -openmp (for icc) to\n"
+           "both compilation and linkage flags (OPTS and LDOPTS in make.inc).\n"
+           "To disable this warning, compile MAGMA with -DNO_WARNING.\n"
+           "==============================================================================\n");
+    #endif
 #else
-   #ifndef NO_WARNING    
-   printf("==========================================================================================\n");
-   printf("  WARNING you are calling a parallel section without linking with a multithread library   \n");
-   printf("  please compile with multi thread library and add -fopenmp(gcc)/-openmp(icc) to both     \n");
-   printf("  compilation and linkage flags.                                                          \n");
-   printf("  If you wish to remove the printout of this WARNING, please compile with -DNO_WARNING    \n");
-   printf("==========================================================================================\n");
-   #endif
+    #ifndef NO_WARNING
+    printf("==============================================================================\n"
+           "WARNING: a parallel section of MAGMA could not be run in parallel because\n"
+           "OpenMP was not enabled; add -fopenmp (for gcc) or -openmp (for icc) to\n"
+           "both compilation and linkage flags (OPTS and LDOPTS in make.inc).\n"
+           "Also, if using multi-threaded MKL, please compile MAGMA with -DMAGMA_WITH_MKL.\n"
+           "To disable this warning, compile MAGMA with -DNO_WARNING.\n"
+           "==============================================================================\n");
+    #endif
 #endif
 }
 
@@ -62,26 +63,28 @@ magma_int_t magma_getlapack_numthreads()
     num_threads = mkl_get_max_threads();
     #endif
 #elif defined(MAGMA_WITH_MKL)
-        num_threads = mkl_get_max_threads();
-        #ifndef NO_WARNING            
-        printf("==========================================================================================\n");
-        printf("  WARNING a parallel section (D&C) that use OPENMP could not perform in parallel because  \n");
-        printf("  it need to be compiled with multi thread library and add -fopenmp(gcc)/-openmp(icc)     \n");
-        printf("  to both compilation and linkage flags.                                                  \n");
-        printf("  If you wish to remove the printout of this WARNING, please compile with -DNO_WARNING    \n");
-        printf("==========================================================================================\n");
-        #endif
+    num_threads = mkl_get_max_threads();
+    #ifndef NO_WARNING
+    printf("==============================================================================\n"
+           "WARNING: a parallel section of MAGMA could not be run in parallel because\n"
+           "OpenMP was not enabled; add -fopenmp (for gcc) or -openmp (for icc) to\n"
+           "both compilation and linkage flags (OPTS and LDOPTS in make.inc).\n"
+           "To disable this warning, compile MAGMA with -DNO_WARNING.\n"
+           "==============================================================================\n");
+    #endif
 #else
-   #ifndef NO_WARNING    
-   printf("==========================================================================================\n");
-   printf("  WARNING you are calling a parallel section without linking with a multithread library   \n");
-   printf("  please compile with multi thread library and add -fopenmp(gcc)/-openmp(icc) to both     \n");
-   printf("  compilation and linkage flags.                                                          \n");
-   printf("  If you wish to remove the printout of this WARNING, please compile with -DNO_WARNING    \n");
-   printf("==========================================================================================\n");
+   #ifndef NO_WARNING
+    printf("==============================================================================\n"
+           "WARNING: a parallel section of MAGMA could not be run in parallel because\n"
+           "OpenMP was not enabled; add -fopenmp (for gcc) or -openmp (for icc) to\n"
+           "both compilation and linkage flags (OPTS and LDOPTS in make.inc).\n"
+           "Also, if using multi-threaded MKL, please compile MAGMA with -DMAGMA_WITH_MKL.\n"
+           "To disable this warning, compile MAGMA with -DNO_WARNING.\n"
+           "==============================================================================\n");
    #endif
 #endif
-    if(num_threads==-1) num_threads = magma_get_numthreads();
+    if(num_threads==-1)
+        num_threads = magma_get_numthreads();
     return num_threads;
 }
 
