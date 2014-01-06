53dd6ec6a7abf1490b988b161dc49cf069ba9c7e
Mark Gates <mgates3@icl.utk.edu>
2014-01-06 22:09:07 +0000

    remove unused macros
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@4042 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- include/magma_mangling.h
+++ include/magma_mangling.h
@@ -22,20 +22,12 @@
 #ifndef MAGMA_FORTRAN_NAME
     #if defined(MAGMA_GLOBAL)
         #define FORTRAN_NAME(lcname, UCNAME)  MAGMA_GLOBAL( lcname, UCNAME )
-        //#define MAGMA_FORTRAN_NAME(lcname, UCNAME)     MAGMA_GLOBAL_( magmaf_##lcname, MAGMAF_##UCNAME )
-        //#define MAGMA_GPU_FORTRAN_NAME(lcname, UCNAME) MAGMA_GLOBAL_( magmaf_##lcname##_gpu, MAGMAF_##UCNAME##_GPU )
     #elif defined(ADD_)
         #define FORTRAN_NAME(lcname, UCNAME)  lcname##_
-        //#define MAGMA_FORTRAN_NAME(lcname, UCNAME)      magmaf_##lcname##_
-        //#define MAGMA_GPU_FORTRAN_NAME(lcname, UCNAME)  magmaf_##lcname##_gpu_
     #elif defined(NOCHANGE)
         #define FORTRAN_NAME(lcname, UCNAME)  lcname
-        //#define MAGMA_FORTRAN_NAME(lcname, UCNAME)      magmaf_##lcname
-        //#define MAGMA_GPU_FORTRAN_NAME(lcname, UCNAME)  magmaf_##lcname##_gpu
     #elif defined(UPCASE)
         #define FORTRAN_NAME(lcname, UCNAME)  UCNAME
-        //#define MAGMA_FORTRAN_NAME(lcname, UCNAME)      MAGMAF_##UCNAME
-        //#define MAGMA_GPU_FORTRAN_NAME(lcname, UCNAME)  MAGMAF_##UCNAME##_GPU
     #else
         #error Define one of ADD_, NOCHANGE, or UPCASE (in make.inc, or in your CFLAGS) for how Fortran functions are name mangled. If using CMake, it should define MAGMA_GLOBAL.
     #endif
