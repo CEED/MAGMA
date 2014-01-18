d845eae6867a8ea24d1c0c163220c111f68bb63a
Hartwig Anzt <hanzt@icl.utk.edu>
2014-01-18 16:13:31 +0000

    release version of CG
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@4110 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/src/zcg.cpp
+++ sparse-iter/src/zcg.cpp
@@ -19,11 +19,6 @@
 #define RTOLERANCE     1e-16
 #define ATOLERANCE     1e-16
 
-
-magma_int_t
-magma_zcg( magma_z_sparse_matrix A, magma_z_vector b, magma_z_vector *x,  
-           magma_solver_parameters *solver_par )
-{
 /*  -- MAGMA (version 1.1) --
        Univ. of Tennessee, Knoxville
        Univ. of California, Berkeley
@@ -46,7 +41,11 @@ magma_zcg( magma_z_sparse_matrix A, magma_z_vector b, magma_z_vector *x,
     magma_z_vector *x                         solution approximation
     magma_solver_parameters *solver_par       solver parameters
 
-    =====================================================================  */
+    ========================================================================  */
+
+magma_int_t
+magma_zcg( magma_z_sparse_matrix A, magma_z_vector b, magma_z_vector *x,  
+           magma_solver_parameters *solver_par ){
 
     // local variables
     magmaDoubleComplex c_zero = MAGMA_Z_ZERO, c_one = MAGMA_Z_ONE, 
@@ -62,7 +61,7 @@ magma_zcg( magma_z_sparse_matrix A, magma_z_vector b, magma_z_vector *x,
     
     // solver variables
     magmaDoubleComplex alpha, beta;
-    double nom, nom0, r0, betanom, den;
+    double nom, nom0, r0, betanom, betanomsq, den;
     magma_int_t i;
 
     // solver setup
@@ -92,58 +91,56 @@ magma_zcg( magma_z_sparse_matrix A, magma_z_vector b, magma_z_vector *x,
     #ifdef ENABLE_TIMER
     double t_spmv1, t_spmv = 0.0;
     double tempo1, tempo2;
+    int iterblock = 1;
     magma_device_sync(); tempo1=magma_wtime();
-   //     printf("Iteration: %4d  Norm: %e  Time: %.2lf  SpMV: %.2lf %.2lf%%  Rest: %.2lf\n", 
-   //                 (solver_par->numiter), nom, 0.0, 0.0, 0.0 );
+    printf("#===============================================#\n");
+    printf("#   CG performance analysis ever %d iteration   #\n", iterblock);
+    printf("#   iter   ||   residual-nrm2    ||   runtime    #\n");
+    printf("#===============================================#\n");
+    printf("      0    ||    %e    ||    0.0000      \n", nom);
+    magma_device_sync(); tempo1=magma_wtime();
     #endif
     
     // start iteration
-    for( solver_par->numiter= 1; i<solver_par->maxiter; solver_par->numiter++ ) {
+    for( solver_par->numiter= 1; i<solver_par->maxiter; solver_par->numiter++ ){
         alpha = MAGMA_Z_MAKE(nom/den, 0.);
         magma_zaxpy(dofs,  alpha, p.val, 1, x->val, 1);               // x = x + alpha p
         magma_zaxpy(dofs, -alpha, q.val, 1, r.val, 1);                // r = r - alpha q
         betanom = magma_dznrm2(dofs, r.val, 1);                       // betanom = || r ||
-        betanom = betanom * betanom;                                  // betanom = r dot r
-      //  printf("Iteration : %4d  Norm: %f\n", i, betanom);
-        if ( betanom < r0 ) {
-            solver_par->numiter = i;
+        betanomsq = betanom * betanom;                                // betanoms = r dot r
+        if (  betanom  < r0 ) {
             break;
         }
-        beta = MAGMA_Z_MAKE(betanom/nom, 0.);                         // beta = betanom/nom
+        beta = MAGMA_Z_MAKE(betanomsq/nom, 0.);                       // beta = betanoms/nom
         magma_zscal(dofs, beta, p.val, 1);                            // p = beta*p
         magma_zaxpy(dofs, c_one, r.val, 1, p.val, 1);                 // p = p + r 
         magma_z_spmv( c_one, A, p, c_zero, q );                       // q = A p
         den = MAGMA_Z_REAL(magma_zdotc(dofs, p.val, 1, q.val, 1));    // den = p dot q
-        nom = betanom;
-
+        nom = betanomsq;
 
         #ifdef ENABLE_TIMER
         //Chronometry  
         magma_device_sync(); tempo2=magma_wtime();
-        //cublasGetVector(1 , sizeof( magmaDoubleComplex ), skp+5, 1, skp_h+5, 1 );
-        if( solver_par->numiter%1000==0 ) {
-        printf("Iteration: %4d  Norm: %e  Time: %.2lf  SpMV: %.2lf %.2lf%%  Rest: %.2lf\n", 
-                    (solver_par->numiter), nom, tempo2-tempo1, t_spmv, 100.0*t_spmv/(tempo2-tempo1), tempo2-tempo1-t_spmv);
+        if( solver_par->numiter%iterblock==0 ) {
+            printf("   %4d    ||    %e    ||    %.4lf  \n", (solver_par->numiter), betanom, tempo2-tempo1 );
         }
         #endif
     } 
-    #ifndef ENABLE_TIMER
-    printf("Iteration: %4d  Norm: %e  Time: %.2lf  SpMV: %.2lf %.2lf%%  Rest: %.2lf\n", 
-                (solver_par->numiter), nom, tempo2-tempo1, t_spmv, 100.0*t_spmv/(tempo2-tempo1), tempo2-tempo1-t_spmv);
+
+    #ifdef ENABLE_TIMER
+    double residual;
+    magma_zresidual( A, b, *x, &residual );
+    printf("#===============================================#\n");
+    printf("# CG solver summary:\n");
+    printf("#    initial residual: %e\n", nom0 );
+    printf("#    iterations: %4d\n#    iterative residual: %e\n",
+            (solver_par->numiter), betanom );
+    printf("#    exact relative residual: %e\n#    runtime: %.4lf sec\n", 
+                residual, tempo2-tempo1);
+    printf("#===============================================#\n");
     #endif
-    
-    printf( "      (r_0, r_0) = %e\n", nom0);
-    printf( "      (r_N, r_N) = %e\n", betanom);
-    printf( "      Number of CG iterations: %d\n", i);
-/*    
-    if (solver_par->epsilon == RTOLERANCE) {
-        magma_z_spmv( c_one, A, *x, c_zero, r );                       // r = A x
-        magma_zaxpy(dofs,  c_mone, b.val, 1, r.val, 1);                // r = r - b
-        den = magma_dznrm2(dofs, r.val, 1);                            // den = || r ||
-        printf( "      || r_N ||   = %e\n", den);
-        solver_par->residual = (double)(den);
-    }
-*/
+        
+    solver_par->residual = (double)(betanom);
     magma_z_vfree(&r);
     magma_z_vfree(&p);
     magma_z_vfree(&q);
