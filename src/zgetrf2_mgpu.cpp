bbfe7797dede765caaba17d33eeabe790123e4e4
Mark Gates <mgates3@icl.utk.edu>
2014-01-24 16:15:04 +0000

    fix info comment
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@4201 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zgetrf2_mgpu.cpp
+++ src/zgetrf2_mgpu.cpp
@@ -67,7 +67,7 @@ magma_zgetrf2_mgpu(magma_int_t num_gpus,
     INFO    (output) INTEGER
             = 0:  successful exit
             < 0:  if INFO = -i, the i-th argument had an illegal value
-                  if INFO = -7, internal GPU memory allocation failed.
+                  or another error occured, such as memory allocation failed.
             > 0:  if INFO = i, U(i,i) is exactly zero. The factorization
                   has been completed, but the factor U is exactly
                   singular, and division by zero will occur if it is used
@@ -409,7 +409,6 @@ magma_zgetrf2_mgpu(magma_int_t num_gpus,
     timer_printf("\n Performance %f GFlop/s\n", FLOPS_ZGETRF(m,n) / 1e9 / time );
 
     return *info;
-    /* End of MAGMA_ZGETRF2_MGPU */
-}
+} /* magma_zgetrf2_mgpu */
 
 #undef inAT
