8cac2b508ac4e5f9ce002271ec532587c7c6817f
Mark Gates <mgates3@icl.utk.edu>
2014-03-14 22:27:12 +0000

    use @date
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@4494 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zpotrf_mgpu_right.cpp
+++ src/zpotrf_mgpu_right.cpp
@@ -3,7 +3,7 @@
        Univ. of Tennessee, Knoxville
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
-       June 2012
+       @date
 
        @precisions normal z -> s d c
 
@@ -39,7 +39,7 @@ magma_zpotrf_mgpu_right(magma_int_t num_gpus, magma_uplo_t uplo, magma_int_t n,
        Univ. of Tennessee, Knoxville
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
-       June 2012
+       @date
 
     Purpose
     =======
