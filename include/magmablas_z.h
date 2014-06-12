4165b9a91cb2b7aa3ef99c11f10972a038b5af69
Tingxing Dong <tdong@vols.utk.edu>
2014-06-12 16:41:25 +0000

    add zgemv_batched.cu which is used in batched colesky add its header. when compiled on tesla 1.x architecture, waring of pointers will be prompted but can be safely ignored
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@4916 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- include/magmablas_z.h
+++ include/magmablas_z.h
@@ -524,6 +524,15 @@ void magmablas_zgemv(
     magmaDoubleComplex beta,
     magmaDoubleComplex_ptr       dy, magma_int_t incy );
 
+void magmablas_zgemv_batched(
+    magma_trans_t trans, magma_int_t m, magma_int_t n,
+    magmaDoubleComplex alpha,
+    magmaDoubleComplex **A_array, magma_int_t ldda,
+    magmaDoubleComplex **dx_array, magma_int_t incx,
+    magmaDoubleComplex beta,
+    magmaDoubleComplex **dy_array, magma_int_t incy,
+    magma_int_t batchCount);
+
 magma_int_t magmablas_zhemv(
     magma_uplo_t uplo, magma_int_t n,
     magmaDoubleComplex alpha,
