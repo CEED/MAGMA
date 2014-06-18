de5fe8fc277bca073471039b25db0b51091ee939
Mark Gates <mgates3@icl.utk.edu>
2014-06-18 20:30:08 +0000

    Update xerbla documentation. Print message instead of calling LAPACK's xerbla.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@4935 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- control/xerbla.cpp
+++ control/xerbla.cpp
@@ -4,39 +4,33 @@
 
 #include "common_magma.h"
 
-extern "C"
-void magma_xerbla(const char *srname , magma_int_t info)
-{
-/*  -- MAGMA (version 1.1) --
-       Univ. of Tennessee, Knoxville
-       Univ. of California, Berkeley
-       Univ. of Colorado, Denver
-       @date
-
+/**
     Purpose
-    =======
-
+    -------
     magma_xerbla is an error handler for the MAGMA routines.
     It is called by a MAGMA routine if an input parameter has an
-    invalid value. It calls the LAPACK XERBLA routine, which by default
-    prints an error message and stops execution.
+    invalid value. It prints an error message.
 
-    Installers may consider modifying the STOP statement in order to
+    Installers may consider modifying it to
     call system-specific exception-handling facilities.
 
     Arguments
-    =========
-
-    SRNAME  (input) CHARACTER*(*)
+    ---------
+    @param[in]
+    srname  CHAR*
             The name of the routine which called XERBLA.
-            In C it is convenient to use __func__.
+            In C/C++ it is convenient to use __func__.
 
-    INFO    (input) INTEGER
+    @param[in]
+    info    INTEGER
             The position of the invalid parameter in the parameter list
             of the calling routine.
 
-    =====================================================================   */
-
-    int len = strlen( srname );
-    lapackf77_xerbla( srname, &info, len );
+    @ingroup magma_aux
+    ********************************************************************/
+extern "C"
+void magma_xerbla(const char *srname , magma_int_t info)
+{
+    fprintf( stderr, "On entry to %s, parameter %d had an illegal value\n",
+             srname, info );
 }
