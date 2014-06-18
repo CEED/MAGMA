4bbaee955df2ebf9dffa68b658203cb4b370d136
Mark Gates <mgates3@icl.utk.edu>
2014-06-18 21:14:27 +0000

    use generic @date
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@4937 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/dtrevc3_mt.cpp
+++ src/dtrevc3_mt.cpp
@@ -3,7 +3,7 @@
        Univ. of Tennessee, Knoxville
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
-       November 2011
+       @date
 
        @author Mark Gates
        @author Azzam Haidar
--- testing/testing_zpotrf_mgpu_right.cpp
+++ testing/testing_zpotrf_mgpu_right.cpp
@@ -3,7 +3,7 @@
  *     Univ. of Tennessee, Knoxville
  *     Univ. of California, Berkeley
  *     Univ. of Colorado, Denver
- *     June 2012
+ *     @date
  *
  * @precisions normal z -> c d s
  *
