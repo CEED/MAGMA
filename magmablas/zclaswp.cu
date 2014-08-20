2cc75e4a994795180472c5007535444ace20f7df
Mark Gates <mgates3@icl.utk.edu>
2014-08-20 17:33:03 +0000

    spelling, capitalization, format docs
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@5322 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- magmablas/zclaswp.cu
+++ magmablas/zclaswp.cu
@@ -13,43 +13,43 @@
 #define NB 64
 
 __global__ void
-zclaswp_kernel(int n, magmaDoubleComplex *a, int lda, magmaFloatComplex *sa, int m, const magma_int_t *ipiv)
+zclaswp_kernel(int n, magmaDoubleComplex *A, int lda, magmaFloatComplex *SA, int m, const magma_int_t *ipiv)
 {
     int ind = blockIdx.x*NB + threadIdx.x;
     int newind;
     magmaFloatComplex res;
     
     if (ind < m) {
-        sa   += ind;
+        SA   += ind;
         ipiv += ind;
         
         newind = ipiv[0];
         
         for(int i=0; i < n; i++) {
-            res = MAGMA_C_MAKE( (float)cuCreal(a[newind+i*lda]),
-                                (float)cuCimag(a[newind+i*lda]) );
-            sa[i*lda] = res; 
+            res = MAGMA_C_MAKE( (float)cuCreal(A[newind+i*lda]),
+                                (float)cuCimag(A[newind+i*lda]) );
+            SA[i*lda] = res; 
         }
     }
 }
 
 __global__ void
-zclaswp_inv_kernel(int n, magmaDoubleComplex *a, int lda, magmaFloatComplex *sa, int m, const magma_int_t *ipiv)
+zclaswp_inv_kernel(int n, magmaDoubleComplex *A, int lda, magmaFloatComplex *SA, int m, const magma_int_t *ipiv)
 {
     int ind = blockIdx.x*NB + threadIdx.x;
     int newind;
     magmaDoubleComplex res;
 
     if (ind < m) {
-        a    += ind;
+        A    += ind;
         ipiv += ind;
 
         newind = ipiv[0];
 
         for(int i=0; i < n; i++) {
-            res = MAGMA_Z_MAKE( (double)cuCrealf(sa[newind+i*lda]),
-                                (double)cuCimagf(sa[newind+i*lda]) );
-            a[i*lda] = res;
+            res = MAGMA_Z_MAKE( (double)cuCrealf(SA[newind+i*lda]),
+                                (double)cuCimagf(SA[newind+i*lda]) );
+            A[i*lda] = res;
         }
     }
 }
@@ -60,33 +60,40 @@ zclaswp_inv_kernel(int n, magmaDoubleComplex *a, int lda, magmaFloatComplex *sa,
     -------
     Row i of A is cast to single precision in row ipiv[i] of SA, for 0 <= i < M.
 
-    N      - (input) INTEGER.
-             On entry, N specifies the number of columns of the matrix A.
-
-    A      - (input) DOUBLE PRECISION array on the GPU, dimension (LDA,N)
-             On entry, the M-by-N matrix to which the row interchanges will be applied.
-
-    LDA    - (input) INTEGER.
-             LDA specifies the leading dimension of A.
-
-    SA     - (output) REAL array on the GPU, dimension (LDA,N)
-             On exit, the single precision, permuted matrix.
+    @param[in]
+    n       INTEGER.
+            On entry, N specifies the number of columns of the matrix A.
+    
+    @param[in]
+    A       DOUBLE PRECISION array on the GPU, dimension (LDA,N)
+            On entry, the M-by-N matrix to which the row interchanges will be applied.
+    
+    @param[in]
+    lda     INTEGER.
+            LDA specifies the leading dimension of A.
+    
+    @param[out]
+    SA      REAL array on the GPU, dimension (LDA,N)
+            On exit, the single precision, permuted matrix.
         
-    M      - (input) The number of rows to be interchanged.
-
-    IPIV   - (input) INTEGER array on the GPU, dimension (M)
-             The vector of pivot indices. Row i of A is cast to single 
-             precision in row ipiv[i] of SA, for 0 <= i < m. 
-
-    INCX   - (input) INTEGER
-             If IPIV is negative, the pivots are applied in reverse 
-             order, otherwise in straight-forward order.
+    @param[in]
+    m       The number of rows to be interchanged.
+    
+    @param[in]
+    ipiv    INTEGER array on the GPU, dimension (M)
+            The vector of pivot indices. Row i of A is cast to single 
+            precision in row ipiv[i] of SA, for 0 <= i < m. 
+    
+    @param[in]
+    incx    INTEGER
+            If IPIV is negative, the pivots are applied in reverse 
+            order, otherwise in straight-forward order.
 
     @ingroup magma_zaux2
     ********************************************************************/
 extern "C" void
-magmablas_zclaswp( magma_int_t n, magmaDoubleComplex *a, magma_int_t lda,
-                   magmaFloatComplex *sa, magma_int_t m,
+magmablas_zclaswp( magma_int_t n, magmaDoubleComplex *A, magma_int_t lda,
+                   magmaFloatComplex *SA, magma_int_t m,
                    const magma_int_t *ipiv, magma_int_t incx )
 {
     int blocks = (m - 1)/NB + 1;
@@ -94,7 +101,7 @@ magmablas_zclaswp( magma_int_t n, magmaDoubleComplex *a, magma_int_t lda,
     dim3 threads(NB, 1, 1);
 
     if (incx >= 0)
-        zclaswp_kernel<<< grid, threads, 0, magma_stream >>>(n, a, lda, sa, m, ipiv);
+        zclaswp_kernel<<< grid, threads, 0, magma_stream >>>(n, A, lda, SA, m, ipiv);
     else
-        zclaswp_inv_kernel<<< grid, threads, 0, magma_stream >>>(n, a, lda, sa, m, ipiv);
+        zclaswp_inv_kernel<<< grid, threads, 0, magma_stream >>>(n, A, lda, SA, m, ipiv);
 }
