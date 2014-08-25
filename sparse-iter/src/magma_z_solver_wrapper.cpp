ba25b1dfb2235b3c5e72bd3393eace2979327697
Hartwig Anzt <hanzt@icl.utk.edu>
2014-08-25 17:01:16 +0000

    make the solver choosable
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@5371 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ sparse-iter/src/magma_z_solver_wrapper.cpp
@@ -0,0 +1,83 @@
+/*
+    -- MAGMA (version 1.1) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       @date
+
+       @precisions normal z -> c d s
+       @author Hartwig Anzt
+
+*/
+#include "common_magma.h"
+#include "../include/magmasparse.h"
+
+
+
+
+/**
+    Purpose
+    -------
+
+    ALlows the user to choose a solver.
+
+    Arguments
+    ---------
+
+    @param
+    A           magma_z_sparse_matrix
+                sparse matrix A    
+
+    @param
+    b           magma_z_vector
+                input vector b     
+
+    @param
+    x           magma_z_vector*
+                output vector x        
+
+    @param
+    precond     magma_z_preconditioner
+                preconditioner
+
+    @ingroup magmasparse_zaux
+    ********************************************************************/
+
+magma_int_t
+magma_z_solver( magma_z_sparse_matrix A, magma_z_vector b, 
+                 magma_z_vector *x, magma_zopts *zopts ){
+
+
+        // preconditioner
+        magma_z_precondsetup( A, b, &zopts->precond_par );
+
+            switch( zopts->solver_par.solver ) {
+                case  Magma_CG:
+                        magma_zcg_res( A, b, x, &zopts->solver_par );break;
+                case  Magma_CGMERGE:
+                        magma_zcg_merge( A, b, x, &zopts->solver_par );break;
+                case  Magma_PCG:
+                        magma_zpcg( A, b, x, &zopts->solver_par, &zopts->precond_par );break;
+                case  Magma_BICGSTAB:
+                        magma_zbicgstab( A, b, x, &zopts->solver_par );break;
+                case  Magma_BICGSTABMERGE: 
+                        magma_zbicgstab_merge( A, b, x, &zopts->solver_par );break;
+                case  Magma_PBICGSTAB: 
+                        magma_zpbicgstab( A, b, x, &zopts->solver_par, &zopts->precond_par );break;
+                case  Magma_GMRES: 
+                        magma_zgmres( A, b, x, &zopts->solver_par );break;
+                case  Magma_PGMRES: 
+                        magma_zpgmres( A, b, x, &zopts->solver_par, &zopts->precond_par );break;
+                case  Magma_LOBPCG: 
+                        magma_zlobpcg( A, &zopts->solver_par );break;
+                case  Magma_ITERREF: 
+                        magma_ziterref( A, b, x, &zopts->solver_par, &zopts->precond_par );break;
+                case  Magma_JACOBI: 
+                        magma_zjacobi( A, b, x, &zopts->solver_par );break;
+                case  Magma_BAITER: 
+                        magma_zbaiter( A, b, x, &zopts->solver_par );break;
+            }
+    return MAGMA_SUCCESS;
+}
+
+
