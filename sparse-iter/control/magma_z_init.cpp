c671cfb9ced31c66de9c84071a4d1cabc3b774a3
Stanimire Tomov <tomov@icl.utk.edu>
2014-08-29 00:04:13 +0000

    use magma zlaset to set the values directly on the GPU
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@5421 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/control/magma_z_init.cpp
+++ sparse-iter/control/magma_z_init.cpp
@@ -84,20 +84,10 @@ magma_z_vinit(    magma_z_vector *x,
     else if( mem_loc == Magma_DEV ){
         x->memory_location = Magma_DEV;
 
-        magmaDoubleComplex *tmp;
-
-        magma_zmalloc_cpu( &tmp, num_rows );
-        if ( tmp == NULL )
-            return MAGMA_ERR_HOST_ALLOC;
-        for( magma_int_t i=0; i<num_rows; i++)
-             tmp[i] = values; 
-
         if (MAGMA_SUCCESS != magma_zmalloc( &x->val, x->num_rows)) 
             return MAGMA_ERR_DEVICE_ALLOC;
 
-        // data transfer
-        magma_zsetvector( x->num_rows, tmp, 1, x->val, 1 );
-        magma_free_cpu(tmp);
+        magmablas_zlaset(MagmaFull, num_rows, 1, values, values, x->val, num_rows);
 
         return MAGMA_SUCCESS; 
     }
