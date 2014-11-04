9352a7a9f516061c5dc40f5a5b9dcfc35ea461f5
Ichi Yamazaki <iyamazak@utk.edu>
2014-11-04 15:39:09 +0000

    Finish fxing the LAPACK definition for Bunch-Kauffman (sorry)..
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@5749 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- include/magma_zlapack.h
+++ include/magma_zlapack.h
@@ -524,8 +524,8 @@ void   lapackf77_zgetrs( const char *trans,
                          magmaDoubleComplex *B, const magma_int_t *ldb,
                          magma_int_t *info );
 
-void   lapackf77_zhetf2( char*, magma_int_t*, 
-                         magmaDoubleComplex*, int*, int*, int* );
+void   lapackf77_zhetf2( const char*, magma_int_t*, 
+                         magmaDoubleComplex*, magma_int_t*, magma_int_t*, magma_int_t* );
 
 void   lapackf77_zhetrs( const char *uplo,
                          const magma_int_t *n, const magma_int_t *nrhs,
