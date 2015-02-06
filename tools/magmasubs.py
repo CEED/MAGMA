d2acc9e17f5199f298e66284c86c65a44dc235e2
Mark Gates <mgates3@icl.utk.edu>
2015-02-06 19:05:41 +0000

    use spaces, not tabs
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@6373 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- tools/magmasubs.py
+++ tools/magmasubs.py
@@ -550,7 +550,7 @@ subs = {
     ('silu',           'dilu',           'cilu',           'zilu'            ),
     ('sailu',          'dailu',          'cailu',          'zailu'           ),
     ('scuilu',         'dcuilu',         'ccuilu',         'zcuilu'          ),
-	('scumilu',        'dcumilu',        'ccumilu',        'zcumilu'         ),
+    ('scumilu',        'dcumilu',        'ccumilu',        'zcumilu'         ),
     ('sbailu',         'dbailu',         'cbailu',         'zbailu'          ),
     ('spastix',        'dpastix',        'cpastix',        'zpastix'         ),
     ('slobpcg',        'dlobpcg',        'clobpcg',        'zlobpcg'         ),
