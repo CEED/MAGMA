0130d2ca9c3c9cac181ec185eaf8c599c5a54abc
Tingxing Dong <tdong@vols.utk.edu>
2015-04-28 21:22:47 +0000

    delete zgemv_batched.cu file and put the batched in fermi.cu
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@6618 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- magmablas/zgemv_fermi.cu
+++ magmablas/zgemv_fermi.cu
@@ -24,14 +24,14 @@
  * This simple implementation loads x directly, relying on the cache,
  * without using shared memory.
  */
-__global__ void
-zgemvn_kernel1_fermi(
+static __device__ void
+zgemvn_device(
     int m, int n, magmaDoubleComplex alpha,
     const magmaDoubleComplex * __restrict__ A, int lda,
     const magmaDoubleComplex * __restrict__ x, int incx, magmaDoubleComplex beta,
     magmaDoubleComplex       * __restrict__ y, int incy)
 {
-#if (__CUDA_ARCH__ >= 200)
+
     int ind = blockIdx.x*BLK_X + threadIdx.x;
     if ( ind < m ) {
         A += ind;
@@ -45,88 +45,51 @@ zgemvn_kernel1_fermi(
         
         y[ind*incy] = alpha*res + beta*y[ind*incy];
     }
-#endif /* (__CUDA_ARCH__ >= 200) */
+
 }
 
-
-/* Compute y = alpha*A*x + beta*y.
- * Each thread block does a BLK_X x N block row of A.
- * Each thread goes across one row, accumulating dot product of row ind and x into res.
- * This implementation loads BLK_Y elements into sx, then multiplies
- * BLK_Y columns of A*sx.
- */
 __global__ void
-zgemvn_kernel2_fermi(
+zgemvn_kernel_fermi(
     int m, int n, magmaDoubleComplex alpha,
     const magmaDoubleComplex * __restrict__ A, int lda,
     const magmaDoubleComplex * __restrict__ x, int incx, magmaDoubleComplex beta,
     magmaDoubleComplex       * __restrict__ y, int incy)
 {
 #if (__CUDA_ARCH__ >= 200)
-    int ind = blockIdx.x*BLK_X + threadIdx.x;
-    
-    // threads past last row redundantly work on last row
-    A += min( ind, m-1 );
-    x += threadIdx.x*incx;
-    
-    magmaDoubleComplex res = MAGMA_Z_ZERO;
-    
-    __shared__ magmaDoubleComplex sx[BLK_Y];
-    
-    // full block-columns
-    int nfull = (n / BLK_Y) * BLK_Y;
-    for( int j=0; j < nfull; j += BLK_Y ) {
-        // load BLK_Y elements of x into sx
-        sx[threadIdx.x] = x[0];
-        x += BLK_Y*incx;
-        __syncthreads();
-        
-        // multiply A*sx
-        #pragma unroll
-        for(int j2=0; j2 < BLK_Y; j2++) {
-            res += A[0] * sx[j2];
-            A += lda;
-        }
-        __syncthreads();
-    }
-    
-    // last, partial block-column
-    // load remaining npart elements of x into sx
-    int npart = n % BLK_Y;
-    if ( threadIdx.x < npart ) {
-        sx[threadIdx.x] = x[0];
-    }
-    else {
-        sx[threadIdx.x] = MAGMA_Z_ZERO;
-    }
-    __syncthreads();
-        
-    // multiply A*sx
-    #pragma unroll
-    for(int j2=0; j2 < npart; j2++) {
-        res += A[0]*sx[j2];
-        A += lda;
-    }
-    
-    if ( ind < m ) {
-        y[ind*incy] = alpha*res + beta*y[ind*incy];
-    }
+    zgemvn_device(m, n, alpha, A, lda, x, incx, beta, y, incy);
 #endif /* (__CUDA_ARCH__ >= 200) */
 }
+
+
+
+__global__ void
+zgemvn_kernel_batched(
+    int m, int n, magmaDoubleComplex alpha,
+    magmaDoubleComplex **A_array, int lda,
+    magmaDoubleComplex **x_array,  int incx,
+    magmaDoubleComplex beta, magmaDoubleComplex  **y_array, int incy)
+{
+    int batchid = blockIdx.z;
+
+    zgemvn_device(m, n, alpha, A_array[batchid], lda, x_array[batchid], incx, beta, y_array[batchid], incy);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
 
 
 /* Compute y = alpha * A^T * x + beta*y.
  * Each thread block does one column of A (i.e., one row of A^T).
  * Each thread does a partial sum, then collectively they do a reduction.
  */
-__global__ void
-zgemvt_kernel_fermi(
+static __device__ void
+zgemvt_device(
     int m, int n, magmaDoubleComplex alpha,
     const magmaDoubleComplex * __restrict__ A, int lda,
     const magmaDoubleComplex * __restrict__ x, int incx, magmaDoubleComplex beta,
     magmaDoubleComplex       * __restrict__ y, int incy)
 {
-#if (__CUDA_ARCH__ >= 200)
+
     int tx = threadIdx.x;
 
     __shared__ magmaDoubleComplex sdata[BLK_X];
@@ -152,21 +115,51 @@ zgemvt_kernel_fermi(
     if ( tx == 0 ) {
         y[blockIdx.y*incy] = alpha*sdata[0] + beta*y[blockIdx.y*incy];
     }
-#endif /* (__CUDA_ARCH__ >= 200) */
+
 }
 
 
-/* Compute y = alpha * A^H * x + beta*y.
- * Same as zgemvt_kernel_fermi but conjugates entries of A.
- */
+
 __global__ void
-zgemvc_kernel_fermi(
+zgemvt_kernel_fermi(
     int m, int n, magmaDoubleComplex alpha,
     const magmaDoubleComplex * __restrict__ A, int lda,
     const magmaDoubleComplex * __restrict__ x, int incx, magmaDoubleComplex beta,
     magmaDoubleComplex       * __restrict__ y, int incy)
 {
 #if (__CUDA_ARCH__ >= 200)
+    zgemvt_device(m, n, alpha, A, lda, x, incx, beta, y, incy);
+#endif /* (__CUDA_ARCH__ >= 200) */
+}
+
+
+
+__global__ void
+zgemvt_kernel_batched(
+    int m, int n, magmaDoubleComplex alpha,
+    magmaDoubleComplex **A_array, int lda,
+    magmaDoubleComplex **x_array,  int incx,
+    magmaDoubleComplex beta, magmaDoubleComplex  **y_array, int incy)
+{
+    int batchid = blockIdx.z;
+
+    zgemvt_device(m, n, alpha, A_array[batchid], lda, x_array[batchid], incx, beta, y_array[batchid], incy);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+/* Compute y = alpha * A^H * x + beta*y.
+ * Same as zgemvt_kernel_fermi but conjugates entries of A.
+ */
+static __device__ void
+zgemvc_device(
+    int m, int n, magmaDoubleComplex alpha,
+    const magmaDoubleComplex * __restrict__ A, int lda,
+    const magmaDoubleComplex * __restrict__ x, int incx, magmaDoubleComplex beta,
+    magmaDoubleComplex       * __restrict__ y, int incy)
+{
+
     int tx = threadIdx.x;
 
     __shared__ magmaDoubleComplex sdata[BLK_X];
@@ -192,9 +185,37 @@ zgemvc_kernel_fermi(
     if ( tx == 0 ) {
         y[blockIdx.y*incy] = alpha*sdata[0] + beta*y[blockIdx.y*incy];
     }
+
+}
+
+__global__ void
+zgemvc_kernel_fermi(
+    int m, int n, magmaDoubleComplex alpha,
+    const magmaDoubleComplex * __restrict__ A, int lda,
+    const magmaDoubleComplex * __restrict__ x, int incx, magmaDoubleComplex beta,
+    magmaDoubleComplex       * __restrict__ y, int incy)
+{
+#if (__CUDA_ARCH__ >= 200)
+    zgemvc_device(m, n, alpha, A, lda, x, incx, beta, y, incy);
 #endif /* (__CUDA_ARCH__ >= 200) */
 }
 
+
+
+__global__ void
+zgemvc_kernel_batched(
+    int m, int n, magmaDoubleComplex alpha,
+    magmaDoubleComplex **A_array, int lda,
+    magmaDoubleComplex **x_array,  int incx,
+    magmaDoubleComplex beta, magmaDoubleComplex  **y_array, int incy)
+{
+    int batchid = blockIdx.z;
+
+    zgemvc_device(m, n, alpha, A_array[batchid], lda, x_array[batchid], incx, beta, y_array[batchid], incy);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
 
 /**
     Purpose
@@ -305,9 +326,9 @@ magmablas_zgemv(
     // --------------------
     // CUDA ARCH 2.x (Fermi) version
     if ( trans == MagmaNoTrans ) {
-        dim3 grid( magma_ceildiv( m, BLK_X ) );
+        dim3 grid( (m - 1)/BLK_X + 1 );
         dim3 threads( BLK_X, 1, 1 );
-        zgemvn_kernel1_fermi<<< grid, threads, 0, magma_stream >>>
+        zgemvn_kernel_fermi<<< grid, threads, 0, magma_stream >>>
             ( m, n, alpha, dA, ldda, dx, incx, beta, dy, incy );
     }
     else if ( trans == MagmaTrans ) {
@@ -321,5 +342,58 @@ magmablas_zgemv(
         dim3 threads ( BLK_X, 1, 1 );
         zgemvc_kernel_fermi<<< grid, threads, 0, magma_stream >>>
             ( m, n, alpha, dA, ldda, dx, incx, beta, dy, incy );
+    }
+}
+
+
+
+extern "C" void
+magmablas_zgemv_batched(
+    magma_trans_t trans, magma_int_t m, magma_int_t n, 
+    magmaDoubleComplex alpha,
+    magmaDoubleComplex_ptr dA_array[], magma_int_t ldda, 
+    magmaDoubleComplex_ptr dx_array[], magma_int_t incx,
+    magmaDoubleComplex beta,
+    magmaDoubleComplex_ptr dy_array[], magma_int_t incy, 
+    magma_int_t batchCount, magma_queue_t queue)
+{
+    magma_int_t info = 0;
+    if ( trans != MagmaNoTrans && trans != MagmaTrans && trans != MagmaConjTrans )
+        info = -1;
+    else if ( m < 0 )
+        info = -2;
+    else if ( n < 0 )
+        info = -3;
+    else if ( ldda < m )
+        info = -6;
+    else if ( incx == 0 )
+        info = -8;
+    else if ( incy == 0 )
+        info = -11;
+    
+    if (info != 0) {
+        magma_xerbla( __func__, -(info) );
+        return;  //info;
+    }    
+
+
+    // --------------------
+    if ( trans == MagmaNoTrans ) {
+        dim3 grid( (m - 1)/BLK_X + 1, 1, batchCount );
+        dim3 threads( BLK_X, 1, 1 );
+        zgemvn_kernel_batched<<< grid, threads, 0, queue >>>
+            ( m, n, alpha, dA_array, ldda, dx_array, incx, beta, dy_array, incy );
+    }
+    else if ( trans == MagmaTrans ) {
+        dim3 grid    ( 1, n, batchCount );
+        dim3 threads ( BLK_X, 1, 1 );
+        zgemvt_kernel_batched<<< grid, threads, 0, queue >>>
+            ( m, n, alpha, dA_array, ldda, dx_array, incx, beta, dy_array, incy );
+    }
+    else if ( trans == MagmaConjTrans ) {
+        dim3 grid    ( 1, n, batchCount );
+        dim3 threads ( BLK_X, 1, 1 );
+        zgemvc_kernel_batched<<< grid, threads, 0, queue >>>
+            ( m, n, alpha, dA_array, ldda, dx_array, incx, beta, dy_array, incy );
     }
 }
