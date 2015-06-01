cb0bff62e78d44f5e3c2abbe3041c98a7b70d9c0
Hartwig Anzt <hanzt@icl.utk.edu>
2015-06-01 20:03:49 +0000

    added the stream version
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@6927 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/include/magmasparse_z.h
+++ sparse-iter/include/magmasparse_z.h
@@ -640,6 +640,12 @@ magma_zidr_acc(
     magma_queue_t queue );
 
 magma_int_t
+magma_zidr_strm(
+    magma_z_matrix A, magma_z_matrix b, 
+    magma_z_matrix *x, magma_z_solver_par *solver_par,
+    magma_queue_t queue );
+
+magma_int_t
 magma_zpidr(
     magma_z_matrix A, magma_z_matrix b,
     magma_z_matrix *x, magma_z_solver_par *solver_par,
