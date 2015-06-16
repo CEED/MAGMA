8f2be1a8c6c70663c76533d990ef3def7d8315bb
Mark Gates <mgates3@icl.utk.edu>
2015-06-16 19:33:22 +0000

    use snprintf instead of sprintf
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@6992 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- control/affinity.cpp
+++ control/affinity.cpp
@@ -19,26 +19,31 @@ affinity_set::affinity_set()
     CPU_ZERO(&set);
 }
 
+
 affinity_set::affinity_set(int cpu_nr)
 {
     CPU_ZERO(&set);
     CPU_SET(cpu_nr, &set);
 }
 
+
 void affinity_set::add(int cpu_nr)
 {
     CPU_SET(cpu_nr, &set);
 }
+
 
 int affinity_set::get_affinity()
 {
     return sched_getaffinity( 0, sizeof(set), &set);
 }
 
+
 int affinity_set::set_affinity()
 {
     return sched_setaffinity( 0, sizeof(set), &set);
 }
+
 
 void affinity_set::print_affinity(int id, const char* s)
 {
@@ -48,26 +53,28 @@ void affinity_set::print_affinity(int id, const char* s)
         printf("Error in sched_getaffinity\n");
 }
 
+
 void affinity_set::print_set(int id, const char* s)
 {
 // CPU_COUNT() first appeared in glibc 2.6.
 #if __GLIBC_PREREQ(2,6)
     char cpustring[1024];
 
-    int cpu_count=CPU_COUNT(&set);
+    int cpu_count = CPU_COUNT(&set);
     int charcnt = 0;
 
-    charcnt = sprintf(cpustring,"thread %d has affinity with %d CPUS: ", id, cpu_count);
+    charcnt = snprintf( cpustring, sizeof(cpustring), "thread %d has affinity with %d CPUS: ", id, cpu_count);
 
-    int nrcpu=0;
+    int nrcpu = 0;
 
-    for(int icpu=0; nrcpu<cpu_count && icpu<CPU_SETSIZE; ++icpu){
-        if(CPU_ISSET(icpu,&set)){
-            charcnt += sprintf(&(cpustring[charcnt]),"%d,",icpu);
+    for(int icpu=0; nrcpu < cpu_count && icpu < CPU_SETSIZE; ++icpu) {
+        if( CPU_ISSET( icpu, &set )) {
+            charcnt += snprintf( &(cpustring[charcnt]), sizeof(cpustring)-charcnt, "%d,", icpu );
             ++nrcpu;
         }
     }
-    charcnt += sprintf(&(cpustring[charcnt-1]),"\n") - 1; // -1 is used to remove "," after last cpu.
+    // charcnt-1 is used to remove "," after last cpu.
+    charcnt += snprintf( &(cpustring[charcnt-1]), sizeof(cpustring)-(charcnt-1), "\n" ) - 1;
     printf("%s: %s", s, cpustring);
     fflush(stdout);
 #endif
