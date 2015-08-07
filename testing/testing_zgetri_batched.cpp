ce301fee196a03d30db0044400a8cb7e4dd9ee38
Azzam Haidar <haidar@icl.utk.edu>
2015-08-07 12:35:09 +0000

    fixing getri to have square matrix
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@7291 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- testing/testing_zgetri_batched.cpp
+++ testing/testing_zgetri_batched.cpp
@@ -21,6 +21,10 @@
 #include "magma_lapack.h"
 #include "testings.h"
 
+#if defined(_OPENMP)
+#include <omp.h>
+#include "magma_threadsetting.h"
+#endif
 
 /* ////////////////////////////////////////////////////////////////////////////
    -- Testing zgetrf_batched
@@ -40,7 +44,7 @@ int main( int argc, char** argv)
 
     magma_int_t     *ipiv, *cpu_info;
     magma_int_t     *d_ipiv, *d_info;
-    magma_int_t M, N, n2, lda, ldda, min_mn, info, info1, info2;
+    magma_int_t N, n2, lda, ldda, info, info1, info2;
     magma_int_t ione     = 1;
     magma_int_t ISEED[4] = {0,0,0,1};
     magma_opts opts;
@@ -60,26 +64,24 @@ int main( int argc, char** argv)
     magma_queue_t queue = opts.queue; //NULL; // The batched routine prefer stream NULL
 
 
-    printf("%% batchCount    M     N     CPU GFlop/s (ms)    GPU GFlop/s (ms)    ||PA-LU||/(||A||*N    tolerance )\n");
+    printf("%% batchCount    N     N     CPU GFlop/s (ms)    GPU GFlop/s (ms)    ||PA-LU||/(||A||*N    tolerance )\n");
     printf("%%===================================================================================================\n");
     for( int i = 0; i < opts.ntest; ++i ) {    
         for( int iter = 0; iter < opts.niter; ++iter ) {
-            M = opts.msize[i];
             N = opts.nsize[i];
-            min_mn = min(M, N);
-            lda    = M;
+            lda    = N;
             n2     = lda*N * batchCount;
-            ldda   = magma_roundup( M, opts.align );  // multiple of 32 by default
-            //gflops = (FLOPS_ZGETRF( M, N ) + FLOPS_ZGETRI( min(M,N) ))/ 1e9 * batchCount; // This is the correct flops but since this getri_batched is based on 2 trsm = getrs and to know the real flops I am using the getrs one
-            gflops = (FLOPS_ZGETRF( M, N ) + FLOPS_ZGETRS( min(M,N), min(M,N) ))/ 1e9 * batchCount;
+            ldda   = magma_roundup( N, opts.align );  // multiple of 32 by default
+            //gflops = (FLOPS_ZGETRF( N, N ) + FLOPS_ZGETRI( N ))/ 1e9 * batchCount; // This is the correct flops but since this getri_batched is based on 2 trsm = getrs and to know the real flops I am using the getrs one
+            gflops = (FLOPS_ZGETRF( N, N ) + FLOPS_ZGETRS( N, N ))/ 1e9 * batchCount;
 
             TESTING_MALLOC_CPU( cpu_info, magma_int_t, batchCount);
-            TESTING_MALLOC_CPU(  ipiv, magma_int_t,     min_mn * batchCount);
+            TESTING_MALLOC_CPU(  ipiv, magma_int_t,     N * batchCount);
             TESTING_MALLOC_CPU(  h_A,  magmaDoubleComplex, n2     );
             TESTING_MALLOC_PIN(  h_R,  magmaDoubleComplex, n2     );
             TESTING_MALLOC_DEV(  d_A,  magmaDoubleComplex, ldda*N * batchCount);
             TESTING_MALLOC_DEV(  d_invA,  magmaDoubleComplex, ldda*N * batchCount);
-            TESTING_MALLOC_DEV(  d_ipiv,  magma_int_t, min_mn * batchCount);
+            TESTING_MALLOC_DEV(  d_ipiv,  magma_int_t, N * batchCount);
             TESTING_MALLOC_DEV(  d_info,  magma_int_t, batchCount);
 
             magma_malloc((void**)&dA_array, batchCount * sizeof(*dA_array));
@@ -91,19 +93,19 @@ int main( int argc, char** argv)
             /* Initialize the matrix */
             lapackf77_zlarnv( &ione, ISEED, &n2, h_A );
             columns = N * batchCount;
-            lapackf77_zlacpy( MagmaUpperLowerStr, &M, &columns, h_A, &lda, h_R, &lda );
-            magma_zsetmatrix( M, columns, h_R, lda, d_A, ldda );
+            lapackf77_zlacpy( MagmaUpperLowerStr, &N, &columns, h_A, &lda, h_R, &lda );
+            magma_zsetmatrix( N, columns, h_R, lda, d_A, ldda );
 
             /* ====================================================================
                Performs operation using MAGMA
                =================================================================== */
             zset_pointer(dA_array, d_A, ldda, 0, 0, ldda * N, batchCount, queue);
             zset_pointer(dinvA_array, d_invA, ldda, 0, 0, ldda * N, batchCount, queue);
-            set_ipointer(dipiv_array, d_ipiv, 1, 0, 0, min(M,N), batchCount, queue);
+            set_ipointer(dipiv_array, d_ipiv, 1, 0, 0, N, batchCount, queue);
 
             gpu_time = magma_sync_wtime(queue);
-            info1 = magma_zgetrf_batched( M, N, dA_array, ldda, dipiv_array, dinfo_array, batchCount, queue);
-            info2 = magma_zgetri_outofplace_batched( min(M,N), dA_array, ldda, dipiv_array, dinvA_array, ldda, dinfo_array, batchCount, opts.queue);
+            info1 = magma_zgetrf_batched( N, N, dA_array, ldda, dipiv_array, dinfo_array, batchCount, queue);
+            info2 = magma_zgetri_outofplace_batched( N, dA_array, ldda, dipiv_array, dinvA_array, ldda, dinfo_array, batchCount, opts.queue);
             gpu_time = magma_sync_wtime(queue) - gpu_time;
             gpu_perf = gflops / gpu_time;
 
@@ -131,19 +133,29 @@ int main( int argc, char** argv)
                            (int) info, magma_strerror( info ));
                 lwork = magma_int_t( MAGMA_Z_REAL( tmp ));
                 TESTING_MALLOC_CPU( work,  magmaDoubleComplex, lwork  );
-                lapackf77_zlacpy( MagmaUpperLowerStr, &M, &columns, h_R, &lda, h_A, &lda );
+                lapackf77_zlacpy( MagmaUpperLowerStr, &N, &columns, h_R, &lda, h_A, &lda );
                 cpu_time = magma_wtime();
+                #if !defined (BATCHED_DISABLE_PARCPU) && defined(_OPENMP)
+                magma_int_t nthreads = magma_get_lapack_numthreads();
+                magma_set_lapack_numthreads(1);
+                magma_set_omp_numthreads(nthreads);
+                #pragma omp parallel for schedule(dynamic)
+                #endif
                 for (int i=0; i < batchCount; i++)
                 {
-                    lapackf77_zgetrf(&M, &N, h_A + i * lda*N, &lda, ipiv + i * min_mn, &info);
-                    if (info != 0)
+                    magma_int_t locinfo;
+                    lapackf77_zgetrf(&N, &N, h_A + i * lda*N, &lda, ipiv + i * N, &locinfo);
+                    if (locinfo != 0)
                         printf("lapackf77_zgetrf returned error %d: %s.\n",
-                           (int) info, magma_strerror( info ));
-                    lapackf77_zgetri(&N, h_A + i * lda*N, &lda, ipiv + i * min_mn, work, &lwork, &info );
-                    if (info != 0)
+                           (int) locinfo, magma_strerror( locinfo ));
+                    lapackf77_zgetri(&N, h_A + i * lda*N, &lda, ipiv + i * N, work, &lwork, &locinfo );
+                    if (locinfo != 0)
                         printf("lapackf77_zgetri returned error %d: %s.\n",
-                           (int) info, magma_strerror( info ));
+                           (int) locinfo, magma_strerror( locinfo ));
                 }
+                #if !defined (BATCHED_DISABLE_PARCPU) && defined(_OPENMP)
+                    magma_set_lapack_numthreads(nthreads);
+                #endif
                 cpu_time = magma_wtime() - cpu_time;
                 cpu_perf = gflops / cpu_time;
             }
@@ -152,25 +164,25 @@ int main( int argc, char** argv)
                =================================================================== */
             if ( opts.lapack ) {
                 printf("%10d %6d %6d   %7.2f (%7.2f)   %7.2f (%7.2f)",
-                       (int) batchCount, (int) M, (int) N, cpu_perf, cpu_time*1000., gpu_perf, gpu_time*1000. );
+                       (int) batchCount, (int) N, (int) N, cpu_perf, cpu_time*1000., gpu_perf, gpu_time*1000. );
             }
             else {
                 printf("%10d %6d %6d     ---   (  ---  )   %7.2f (%7.2f)",
-                       (int) batchCount, (int) M, (int) N, gpu_perf, gpu_time*1000. );
+                       (int) batchCount, (int) N, (int) N, gpu_perf, gpu_time*1000. );
             }
 
             double err = 0.0;
             if ( opts.check ) {
-                magma_getvector( min_mn * batchCount, sizeof(magma_int_t), d_ipiv, 1, ipiv, 1 );
-                magma_zgetmatrix( min(M,N), N*batchCount, d_invA, ldda, h_R, lda );
+                magma_getvector( N * batchCount, sizeof(magma_int_t), d_ipiv, 1, ipiv, 1 );
+                magma_zgetmatrix( N, N*batchCount, d_invA, ldda, h_R, lda );
                 int stop=0;
                 n2     = lda*N;
                 for (int i=0; i < batchCount; i++)
                 {
-                    for (int k=0; k < min_mn; k++) {
-                        if (ipiv[i*min_mn+k] < 1 || ipiv[i*min_mn+k] > M )
+                    for (int k=0; k < N; k++) {
+                        if (ipiv[i*N+k] < 1 || ipiv[i*N+k] > N )
                         {
-                            printf("error for matrix %d ipiv @ %d = %d\n", (int) i, (int) k, (int) ipiv[i*min_mn+k]);
+                            printf("error for matrix %d ipiv @ %d = %d\n", (int) i, (int) k, (int) ipiv[i*N+k]);
                             stop=1;
                         }
                     }
