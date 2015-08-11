d86b6f903209347ed40a55fd1fbb388bbb585b36
Mark Gates <mgates3@icl.utk.edu>
2015-08-11 13:58:39 +0000

    add magma_cabs, used in clMAGMA
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@7345 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ control/abs.cpp
@@ -0,0 +1,53 @@
+#include "common_magma.h"
+
+// ========================================
+// complex support
+
+// --------------------
+// propagates inf and nan correctly:
+//     abs( [xxx, nan] ) == nan, for any xxx
+//     abs( [nan, xxx] ) == nan, for any xxx
+//     abs( [inf, xxx] ) == inf, for any xxx except nan
+//     abs( [xxx, inf] ) == inf, for any xxx except nan
+extern "C" double
+magma_cabs(magmaDoubleComplex z)
+{
+    float x = abs( real( z ));
+    float y = abs( imag( z ));
+    float big, small;
+    if ( x > y ) {
+        big   = x;
+        small = y;
+    }
+    else {
+        big   = y;
+        small = x;
+    }
+    if ( big == 0 || isinf(big) ) {
+        return big + small;  // add to propagate nan
+    }
+    small /= big;
+    return big * sqrt( 1 + small*small );
+}
+
+// --------------------
+extern "C" float
+magma_cabsf(magmaFloatComplex z)
+{
+    float x = abs( real( z ));
+    float y = abs( imag( z ));
+    float big, small;
+    if ( x > y ) {
+        big   = x;
+        small = y;
+    }
+    else {
+        big   = y;
+        small = x;
+    }
+    if ( big == 0 || isinf(big) ) {
+        return big + small;  // add to propagate nan
+    }
+    small /= big;
+    return big * sqrt( 1 + small*small );
+}
