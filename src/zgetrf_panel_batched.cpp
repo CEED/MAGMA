893fc63143b10c0499ca6b5e5c11882d1dbb149d
Azzam Haidar <haidar@icl.utk.edu>
2015-08-25 20:01:18 +0000

    adding more documentation to the internal routine
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@7456 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zgetrf_panel_batched.cpp
+++ src/zgetrf_panel_batched.cpp
@@ -12,12 +12,84 @@
 */
 
 #include "common_magma.h"
-
 /**
     Purpose
     -------
-    this is an internal routine that might have many assumption.
-    No documentation is available today.
+    This is an internal routine that might have many assumption.
+    Documentation is not fully completed
+
+    ZGETRF_PANEL computes an LU factorization of a general M-by-N matrix A
+    using partial pivoting with row interchanges.
+
+    The factorization has the form
+        A = P * L * U
+    where P is a permutation matrix, L is lower triangular with unit
+    diagonal elements (lower trapezoidal if m > n), and U is upper
+    triangular (upper trapezoidal if m < n).
+
+    This is the right-looking Level 3 BLAS version of the algorithm.
+
+    This is a batched version that factors batchCount M-by-N matrices in parallel.
+    dA, ipiv, and info become arrays with one entry per matrix.
+
+    Arguments
+    ---------
+    @param[in]
+    m       INTEGER
+            The number of rows of each matrix A.  M >= 0.
+
+    @param[in]
+    n       INTEGER
+            The number of columns of each matrix A.  N >= 0.
+
+    @param[in,out]
+    dA_array    Array of pointers, dimension (batchCount).
+            Each is a COMPLEX_16 array on the GPU, dimension (LDDA,N).
+            On entry, each pointer is an M-by-N matrix to be factored.
+            On exit, the factors L and U from the factorization
+            A = P*L*U; the unit diagonal elements of L are not stored.
+
+    @param[in]
+    ldda    INTEGER
+            The leading dimension of each array A.  LDDA >= max(1,M).
+
+    @param[out]
+    dipiv_array  Array of pointers, dimension (batchCount), for corresponding matrices.
+            Each is an INTEGER array, dimension (min(M,N))
+            The pivot indices; for 1 <= i <= min(M,N), row i of the
+            matrix was interchanged with row IPIV(i).
+
+    @param[out]
+    dpivinfo_array  Array of pointers, dimension (batchCount), for internal use.
+
+    @param[in]
+    dW1_displ  Workspace array of pointers, for internal use.
+
+    @param[out]
+    info_array  Array of INTEGERs, dimension (batchCount), for corresponding matrices.
+      -     = 0:  successful exit
+      -     < 0:  if INFO = -i, the i-th argument had an illegal value
+                  or another error occured, such as memory allocation failed.
+      -     > 0:  if INFO = i, U(i,i) is exactly zero. The factorization
+                  has been completed, but the factor U is exactly
+                  singular, and division by zero will occur if it is used
+                  to solve a system of equations.
+
+    @param[in]
+    gbstep  INTEGER
+            internal use.
+
+    @param[in]
+    batchCount  INTEGER
+                The number of matrices to operate on.
+
+    @param[in]
+    myhandle   Cublas_Handle_t
+            Cublas handle might be used internally.
+
+    @param[in]
+    queue   magma_queue_t
+            Queue to execute in.
 
     @ingroup magma_zgesv_comp
     ********************************************************************/
