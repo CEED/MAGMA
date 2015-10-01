ab47842b42db572e506f42ede0b8636b0e50dead
Hartwig Anzt <hanzt@icl.utk.edu>
2015-10-01 12:56:20 +0000

    need to commit as I have to continue development on another platform
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@7593 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/include/magmasparse_z.h
+++ sparse-iter/include/magmasparse_z.h
@@ -622,6 +622,13 @@ magma_zfgmres(
     magma_queue_t queue );
 
 magma_int_t
+magma_zbfgmres(
+    magma_z_matrix A, magma_z_matrix b, 
+    magma_z_matrix *x, magma_z_solver_par *solver_par, 
+    magma_z_preconditioner *precond_par,
+    magma_queue_t queue );
+
+magma_int_t
 magma_zidr(
     magma_z_matrix A, magma_z_matrix b, 
     magma_z_matrix *x, magma_z_solver_par *solver_par,
