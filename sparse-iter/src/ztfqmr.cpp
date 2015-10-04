91b2550fb9fa88067ba337bd327dd77e4999b8b9
Hartwig Anzt <hanzt@icl.utk.edu>
2015-10-04 13:49:39 +0000

    developments on lips
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@7644 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/src/ztfqmr.cpp
+++ sparse-iter/src/ztfqmr.cpp
@@ -142,6 +142,7 @@ magma_ztfqmr(
         magma_zaxpy(dofs, c_one, u.dval, 1, d.dval, 1);     // d = u + theta * theta / alpha * nu * d
         printf("tau = %.8e\n",tau);
         theta = magma_zsqrt( magma_zdotc(dofs, r.dval, 1, r_tld.dval, 1) ) / tau;
+            printf("tt = %.8e\n",magma_zdotc(dofs, r.dval, 1, r_tld.dval, 1));    
         c = c_one / magma_zsqrt( c_one + theta*theta );
         tau = tau * theta *c;
         nu = c * c * alpha;
