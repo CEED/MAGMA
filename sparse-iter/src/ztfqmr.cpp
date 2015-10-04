54ba0d0b0e8dcc95c7886d590a698dbca24b6626
Hartwig Anzt <hanzt@icl.utk.edu>
2015-10-04 12:39:50 +0000

    developments on lips
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@7626 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/src/ztfqmr.cpp
+++ sparse-iter/src/ztfqmr.cpp
@@ -79,9 +79,9 @@ magma_ztfqmr(
     magma_int_t dofs = A.num_rows* b.num_cols;
 
     // GPU workspace
-    magma_z_matrix r={Magma_CSR}, rt={Magma_CSR}, r_tld={Magma_CSR},
+    magma_z_matrix r={Magma_CSR}, rt={Magma_CSR}, r_tld={Magma_CSR}, t={Magma_CSR},
                     //p={Magma_CSR}, q={Magma_CSR}, u={Magma_CSR}, v={Magma_CSR},  t={Magma_CSR},
-                    d={Magma_CSR}, w={Magma_CSR}, u_hat={Magma_CSR}, v_hat={Magma_CSR};
+                    d={Magma_CSR}, w={Magma_CSR}, u={Magma_CSR}, v={Magma_CSR};
     CHECK( magma_zvinit( &r, Magma_DEV, A.num_rows, b.num_cols, c_zero, queue ));
     CHECK( magma_zvinit( &rt,Magma_DEV, A.num_rows, b.num_cols, c_zero, queue ));
     CHECK( magma_zvinit( &r_tld,Magma_DEV, A.num_rows, b.num_cols, c_zero, queue ));
