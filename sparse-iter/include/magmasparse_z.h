f27122756b73dfd965b02a34a67fe14adab1c605
Hartwig Anzt <hanzt@icl.utk.edu>
2015-10-04 19:27:46 +0000

    preconditioned TFQMR
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@7688 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/include/magmasparse_z.h
+++ sparse-iter/include/magmasparse_z.h
@@ -608,6 +608,13 @@ magma_ztfqmr(
     magma_queue_t queue );
 
 magma_int_t
+magma_zptfqmr(
+    magma_z_matrix A, magma_z_matrix b, magma_z_matrix *x,
+    magma_z_solver_par *solver_par,
+    magma_z_preconditioner *precond_par,
+    magma_queue_t queue );
+
+magma_int_t
 magma_zbicgstab(
     magma_z_matrix A, magma_z_matrix b, magma_z_matrix *x, 
     magma_z_solver_par *solver_par,
