8ec0d64fd797e487cf7e8a0185869a463a122399
Hartwig Anzt <hanzt@icl.utk.edu>
2015-10-05 17:38:56 +0000

    first batch for qmr
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@7729 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/src/zqmr.cpp
+++ sparse-iter/src/zqmr.cpp
@@ -55,7 +55,6 @@ extern "C" magma_int_t
 magma_zqmr(
     magma_z_matrix A, magma_z_matrix b, magma_z_matrix *x,
     magma_z_solver_par *solver_par,
-    magma_z_preconditioner *precond_par,
     magma_queue_t queue )
 {
     magma_int_t info = 0;
