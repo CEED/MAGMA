7f5bb8affa831dfd2d486b22e307ecbf981f7094
Hartwig Anzt <hanzt@icl.utk.edu>
2015-10-09 13:00:14 +0000

    add matrixinfo also to solver tester
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@7896 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/testing/testing_zsolver.cpp
+++ sparse-iter/testing/testing_zsolver.cpp
@@ -72,7 +72,16 @@ int main(  int argc, char** argv )
 
         printf( "\n%% matrix info: %d-by-%d with %d nonzeros\n\n",
                             (int) A.num_rows,(int) A.num_cols,(int) A.nnz );
-
+        
+        printf("matrixinfo = [ \n");
+        printf("%%   size   (m x n)     ||   nonzeros (nnz)   ||   nnz/m \n");
+        printf("%%======================================================="
+                            "======%%\n");
+        printf("   %8d  %8d          %10d          %10d\n",
+            (int) A.num_rows, (int) A.num_cols ,(int) A.nnz, (int) A.nnz/A.num_rows );
+        printf("%%======================================================="
+        "======%%\n");
+        printf("];\n");
         // for the eigensolver case
         zopts.solver_par.ev_length = A.num_rows;
         CHECK( magma_zeigensolverinfo_init( &zopts.solver_par, queue ));
