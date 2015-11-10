7685b42ed9f8f2644139299a6e5e702a62720733
Mark Gates <mgates3@icl.utk.edu>
2015-11-10 15:14:55 +0000

    scripts to help v1 -> v2 conversion
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@8023 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

---
+++ scripts/convert_kernel_v2.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -p
+#
+# Does basic conversion of MAGMA kernels from v1 to v2.
+# 1) Kernel launches get stream from queue.
+# 2) Old references to magma_stream use magmablasGetQueue() to get current queue.
+#
+# This does not attempt to write _q versions of functions,
+# e.g., magmablas_ztrsm_q and magmablas_ztrsm.
+#
+# @author Mark Gates
+
+s/<<<(\S)/<<< $1/;
+s/queue *>>>/queue->cuda_stream() >>>/;
+s/magma_stream/magmablasGetQueue()/;
---
+++ scripts/convert_src_v2.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/perl -i
+#
+# Does some basic conversion from MAGMA v1 to v2.
+# 1) Change "stream[...]" to "queues[...]"
+#
+# 2) Change header to magma_v2.h or magma_internal.h
+#
+# 3) Add device to magma_queue_create.
+#    This may add extraneous get_device calls that should be manually removed.
+#
+# 4) Comment out Set/GetKernelStream.
+#    These should be manually removed after verifying the code.
+#
+# 5) Add queue argument to MAGMA kernel calls.
+#    It gets this queue from the last seen SetKernelStream -- but this totally
+#    ignores the actual control flow of the program, so it may be wrong!
+#
+# Modified code must be manually verified for correctness.
+#
+# @author Mark Gates
+
+use strict;
+
+# MAGMA kernels, in alphabetical order
+my @kernels = qw(
+	axpy
+	dot
+	gemm
+	gemv
+	hemm
+	hemv
+	her2k
+	herk
+	lacpy
+	lange
+	lanhe
+	lansy
+	lantr
+	larfb
+	laset
+	laswp
+	symm
+	symv
+	trmm
+	trsm
+	trsv
+	transpose
+	nrm2
+	znrm2
+	cnrm2
+);
+
+my @setget = qw(
+	getvector
+	getmatrix
+	setvector
+	setmatrix
+);
+
+my $kernels = join( "|", @kernels );
+my $setget  = join( "|", @setget  );
+
+$/ = ";";  # slurp C statements
+
+my $queue = "UNKNOWN";
+
+while( <> ) {
+	##print "<<<< $_ >>>>\n\n";
+	
+	# rename stream array to queues
+	s/\bstream\[/queues[/g;
+	
+	# fix headers
+	s/#include "magma.h"/#include "magma_v2.h"/;
+	s/#include "common_magma.h"/#include "magma_internal.h"/;
+	s/#include "common_sparsemagma.h"/#include "magmasparse_internal.h"/;
+	
+	# fix queue_create
+	#  ($1)                      ($2 )
+	s/^( +)magma_queue_create\( *(.*?) *\);/${1}magma_int_t cdev;\n${1}magma_getdevice( \&cdev );\n${1}magma_queue_create( cdev, $2 );/mg;
+	
+	# comment out Set/GetKernelStream & record queue
+	if ( s|(magmablasSetKernelStream\( *(.*?) *\);)|//$1|g ) {
+		##print "[[[[ queue $queue ]]]]\n";
+		$queue = $2;
+	}
+	s|(magmablasGetKernelStream\( *(.*?) *\);)|//$1|g;
+	s|(magma_queue_t +orig_queue)|//$1|g;
+	
+	# add queue to BLAS/GPU kernels
+	#   ($1 ..............................................)      ($2 ...)
+	s/\b(magma(?:blas)?_[isdcz](?:(?:$kernels)\w*|$setget)) *\( ?([^;]*?)\s*\);/$1( $2, $queue );/g;
+	
+	print;
+}
