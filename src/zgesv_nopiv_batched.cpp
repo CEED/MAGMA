b4aa0ece1fd863a981d6b3b8e5d94b671462e759
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2015-12-08 04:13:10 +0000

    updating batched routines to v2
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@8192 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zgesv_nopiv_batched.cpp
+++ src/zgesv_nopiv_batched.cpp
@@ -1,5 +1,5 @@
 /*
-    -- MAGMA (version 1.4) --
+    -- MAGMA (version 2.0) --
        Univ. of Tennessee, Knoxville
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
@@ -9,7 +9,7 @@
 
        @precisions normal z -> s d c
 */
-#include "common_magma.h"
+#include "magma_internal.h"
 #include "batched_kernel_param.h"
 /**
     Purpose
