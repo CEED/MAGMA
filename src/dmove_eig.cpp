d8cfaaa02b8bbeace583710b3c5dc8539270280e
Mark Gates <mgates3@icl.utk.edu>
2015-12-08 04:22:11 +0000

    update dlaln2, dmove_eig to v2 (no real changes)
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@8195 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/dmove_eig.cpp
+++ src/dmove_eig.cpp
@@ -1,5 +1,5 @@
 /*
-    -- MAGMA (version 1.1) --
+    -- MAGMA (version 2.0) --
        Univ. of Tennessee, Knoxville
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
@@ -11,7 +11,7 @@
        @precisions normal d -> s
 
 */
-#include "common_magma.h"
+#include "magma_internal.h"
 
 extern "C" void
 magma_dmove_eig(
