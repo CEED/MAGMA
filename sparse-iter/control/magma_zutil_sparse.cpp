a6543bbac6d55287b6f14d9ea003181d606a089f
Hartwig Anzt <hanzt@icl.utk.edu>
2015-12-11 13:37:37 +0000

    change blocksize/alignment
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@8251 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/control/magma_zutil_sparse.cpp
+++ sparse-iter/control/magma_zutil_sparse.cpp
@@ -100,8 +100,8 @@ magma_zparse_opts(
     
     // fill in default values
     opts->input_format = Magma_CSR;
-    opts->blocksize = 8;
-    opts->alignment = 8;
+    opts->blocksize = 32;
+    opts->alignment = 1;
     opts->output_format = Magma_CSR;
     opts->input_location = Magma_CPU;
     opts->output_location = Magma_CPU;
