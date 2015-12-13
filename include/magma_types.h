0a2bcaea725cbd8e4251114972c1edbb3703aad4
Hartwig Anzt <hanzt@icl.utk.edu>
2015-12-13 00:15:28 +0000

    added preconditioned IDR to solver options
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@8268 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- include/magma_types.h
+++ include/magma_types.h
@@ -520,7 +520,8 @@ typedef enum {
   Magma_PBICGSTABMERGE = 495,
     Magma_AICT         = 496,
     Magma_CUSTOMIC     = 497,
-    Magma_CUSTOMILU    = 498
+    Magma_CUSTOMILU    = 498,
+    Magma_PIDRMERGE    = 499
 } magma_solver_type;
 
 typedef enum {
