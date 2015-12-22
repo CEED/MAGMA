0394b067b494fa7a0f16d7f7e99ba2e1310f4f1b
Mark Gates <mgates3@icl.utk.edu>
2015-12-22 21:45:03 +0000

    update to v2
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@8333 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zstedx.cpp
+++ src/zstedx.cpp
@@ -1,5 +1,5 @@
 /*
-    -- MAGMA (version 1.1) --
+    -- MAGMA (version 2.0) --
        Univ. of Tennessee, Knoxville
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
@@ -10,7 +10,7 @@
        @precisions normal z -> c
 
 */
-#include "common_magma.h"
+#include "magma_internal.h"
 
 /**
     Purpose
