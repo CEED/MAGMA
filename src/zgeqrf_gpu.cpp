584c60e4d5386af6e611e5625c596e1e5cb08758
Mark Gates <mgates3@icl.utk.edu>
2016-01-01 09:48:33 +0000

    Convert to v2. Fix lwork bug for m = nb, n >> m     (ex: -N 64,10000). Fix lwork bug for m >> n, n = nb+eps (ex: -N 10000,129). Sync geqrf_gpu, geqrf2_gpu, geqrf3_gpu codes. Test geqrf3_gpu same as geqrf2_gpu, which allows testing m < n.
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@8375 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- src/zgeqrf_gpu.cpp
+++ src/zgeqrf_gpu.cpp
@@ -7,7 +7,7 @@
 
        @author Stan Tomov
        @author Mark Gates
-       
+
        @precisions normal z -> s d c
 */
 #include "magma_internal.h"
@@ -21,12 +21,15 @@
       Then, the inverse is calculated in place in work, so as a final result,
       work holds the inverse of the upper triangular diagonal block.
 */
-void zsplit_diag_block(magma_int_t ib, magmaDoubleComplex *A, magma_int_t lda, magmaDoubleComplex *work)
+void zsplit_diag_block_invert(
+    magma_int_t ib, magmaDoubleComplex *A, magma_int_t lda,
+    magmaDoubleComplex *work )
 {
+    const magmaDoubleComplex c_zero = MAGMA_Z_ZERO;
+    const magmaDoubleComplex c_one  = MAGMA_Z_ONE;
+    
     magma_int_t i, j, info;
     magmaDoubleComplex *cola, *colw;
-    magmaDoubleComplex c_zero = MAGMA_Z_ZERO;
-    magmaDoubleComplex c_one  = MAGMA_Z_ONE;
 
     for (i=0; i < ib; i++) {
         cola = A    + i*lda;
@@ -51,8 +54,8 @@ void zsplit_diag_block(magma_int_t ib, magmaDoubleComplex *A, magma_int_t lda, m
     the block QR factorization so that they can be applied directly (i.e.,
     without being recomputed) later. As a result, the application
     of Q is much faster. Also, the upper triangular matrices for V have 0s
-    in them. The corresponding parts of the upper triangular R are inverted
-    and stored separately in dT.
+    in them. The corresponding parts of the upper triangular R are inverted and
+    stored separately in dT.
 
     Arguments
     ---------
@@ -75,7 +78,7 @@ void zsplit_diag_block(magma_int_t ib, magmaDoubleComplex *A, magma_int_t lda, m
             Details).
 
     @param[in]
-    ldda     INTEGER
+    ldda    INTEGER
             The leading dimension of the array dA.  LDDA >= max(1,M).
             To benefit from coalescent memory accesses LDDA must be
             divisible by 16.
@@ -89,9 +92,10 @@ void zsplit_diag_block(magma_int_t ib, magmaDoubleComplex *A, magma_int_t lda, m
     dT      (workspace) COMPLEX_16 array on the GPU,
             dimension (2*MIN(M, N) + ceil(N/32)*32 )*NB,
             where NB can be obtained through magma_get_zgeqrf_nb(M).
-            It starts with MIN(M,N)*NB block that store the triangular T
-            matrices, followed by the MIN(M,N)*NB block of the diagonal
-            inverses for the R matrix. The rest of the array is used as workspace.
+            It starts with a MIN(M,N)*NB block that stores the triangular T
+            matrices, followed by a MIN(M,N)*NB block that stores inverses of
+            the diagonal blocks of the R matrix.
+            The rest of the array is used as workspace.
 
     @param[out]
     info    INTEGER
@@ -103,11 +107,11 @@ void zsplit_diag_block(magma_int_t ib, magmaDoubleComplex *A, magma_int_t lda, m
     ---------------
     The matrix Q is represented as a product of elementary reflectors
 
-       Q = H(1) H(2) . . . H(k), where k = min(m,n).
+        Q = H(1) H(2) . . . H(k), where k = min(m,n).
 
     Each H(i) has the form
 
-       H(i) = I - tau * v * v'
+        H(i) = I - tau * v * v^H
 
     where tau is a complex scalar, and v is a complex vector with
     v(1:i-1) = 0 and v(i) = 1; v(i+1:m) is stored on exit in A(i+1:m,i),
@@ -126,21 +130,19 @@ magma_zgeqrf_gpu(
     #ifdef HAVE_clBLAS
     #define dA(i_, j_)  dA, (dA_offset + (i_) + (j_)*(ldda))
     #define dT(i_)      dT, (dT_offset + (i_)*nb)
-    #define dUT(i_)     dT, (dT_offset + (  minmn + (i_))*nb)
+    #define dR(i_)      dT, (dT_offset + (  minmn + (i_))*nb)
     #define dwork(i_)   dT, (dT_offset + (2*minmn + (i_))*nb)
     #else
     #define dA(i_, j_) (dA + (i_) + (j_)*(ldda))
     #define dT(i_)     (dT + (i_)*nb)
-    #define dUT(i_)    (dT + (  minmn + (i_))*nb)
+    #define dR(i_)     (dT + (  minmn + (i_))*nb)
     #define dwork(i_)  (dT + (2*minmn + (i_))*nb)
     #endif
     
-    magma_int_t i, k, minmn, old_i, old_ib, rows, cols;
-    magma_int_t ib, nb;
-    magma_int_t ldwork, lddwork, lwork, lhwork;
-    magmaDoubleComplex *work, *hwork, *ut;
+    magmaDoubleComplex *work, *hwork, *R;
+    magma_int_t cols, i, ib, ldwork, lddwork, lhwork, lwork, minmn, nb, old_i, old_ib, rows;
     
-    /* check arguments */
+    // check arguments
     *info = 0;
     if (m < 0) {
         *info = -1;
@@ -154,97 +156,118 @@ magma_zgeqrf_gpu(
         return *info;
     }
     
-    k = minmn = min(m,n);
-    if (k == 0)
+    minmn = min( m, n );
+    if (minmn == 0)
         return *info;
     
+    // TODO: use min(m,n), but that affects dT
     nb = magma_get_zgeqrf_nb( m );
     
+    // dT contains 3 blocks:
+    // dT    is minmn*nb
+    // dR    is minmn*nb
+    // dwork is n*nb
+    lddwork = n;
+    
     // work  is m*nb for panel
-    // hwork is n*nb
-    // ut    is nb*nb
-    lwork  = (m + n + nb)*nb;
-    lhwork = n*nb;
+    // hwork is n*nb, and at least nb*nb for T in larft
+    // R     is nb*nb
+    ldwork = m;
+    lhwork = max( n*nb, nb*nb );
+    lwork  = ldwork*nb + lhwork + nb*nb;
+    // last block needs rows*cols for matrix and prefers cols*nb for work
+    // worst case is n > m*nb, m a small multiple of nb:
+    // needs n*nb + n > (m+n)*nb
+    // prefers 2*n*nb, about twice above (m+n)*nb.
+    i = ((minmn-1)/nb)*nb;
+    lwork = max( lwork, (m-i)*(n-i) + (n-i)*nb );
     
     if (MAGMA_SUCCESS != magma_zmalloc_pinned( &work, lwork )) {
         *info = MAGMA_ERR_HOST_ALLOC;
         return *info;
     }
-    hwork = work + m*nb;
-    ut    = work + m*nb + n*nb;
-    memset( ut, 0, nb*nb*sizeof(magmaDoubleComplex) );
+    hwork = work + ldwork*nb;
+    R     = work + ldwork*nb + lhwork;
+    memset( R, 0, nb*nb*sizeof(magmaDoubleComplex) );
     
     magma_queue_t queues[2];
     magma_device_t cdev;
     magma_getdevice( &cdev );
     magma_queue_create( cdev, &queues[0] );
     magma_queue_create( cdev, &queues[1] );
-    
-    ldwork  = m;
-    lddwork = n;
-    
-    if ( nb > 1 && nb < k ) {
-        /* Use blocked code initially */
+        
+    if ( nb > 1 && nb < minmn ) {
+        // need nb*nb for T in larft
+        assert( lhwork >= nb*nb );
+        
+        // Use blocked code initially
         old_i = 0; old_ib = nb;
-        for (i = 0; i < k-nb; i += nb) {
-            ib = min( k-i, nb );
+        for (i = 0; i < minmn-nb; i += nb) {
+            ib = min( minmn-i, nb );
             rows = m - i;
+            
+            // get i-th panel from device
             magma_zgetmatrix_async( rows, ib,
-                                    dA(i,i),  ldda,
-                                    work, ldwork, queues[1] );
+                                    dA(i,i), ldda,
+                                    work,    ldwork, queues[1] );
             if (i > 0) {
-                /* Apply H' to A(i:m,i+2*ib:n) from the left */
+                // Apply H^H to A(i:m,i+2*ib:n) from the left
                 cols = n - old_i - 2*old_ib;
                 magma_zlarfb_gpu( MagmaLeft, MagmaConjTrans, MagmaForward, MagmaColumnwise,
                                   m-old_i, cols, old_ib,
                                   dA(old_i, old_i         ), ldda, dT(old_i), nb,
-                                  dA(old_i, old_i+2*old_ib), ldda, dwork(0), lddwork, queues[0] );
+                                  dA(old_i, old_i+2*old_ib), ldda, dwork(0),  lddwork, queues[0] );
                 
-                /* store the diagonal block */
+                // Fix the diagonal block
                 magma_zsetmatrix_async( old_ib, old_ib,
-                                        ut,         old_ib,
-                                        dUT(old_i), old_ib, queues[0] );
+                                        R,         old_ib,
+                                        dR(old_i), old_ib, queues[0] );
             }
             
-            magma_queue_sync( queues[1] );
-            lapackf77_zgeqrf( &rows, &ib, work, &ldwork, tau+i, hwork, &lhwork, info );
-            /* Form the triangular factor of the block reflector in hwork
-               H = H(i) H(i+1) . . . H(i+ib-1) */
+            magma_queue_sync( queues[1] );  // wait to get work(i)
+            lapackf77_zgeqrf( &rows, &ib, work, &ldwork, &tau[i], hwork, &lhwork, info );
+            // Form the triangular factor of the block reflector in hwork
+            // H = H(i) H(i+1) . . . H(i+ib-1)
             lapackf77_zlarft( MagmaForwardStr, MagmaColumnwiseStr,
                               &rows, &ib,
-                              work, &ldwork, tau+i, hwork, &ib );
+                              work, &ldwork, &tau[i], hwork, &ib );
             
-            /* Put 0s in the upper triangular part of a panel (and 1s on the
-               diagonal); copy the upper triangular to ut and invert it. */
+            // wait for previous trailing matrix update (above) to finish with R
             magma_queue_sync( queues[0] );
-            zsplit_diag_block( ib, work, ldwork, ut );
+            
+            // copy the upper triangle of panel to R and invert it, and
+            // set  the upper triangle of panel (V) to identity
+            zsplit_diag_block_invert( ib, work, ldwork, R );
+            
+            // send i-th V matrix to device
             magma_zsetmatrix( rows, ib,
                               work, ldwork,
-                              dA(i,i), ldda, queues[0] );
+                              dA(i,i), ldda, queues[1] );
             
             if (i + ib < n) {
-                /* Send the triangular factor T to the GPU */
-                magma_zsetmatrix( ib, ib, hwork, ib, dT(i), nb, queues[0] );
+                // send T matrix to device
+                magma_zsetmatrix( ib, ib,
+                                  hwork, ib,
+                                  dT(i), nb, queues[1] );
                 
-                if (i+nb < k-nb) {
-                    /* Apply H' to A(i:m,i+ib:i+2*ib) from the left */
+                if (i+nb < minmn-nb) {
+                    // Apply H^H to A(i:m,i+ib:i+2*ib) from the left
                     magma_zlarfb_gpu( MagmaLeft, MagmaConjTrans, MagmaForward, MagmaColumnwise,
                                       rows, ib, ib,
                                       dA(i, i   ), ldda, dT(i),  nb,
-                                      dA(i, i+ib), ldda, dwork(0), lddwork, queues[0] );
+                                      dA(i, i+ib), ldda, dwork(0), lddwork, queues[1] );
                 }
                 else {
-                    cols = n-i-ib;
+                    // Apply H^H to A(i:m,i+ib:n) from the left
                     magma_zlarfb_gpu( MagmaLeft, MagmaConjTrans, MagmaForward, MagmaColumnwise,
-                                      rows, cols, ib,
+                                      rows, n-i-ib, ib,
                                       dA(i, i   ), ldda, dT(i),  nb,
-                                      dA(i, i+ib), ldda, dwork(0), lddwork, queues[0] );
-                    /* store the diagonal block */
+                                      dA(i, i+ib), ldda, dwork(0), lddwork, queues[1] );
+                    // Fix the diagonal block
                     magma_zsetmatrix( ib, ib,
-                                      ut, ib,
-                                      dUT(i), ib, queues[0] );
+                                      R,     ib,
+                                      dR(i), ib, queues[1] );
                 }
-                magma_queue_sync( queues[0] );
                 old_i  = i;
                 old_ib = ib;
             }
@@ -253,19 +276,21 @@ magma_zgeqrf_gpu(
         i = 0;
     }
     
-    /* Use unblocked code to factor the last or only block. */
-    if (i < k) {
-        ib   = n-i;
+    // Use unblocked code to factor the last or only block.
+    if (i < minmn) {
         rows = m-i;
-        magma_zgetmatrix( rows, ib, dA(i, i), ldda, work, rows, queues[0] );
-        lapackf77_zgeqrf( &rows, &ib, work, &rows, tau+i, hwork, &lhwork, info );
-        magma_zsetmatrix( rows, ib, work, rows, dA(i, i), ldda, queues[0] );
+        cols = n-i;
+        magma_zgetmatrix( rows, cols, dA(i, i), ldda, work, rows, queues[1] );
+        // see comments for lwork above
+        lhwork = lwork - rows*cols;
+        lapackf77_zgeqrf( &rows, &cols, work, &rows, &tau[i], &work[rows*cols], &lhwork, info );
+        magma_zsetmatrix( rows, cols, work, rows, dA(i, i), ldda, queues[1] );
     }
-    
+        
     magma_queue_destroy( queues[0] );
     magma_queue_destroy( queues[1] );
     
     magma_free_pinned( work );
     
     return *info;
-} /* magma_zgeqrf_gpu */
+} // magma_zgeqrf_gpu
