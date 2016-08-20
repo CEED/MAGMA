df77440301ee06d4ec1329b88979cf20a0b7919f
Mark Gates <mgates3@icl.utk.edu>
2016-08-20 15:00:42 +0000

    nom unused
    git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@9440 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

--- sparse-iter/src/zbicgstab_merge.cpp
+++ sparse-iter/src/zbicgstab_merge.cpp
@@ -83,15 +83,16 @@ magma_zbicgstab_merge(
     
     // solver variables
     magmaDoubleComplex alpha, beta, omega, rho_old, rho_new;
-    double nom, betanom, nom0, r0, res, nomb;
+    double betanom, nom0, r0, res, nomb;
     res=0;
+    //double nom;
     //double den;
 
     // solver setup
     CHECK(  magma_zresidualvec( A, b, *x, &r, &nom0, queue));
     magma_zcopy( dofs, r.dval, 1, rr.dval, 1, queue );                  // rr = r
     betanom = nom0;
-    nom = nom0*nom0;
+    //nom = nom0*nom0;
     rho_new = magma_zdotc( dofs, r.dval, 1, r.dval, 1, queue );             // rho=<rr,r>
     rho_old = omega = alpha = MAGMA_Z_MAKE( 1.0, 0. );
     solver_par->init_res = nom0;
@@ -188,7 +189,7 @@ magma_zbicgstab_merge(
 
         res = betanom = magma_dznrm2( dofs, r.dval, 1, queue );
 
-        nom = betanom*betanom;
+        //nom = betanom*betanom;
 
         if ( solver_par->verbose > 0 ) {
             tempo2 = magma_sync_wtime( queue );
