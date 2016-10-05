617699e0b421a3ca3c4a114abe1d669fabcb685f
Hartwig Anzt <hanzt@icl.utk.edu>
2016-10-05 17:35:49 +0200

    workaround for CG+CSR5

--- sparse/control/magma_zutil_sparse.cpp
+++ sparse/control/magma_zutil_sparse.cpp
@@ -507,6 +507,10 @@ magma_zparse_opts(
         && opts->precond_par.solver == Magma_PARILU )
             opts->precond_par.solver = Magma_PARIC;
             
+    // workaround for CG not being optimized for CSR5
+    if ( opts->solver_par.solver == Magma_CGMERGE && opts->output_format == Magma_CSR5 )
+        opts->solver_par.solver = Magma_CG;
+            
             
     return info;
 }
