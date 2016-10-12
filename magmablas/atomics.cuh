67139964308f4e8770972400daa26d08d46dd1c3
Hartwig Anzt <hanzt@icl.utk.edu>
2016-10-12 15:29:27 -0400

    fix from Ahmad

--- magmablas/atomics.cuh
+++ magmablas/atomics.cuh
@@ -11,10 +11,10 @@ magmablas_satomic_add(float* address, float val)
     return atomicAdd(address, val);
 }
 /***************************************************************************/
-#if __CUDA_ARCH__ < 600    // atomic add for double precision is natively supported on sm_60
 __device__ static __inline__ double 
 magmablas_datomic_add(double* address, double val)
 {
+#if __CUDA_ARCH__ < 600    // atomic add for double precision is natively supported on sm_60
 	unsigned long long int* address_as_ull =
 	(unsigned long long int*)address;
 	unsigned long long int old = *address_as_ull, assumed;
@@ -23,8 +23,10 @@ magmablas_datomic_add(double* address, double val)
 		old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val + __longlong_as_double(assumed)));
 	} while (assumed != old);
 	return __longlong_as_double(old);
-}
+#else
+    return atomicAdd(address, val);
 #endif
+}
 /***************************************************************************/
 __device__ static __inline__ magmaFloatComplex 
 magmablas_catomic_add(magmaFloatComplex* address, magmaFloatComplex val)
