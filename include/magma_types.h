bfea5cb01ab8dc95b62b3163b16ce03a68ee92df
Unknown <mgates3@utk.edu>
2016-11-20 20:19:51 -0500

    version 2.2.0

--- include/magma_types.h
+++ include/magma_types.h
@@ -310,7 +310,7 @@ float  magma_cabsf( magmaFloatComplex  x );
 
 // -----------------------------------------------------------------------------
 #define MAGMA_VERSION_MAJOR 2
-#define MAGMA_VERSION_MINOR 1
+#define MAGMA_VERSION_MINOR 2
 #define MAGMA_VERSION_MICRO 0
 
 // stage is "svn", "beta#", "rc#" (release candidate), or blank ("") for final release
